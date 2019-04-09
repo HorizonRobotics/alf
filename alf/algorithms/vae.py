f5f50377afc5014141f6509278971c8908dd59f2
Tao Xu <taoxu76@gmail.com>
2019-04-08 17:14:20 -0700

    addressing comments and test encoding/sampling vae with prior networks

--- alf/algorithms/vae.py
+++ alf/algorithms/vae.py
@@ -1,35 +1,36 @@
 import tensorflow as tf
+from tf_agents.networks import network
 
-
-class VariationalAutoEncoder:
+class VariationalAutoEncoder(network.Network):
     """
     VariationalAutoEncoder encodes data into diagonal multivariate gaussian, do sampling with
     reparametrization trick, and return kl divergence between posterior and prior.
     """
     def __init__(self,
-                 hidden_dims,
+                 hidden_dim,
                  prior_network=None,
                  preprocess_layers=None):
 
         """Create an instance of `VariationalAutoEncoder`
 
         Args:
-             hidden_dims (int): dimension of latent dimension
+             hidden_dim (int): dimension of latent vector
              prior_network:     network to compute the priors (mean, log_var)
              preprocess_layers: layers to preprocess input data before project into (mean, log_var)
 
         """
+        super(VariationalAutoEncoder, self).__init__(dim, size)
         if preprocess_layers is None:
-            # according to appendix in deepmind merlin paper
+            # according to appendix 2.4-2.5 in paper: https://arxiv.org/pdf/1803.10760.pdf
             self._preprocess_layers = tf.keras.Sequential([
-                tf.keras.layers.Dense(2 * hidden_dims, activation='tanh') for i in range(2)])
+                tf.keras.layers.Dense(2 * hidden_dim, activation='tanh') for i in range(2)])
         else:
             self._preprocess_layers = preprocess_layers
 
-        self._hidden_dims = hidden_dims
+        self._hidden_dim = hidden_dim
         self._prior_network = prior_network
-        self._z_mean = tf.keras.layers.Dense(hidden_dims)
-        self._z_log_var = tf.keras.layers.Dense(hidden_dims)
+        self._z_mean = tf.keras.layers.Dense(hidden_dim)
+        self._z_log_var = tf.keras.layers.Dense(hidden_dim)
 
     def sampling_forward(self, inputs):
         """Encoding the data into latent space then do sampling.
@@ -37,23 +38,25 @@ class VariationalAutoEncoder:
         Args:
             inputs (Tensor or Tuple of Tensor): data to be encoded. If it has a prior network, then the argument
             is a tuple of (prior_input, new_observation).
+        Returns:
+            Tuple of z and kl_loss. z is tensor of shape (N, hidden_dim), kl_loss is tensor of shape (N, 1)
         """
         if self._prior_network:
             prior_input, new_obs = inputs
             prior_z_mean, prior_z_log_var = self._prior_network(prior_input)
-            inputs = tf.concat([prior_input, new_obs, prior_z_mean, priror_z_log_var], -1)
+            inputs = tf.concat([prior_input, new_obs, prior_z_mean, prior_z_log_var], -1)
 
         latents = self._preprocess_layers(inputs)
         z_mean = self._z_mean(latents)
         z_log_var = self._z_log_var(latents)
 
         if self._prior_network:
+            kl_div_loss = tf.square(z_mean) / tf.exp(prior_z_log_var) + \
+                          tf.exp(z_log_var) - z_log_var - 1.0
+
             z_mean += prior_z_mean
             z_log_var += prior_z_log_var
 
-            kl_div_loss = tf.square(z_mean - prior_z_mean) / tf.exp(prior_z_log_var) + \
-                          tf.exp(z_log_var - prior_z_log_var) + \
-                          prior_z_log_var - z_log_var - 1.0
         else:
             kl_div_loss = tf.square(z_mean) + tf.exp(z_log_var) - 1.0 - z_log_var
 
--- alf/algorithms/vae_test.py
+++ alf/algorithms/vae_test.py
@@ -1,80 +1,161 @@
 import tensorflow as tf
+import unittest
 import alf.algorithms.vae as vae
 import numpy as np
 
 from tensorflow.keras.datasets import mnist
-
-# MNIST dataset
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-
-image_size = x_train.shape[1]
-original_dim = image_size * image_size
-x_train = np.reshape(x_train, [-1, original_dim])
-x_test = np.reshape(x_test, [-1, original_dim])
-x_train = x_train.astype('float32') / 255
-x_test = x_test.astype('float32') / 255
-
-# network parameters
-input_shape = (original_dim, )
-latent_dim = 2
-batch_size = 100
-epochs = 50
-
-vae = vae.VariationalAutoEncoder(
-    latent_dim,
-    preprocess_layers=tf.keras.layers.Dense(512, activation='relu'))
-
-decoding_l1 = tf.keras.layers.Dense(512, activation='relu')
-decoding_l2 = tf.keras.layers.Dense(original_dim, activation='sigmoid')
-
-inputs = tf.keras.layers.Input(shape=input_shape)
-z, kl_loss = vae.sampling_forward(inputs)
-outputs = decoding_l2(decoding_l1(z))
-
-loss = tf.reduce_mean(tf.keras.losses.mse(inputs, outputs) * original_dim + kl_loss)
-
-model = tf.keras.Model(inputs, outputs, name = "vae")
-model.add_loss(loss)
-model.compile(optimizer='adam')
-model.summary()
-
-model.fit(x_train,
-          epochs=epochs,
-          batch_size=batch_size,
-          validation_data=(x_test, None))
-model.save_weights('/tmp/vae/vae_mlp_mnist')
-
 import matplotlib.pyplot as plt
 
-# test decoding image qualities
-nrows = 20
-ncols = 2
-test_inputs = x_test[:nrows]
-decoded_output = model.predict(test_inputs)
-
-fig = plt.figure()
-idx = 1
-for i in range(nrows):
-    fig.add_subplot(nrows, ncols, idx)
-    plt.imshow(np.reshape(x_test[i], (image_size, image_size)))
-    fig.add_subplot(nrows, ncols, idx + 1)
-    plt.imshow(np.reshape(decoded_output[i], (image_size, image_size)))
-
-    idx+= 2
-plt.show()
+class VaeMnistTest(unittest.TestCase):
+    def setUp(self):
+        # MNIST dataset
+        (x_train, self.y_train), (x_test, self.y_test) = mnist.load_data()
+        self.image_size = x_train.shape[1]
+        self.original_dim = self.image_size * self.image_size
+        self.x_train = np.reshape(x_train, [-1, self.original_dim])
+        self.x_test = np.reshape(x_test, [-1, self.original_dim])
+        self.x_train = self.x_train.astype('float32') / 255
+        self.x_test = self.x_test.astype('float32') / 255
 
 
-# test sampled image qualities
-eps = tf.random.normal((nrows * nrows, latent_dim), dtype=tf.float32, mean=0., stddev=1.0)
-sampled_outputs = decoding_l2(decoding_l1(eps))
+        # network parameters
+        self.input_shape = (self.original_dim, )
+        self.latent_dim = 2
+        self.batch_size = 100
+        self.epochs = 20
 
-fig = plt.figure()
-#import pdb; pdb.set_trace()
-idx = 0
-for i in range(nrows):
-    for j in range(nrows):
-        fig.add_subplot(nrows, nrows, idx + 1)
-        plt.imshow(np.reshape(sampled_outputs[idx], (image_size, image_size)))
-        idx += 1
 
-plt.show()
+    def showEncodedImages(self, model, withPrior=False):
+        # test decoding image qualities
+        nrows = 20
+        ncols = 2
+        test_inputs = self.x_test[:nrows] if not withPrior else (self.y_test[:nrows], self.x_test[:nrows])
+        decoded_output = model.predict(test_inputs)
+
+        fig = plt.figure()
+        idx = 1
+        for i in range(nrows):
+            fig.add_subplot(nrows, ncols, idx)
+            plt.imshow(np.reshape(self.x_test[i], (self.image_size, self.image_size)))
+            fig.add_subplot(nrows, ncols, idx + 1)
+            plt.imshow(np.reshape(decoded_output[i], (self.image_size, self.image_size)))
+            idx+= 2
+
+        plt.show()
+
+
+    def showSampledImages(self, decodingFunction):
+        nrows = 10
+        eps = tf.random.normal((nrows * nrows, self.latent_dim), dtype=tf.float32, mean=0., stddev=1.0)
+        sampled_outputs = decodingFunction(eps)
+        fig = plt.figure()
+        idx = 0
+        for i in range(nrows):
+            for j in range(nrows):
+                fig.add_subplot(nrows, nrows, idx + 1)
+                plt.imshow(np.reshape(sampled_outputs[idx], (self.image_size, self.image_size)))
+                idx += 1
+
+        plt.show()
+
+
+class VaeNopriorTest(VaeMnistTest):
+    def runTest(self):
+        encoder = vae.VariationalAutoEncoder(
+            self.latent_dim,
+            preprocess_layers=tf.keras.layers.Dense(512, activation='relu'))
+
+        decoding_layers = tf.keras.Sequential([
+            tf.keras.layers.Dense(512, activation='relu'),
+            tf.keras.layers.Dense(self.original_dim, activation='sigmoid')])
+
+        inputs = tf.keras.layers.Input(shape=self.input_shape)
+        z, kl_loss = encoder.sampling_forward(inputs)
+        outputs = decoding_layers(z)
+
+        loss = tf.reduce_mean(tf.keras.losses.mse(inputs, outputs) * self.original_dim + kl_loss)
+
+        model = tf.keras.Model(inputs, outputs, name = "vae")
+        model.add_loss(loss)
+        model.compile(optimizer='adam')
+        model.summary()
+
+        hist = model.fit(self.x_train,
+          epochs=self.epochs,
+          batch_size=self.batch_size,
+          validation_data=(self.x_test, None))
+
+        last_val_loss = hist.history['val_loss'][-1]
+        self.assertTrue(last_val_loss <= 39.0 and last_val_loss > 38.0)
+        self.showEncodedImages(model)
+        self.showSampledImages(lambda eps: decoding_layers(eps))
+
+
+class PriorNetwork(tf.keras.Model):
+    def __init__(self, embedding_dim, hidden_dim):
+        super(PriorNetwork, self).__init__()
+        self.latent_nn = tf.keras.layers.Dense(2 * hidden_dim)
+        self.z_mean = tf.keras.layers.Dense(hidden_dim)
+        self.z_log_var = tf.keras.layers.Dense(hidden_dim)
+
+    def call(self, state):
+        latent = self.latent_nn(state)
+        return self.z_mean(latent), self.z_log_var(latent)
+
+class VaePriorNetworkTest(VaeMnistTest):
+    def runTest(self):
+        prior_network = PriorNetwork(2, self.latent_dim)
+        encoder = vae.VariationalAutoEncoder(
+            self.latent_dim,
+            prior_network=prior_network,
+            preprocess_layers=tf.keras.layers.Dense(512, activation='relu'))
+
+        decoding_layers = tf.keras.Sequential([
+            tf.keras.layers.Dense(512, activation='relu'),
+            tf.keras.layers.Dense(self.original_dim, activation='sigmoid')])
+
+        inputs = tf.keras.layers.Input(shape=self.input_shape)
+        prior_inputs = tf.keras.layers.Input(shape=(1,))
+        prior_inputs_one_hot = tf.reshape(tf.one_hot(tf.cast(prior_inputs, tf.int32), 10), shape=(-1, 10))
+        z, kl_loss = encoder.sampling_forward((prior_inputs_one_hot, inputs))
+        outputs = decoding_layers(z)
+
+        loss = tf.reduce_mean(tf.keras.losses.mse(inputs, outputs) * self.original_dim + kl_loss)
+
+        model = tf.keras.Model(inputs=[prior_inputs, inputs], outputs=outputs, name = "vae")
+        model.add_loss(loss)
+        model.compile(optimizer='adam')
+        model.summary()
+
+        hist = model.fit([self.y_train, self.x_train],
+          epochs=self.epochs,
+          batch_size=self.batch_size,
+          validation_data=([self.y_test, self.x_test], None))
+
+        last_val_loss = hist.history['val_loss'][-1]
+        # total loss is much smaller with label based prior network.
+        self.assertTrue(last_val_loss <= 35.5 and last_val_loss >= 34.5)
+        self.showEncodedImages(model, True)
+
+        # with prior network, sampling is more complicated
+        nrows = 10
+        fig = plt.figure()
+        idx = 0
+        for i in range(10):
+            z_mean_prior, z_log_var_prior = prior_network(
+                tf.stack([tf.one_hot(i, 10) for _ in range(10)]))
+
+            eps = tf.random.normal((nrows, self.latent_dim), dtype=tf.float32, mean=0., stddev=0.5)
+            sampled_outputs = decoding_layers(eps + z_mean_prior)
+            # for the same digit i, we sample a bunch of images
+            # it actually looks great.
+            for j in range(nrows):
+                fig.add_subplot(nrows, nrows, idx + 1)
+                plt.imshow(np.reshape(sampled_outputs[j], (self.image_size, self.image_size)))
+                idx += 1
+
+        plt.show()
+
+
+if __name__ == '__main__':
+    unittest.main()
