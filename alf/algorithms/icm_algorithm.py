013c2c2c8a8cc678075b852daa90fed21892cf0f
emailweixu <emailweixu@gmail.com>
2019-05-03 11:58:41 -0700

    ICM algorithm

---
+++ alf/algorithms/icm_algorithm.py
@@ -0,0 +1,163 @@
+# Copyright (c) 2019 Horizon Robotics. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from collections import namedtuple
+
+import gin.tf
+import tensorflow as tf
+
+from tf_agents.agents.tf_agent import LossInfo
+from tf_agents.networks.network import Network
+import tf_agents.specs.tensor_spec as tensor_spec
+
+from alf.algorithms.algorithm import Algorithm, AlgorithmStep
+from alf.utils.losses import element_wise_squared_loss
+from alf.utils.adaptive_normalizer import ScalarAdaptiveNormalizer
+from alf.utils.encoding_network import EncodingNetwork
+
+ICMLossInfo = namedtuple("ICMLossInfo", ["forward_loss", "inverse_loss"])
+
+
+def validate_specs(action_spec):
+    """Validates the spec contains a single action."""
+    flat_action_spec = tf.nest.flatten(action_spec)
+    if len(flat_action_spec) > 1:
+        raise ValueError(
+            'Network only supports action_specs with a single action.')
+
+    if flat_action_spec[0].shape not in [(), (1, )]:
+        raise ValueError(
+            'Network only supports action_specs with shape in [(), (1,)])')
+
+
+@gin.configurable
+class ICMAlgorithm(Algorithm):
+    """Intrinsic Curiosity Module
+
+    This module generate the intrisic reward based on predition error of
+    observation.
+
+    See Pathak et al "Curiosity-driven Exploriation by Self-supervised Prediction"
+    """
+
+    def __init__(self,
+                 action_spec,
+                 feature_spec,
+                 hidden_size=256,
+                 reward_scale=1.0,
+                 reward_adapt_speed=8.0,
+                 encoding_net: Network = None,
+                 forward_net: Network = None,
+                 inverse_net: Network = None,
+                 name="ICMAlgorithm"):
+
+        super(ICMAlgorithm, self).__init__(
+            train_state_spec=feature_spec, name=name)
+
+        flat_action_spec = tf.nest.flatten(action_spec)
+        if len(flat_action_spec) > 1:
+            raise ValueError(
+                'ICM only supports action_specs with a single action.')
+
+        action_spec = flat_action_spec[0]
+
+        if tensor_spec.is_discrete(action_spec):
+            self._num_actions = action_spec.maximum - action_spec.minimum + 1
+        else:
+            self._num_actions = action_spec.shape[-1]
+
+        self._action_spec = action_spec
+        self._reward_scale = 1.0
+
+        feature_dim = tf.nest.flatten(feature_spec)[0].shape[-1]
+
+        self._encoding_net = encoding_net
+
+        if forward_net is None:
+            encoded_action_spec = tensor_spec.TensorSpec((self._num_actions, ),
+                                                         dtype=tf.float32)
+            forward_net = EncodingNetwork(
+                name="forward_net",
+                input_tensor_spec=[feature_spec, encoded_action_spec],
+                fc_layer_params=(hidden_size, ),
+                last_layer_size=feature_dim)
+
+        self._forward_net = forward_net
+
+        if inverse_net is None:
+            inverse_net = EncodingNetwork(
+                name="inverse_net",
+                input_tensor_spec=[feature_spec, feature_spec],
+                fc_layer_params=(hidden_size, ),
+                last_layer_size=self._num_actions,
+                last_kernel_initializer=tf.initializers.Zeros())
+
+        self._inverse_net = inverse_net
+
+        self._reward_normalizer = ScalarAdaptiveNormalizer(
+            speed=reward_adapt_speed)
+
+    def _encode_action(self, action):
+        if tensor_spec.is_discrete(self._action_spec):
+            return tf.one_hot(indices=action, depth=self._num_actions)
+        else:
+            return action
+
+    def _variables(self):
+        vars = self._forward_net.variables + self._inverse_net.variables
+        if self._encoding_net:
+            vars += self._encoding_net.variables
+        return vars
+
+    def train_step(self, inputs, state):
+        """
+        Args:
+            inputs (tuple): observation and previous action
+        Returns:
+            TrainStep:
+                outputs: intrisic reward
+                state:
+                info:
+        """
+        feature, prev_action = inputs
+        if self._encoding_net is not None:
+            feature, _ = self._encoding_net(feature)
+        prev_feature = state
+        prev_action = self._encode_action(prev_action)
+
+        forward_pred, _ = self._forward_net(
+            inputs=[tf.stop_gradient(prev_feature), prev_action])
+        forward_loss = 0.5 * tf.reduce_mean(
+            tf.square(tf.stop_gradient(feature) - forward_pred), axis=-1)
+
+        action_pred, _ = self._inverse_net(inputs=[prev_feature, feature])
+
+        if tensor_spec.is_discrete(self._action_spec):
+            inverse_loss = tf.nn.softmax_cross_entropy_with_logits(
+                labels=prev_action, logits=action_pred)
+        else:
+            inverse_loss = 0.5 * tf.reduce_mean(
+                tf.square(prev_action - action_pred), axis=-1)
+
+        intrinsic_reward = tf.stop_gradient(forward_loss)
+        intrinsic_reward = self._reward_normalizer.normalize(intrinsic_reward)
+        intrinsic_reward *= self._reward_scale
+
+        return AlgorithmStep(
+            outputs=intrinsic_reward,
+            state=feature,
+            info=LossInfo(
+                loss=forward_loss + inverse_loss,
+                extra=ICMLossInfo(
+                    forward_loss=forward_loss, inverse_loss=inverse_loss)))
--- alf/utils/common.py
+++ alf/utils/common.py
@@ -26,8 +26,10 @@ def get_target_updater(model, target_model, tau=1.0, period=1):
     w_t = (1 - tau) * w_t + tau * w_s
 
     Args:
+        model (Network): the current model.
+        target_model (Network): the model to be updated.
         tau (float): A float scalar in [0, 1]. Default `tau=1.0` means hard
-        update.
+            update.
         period (int): Step interval at which the target model is updated.
 
     Returns:
@@ -43,20 +45,58 @@ def get_target_updater(model, target_model, tau=1.0, period=1):
                                        'periodic_update_targets')
 
 
-def add_loss_summaries(loss_info: LossInfo, step):
+def add_nested_summaries(prefix, data):
+    """Add summary about loss_info
+
+    Args:
+        prefix (str): the prefix of the names of the summaries
+        data (namedtuple): data to be summarized
+    """
+    for field in data._fields:
+        elem = getattr(data, field)
+        name = prefix + '/' + field
+        if isinstance(elem, tuple) and hasattr(elem, '_fields'):
+            add_nested_summaries(name, elem)
+        elif isinstance(elem, tf.Tensor):
+            tf.summary.scalar(name, elem)
+
+
+def add_loss_summaries(loss_info: LossInfo):
     """Add summary about loss_info
 
     Args:
         loss_info (LossInfo): loss_info.extra must be a namedtuple
     """
-    tf.summary.scalar('loss', data=loss_info.loss, step=step)
+    tf.summary.scalar('loss', data=loss_info.loss)
     if not loss_info.extra:
         return
     if getattr(loss_info.extra, '_fields') is None:
         return
-    for field in loss_info.extra._fields:
-        tf.summary.scalar(
-            'loss/' + field, data=getattr(loss_info.extra, field), step=step)
+    add_nested_summaries('loss', loss_info.extra)
+
+
+def add_action_summaries(actions, action_specs):
+    """Generate histogram summaries for actions.
+
+    Actions whose rank is more than 1 will be skipped.
+
+    Args:
+        actions (nested Tensor): actions to be summarized
+        action_specs (nested TensorSpec): spec for the actions
+    """
+    action_specs = tf.nest.flatten(action_specs)
+    actions = tf.nest.flatten(actions)
+    for i, (action, action_spec) in enumerate(zip(actions, action_specs)):
+        if len(action_spec.shape) > 1:
+            continue
+        if len(action_spec.shape) == 0:
+            action_dim = 1
+        else:
+            action_dim = action_spec.shape[-1]
+        action = tf.reshape(action, (-1, action_dim))
+        for a in range(action_dim):
+            # TODO: use a descriptive name for the summary
+            tf.summary.histogram("action/%s/%s" % (i, a), action[:, a])
 
 
 def get_distribution_params(nested_distribution):
@@ -65,10 +105,10 @@ def get_distribution_params(nested_distribution):
     Only returns parameters that have tf.Tensor values.
 
     Args:
-      nested_distribution: The nest of distributions whose parameter tensors to
-        extract.
+        nested_distribution (nested tf.distribution.Distribution):
+            The distributions whose parameter tensors to extract.
     Returns:
-      A nest of distribution parameters. Each leaf is a dict corresponding to
+        A nest of distribution parameters. Each leaf is a dict corresponding to
         one distribution, with keys as parameter name and values as tensors
         containing parameter values.
     """
@@ -88,8 +128,8 @@ def reset_state_if_necessary(state, initial_state, reset_mask):
     """Reset state to initial state according to reset_mask
     
     Args:
-      state: nested Tensor
-      initial_state: nested Tensor
+      state (nested Tensor): the current batched states
+      initial_state (nested Tensor): batched intitial states
       reset_mask: nested Tensor with shape=(batch_size,), dtype=tf.bool
     Returns:
       nested Tensor
