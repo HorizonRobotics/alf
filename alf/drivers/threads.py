b214bfc5ea66b82e64e6f579582a328077d0ea16
Le Horizon <le.zhao+git@horizon.ai>
2019-08-26 10:17:23 -0700

    missed one: while_loop return list instead of tuple
    (cherry picked from commit d360dd37da83a7412dbe53c003c020440e3c2d34)
    (cherry picked from commit c51016eaaad3288586be1251c30d3f4694e84aa5)

--- alf/drivers/threads.py
+++ alf/drivers/threads.py
@@ -304,7 +304,7 @@ class ActorThread(Thread):
 
         self._enqueue_actions(policy_step, action_dist_param, i,
                               action_return_queue)
-        return i + 1, env_ids, policy_step, action_dist_param
+        return [i + 1, env_ids, policy_step, action_dist_param]
 
     def _step(self, algorithm, time_step, state):
         policy_step = common.algorithm_step(
@@ -414,7 +414,7 @@ class EnvThread(Thread):
                 act_dist_param=act_dist_param,
                 next_time_step=next_time_step,
                 env_id=()))
-        return next_time_step, policy_step.state
+        return [next_time_step, policy_step.state]
 
     def _unroll_env(self, time_step, policy_state, unroll_length):
         time_step, policy_state = tf.while_loop(
