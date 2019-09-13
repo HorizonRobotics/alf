03c78b9a6083f1c3fc40aa139bf19a5d9f8de71f
haonan.yu <haonanu@gmail.com>
2019-09-13 16:17:00 -0700

    remove imports

--- alf/trainers/on_policy_trainer.py
+++ alf/trainers/on_policy_trainer.py
@@ -11,8 +11,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import time
-from absl import logging
 
 import gin.tf
 from alf.drivers.on_policy_driver import OnPolicyDriver
