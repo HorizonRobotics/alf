fd3f2ae423768f753b30131768dddfa07e2be65b
haonan.yu <haonanu@gmail.com>
2019-09-13 13:43:09 -0700

    fix throughput for on-policy training

--- alf/trainers/policy_trainer.py
+++ alf/trainers/policy_trainer.py
@@ -25,6 +25,7 @@ from tf_agents.eval import metric_utils
 from tf_agents.utils import common as tfa_common
 
 from alf.drivers.on_policy_driver import OnPolicyDriver
+from alf.trainers.off_policy_trainer import OffPolicyTrainer
 from alf.utils.metric_utils import eager_compute
 from tf_agents.metrics import tf_metrics
 from alf.utils import common
@@ -274,10 +275,13 @@ class Trainer(object):
                 policy_state=policy_state,
                 time_step=time_step)
             t = time.time() - t0
-            logging.info(
-                '%s time=%.3f throughput=%0.2f' %
-                (iter_num, t,
-                 int(steps) * self._config.num_updates_per_train_step / t))
+            # only off-policy trainers will update several times
+            if issubclass(self._config._trainer, OffPolicyTrainer):
+                num_updates = self._config.num_updates_per_train_step
+            else:  # on-policy trainer only updates on a batch once
+                num_updates = 1
+            logging.info('%s time=%.3f throughput=%0.2f' %
+                         (iter_num, t, int(steps) * num_updates / t))
             tf.summary.scalar("time/train_iter", t)
             if (iter_num + 1) % self._checkpoint_interval == 0:
                 self._save_checkpoint()
