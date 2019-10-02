64fcca697f53c7dc7b6b8d543ab66a7731c9d613
haonan.yu <haonanu@gmail.com>
2019-10-01 17:56:35 -0700

    add env wrappers options for all suites

--- alf/environments/suite_mario.py
+++ alf/environments/suite_mario.py
@@ -49,6 +49,7 @@ def load(game,
          data_format='channels_last',
          record=False,
          crop=True,
+         gym_env_wrappers=(),
          env_wrappers=(),
          max_episode_steps=4500,
          spec_dtype_map=None):
@@ -66,6 +67,7 @@ def load(game,
                `False` for not record otherwise record to current working directory or
                specified director
         crop: whether to crop frame to fixed size
+        gym_env_wrappers: list of gym env wrappers
         env_wrappers: list of tf_agents env wrappers
         max_episode_steps: max episode step limit
         spec_dtype_map: A dict that maps gym specs to tf dtypes to use as the
@@ -99,7 +101,7 @@ def load(game,
             env,
             discount=discount,
             max_episode_steps=max_episode_steps,
-            gym_env_wrappers=(),
+            gym_env_wrappers=gym_env_wrappers,
             env_wrappers=env_wrappers,
             spec_dtype_map=spec_dtype_map,
             auto_reset=True)
