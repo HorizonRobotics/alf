HorizonRobotics/alf

commit 65d12a1c1fab272bf844cf5fda42188230a8fd2a

PR refactor off_policy_algorithm train (#229)
* move off_policy train, exp_observers, and exp_replayer from driver to algorithm

* address code review feedback

* address 2nd round code review feedback

* address 3rd round code review feedback

alf/algorithms/rl_algorithm.py
@@ -14,6 +14,8 @@
 """Base class for RL algorithms."""
 
 from abc import abstractmethod
+import os
+import psutil
 from typing import Callable
 
 import tensorflow as tf
@@ -23,6 +25,10 @@ from tf_agents.trajectories.time_step import StepType
 
 from alf.algorithms.algorithm import Algorithm
 from alf.utils.common import ActionTimeStep, namedtuple, LossInfo, make_action_time_step
+from tf_agents.utils import eager_utils
+from tf_agents.metrics import tf_metrics
+
+import alf.utils
 
 TrainingInfo = namedtuple("TrainingInfo", [
     "action_distribution", "action", "step_type", "reward", "discount", "info",
@@ -78,6 +84,8 @@ class RLAlgorithm(Algorithm):
                  reward_shaping_fn: Callable = None,
                  observation_transformer: Callable = None,
                  debug_summaries=False,
+                 summarize_grads_and_vars=False,
+                 summarize_action_distributions=False,
                  name="RLAlgorithm"):
         """Create a RLAlgorithm.
 
@@ -97,6 +105,10 @@ class RLAlgorithm(Algorithm):
                 `time_step.observation`
             debug_summaries (bool): True if debug summaries should be created.
             name (str): Name of this algorithm.
+            summarize_grads_and_vars (bool): If True, gradient and network
+                variable summaries will be written during training.
+            summarize_action_distributions (bool): If True, generate summaris
+                for the action distributions.
         """
         super(RLAlgorithm, self).__init__(
             train_state_spec=train_state_spec,
@@ -112,6 +124,11 @@ class RLAlgorithm(Algorithm):
         self._action_distribution_spec = action_distribution_spec
         self._reward_shaping_fn = reward_shaping_fn
         self._observation_transformer = observation_transformer
+        self._exp_observers = []
+        self._proc = psutil.Process(os.getpid())
+        self._debug_summaries = debug_summaries
+        self._summarize_grads_and_vars = summarize_grads_and_vars
+        self._summarize_action_distributions = summarize_action_distributions
         self._use_rollout_state = False
 
     @property
@@ -132,10 +149,71 @@ class RLAlgorithm(Algorithm):
         """Return the action distribution spec for the action distributions."""
         return self._action_distribution_spec
 
+    @property
+    def exp_observers(self):
+        """Return experience observers."""
+        return self._exp_observers
+
+    def set_metrics(self, metrics=[]):
+        """Set metrics.
+
+        metrics (list[TFStepMetric]): An optional list of metrics
+            len(metrics) >= 2 as required by calling "self._metrics[:2]" in training_summary()
+        """
+        self._metrics = metrics
+
+    def set_summary_settings(self,
+                             summarize_grads_and_vars=False,
+                             summarize_action_distributions=False):
+        """Set summary flags."""
+        self._summarize_grads_and_vars = summarize_grads_and_vars
+        self._summarize_action_distributions = summarize_action_distributions
+
     def add_reward_summary(self, name, rewards):
         if self._debug_summaries:
             tf.summary.histogram(name + "/value", rewards)
             tf.summary.scalar(name + "/mean", tf.reduce_mean(rewards))
+
+    def add_experience_observer(self, observer: Callable):
+        """Add an observer to receive experience.
+
+        Args:
+            observer (Callable): callable which accept Experience as argument.
+        """
+        self._exp_observers.append(observer)
+
+    def training_summary(self, training_info, loss_info, grads_and_vars):
+        """Generate summaries for training & loss info."""
+
+        if self._summarize_grads_and_vars:
+            alf.utils.summary_utils.add_variables_summaries(grads_and_vars)
+            alf.utils.summary_utils.add_gradients_summaries(grads_and_vars)
+        if self._debug_summaries:
+            alf.utils.common.add_action_summaries(training_info.action,
+                                                  self._action_spec)
+            alf.utils.common.add_loss_summaries(loss_info)
+
+        if self._summarize_action_distributions:
+            alf.utils.summary_utils.summarize_action_dist(
+                training_info.action_distribution, self._action_spec)
+            if training_info.collect_action_distribution:
+                alf.utils.summary_utils.summarize_action_dist(
+                    action_distributions=training_info.
+                    collect_action_distribution,
+                    action_specs=self._action_spec,
+                    name="collect_action_dist")
+
+        if self._metrics:
+            for metric in self._metrics:
+                metric.tf_summaries(step_metrics=self._metrics[:2])
+
+        mem = tf.py_function(
+            lambda: self._proc.memory_info().rss // 1e6, [],
+            tf.float32,
+            name='memory_usage')
+        if not tf.executing_eagerly():
+            mem.set_shape(())
+        tf.summary.scalar(name='memory_usage', data=mem)
 
     def greedy_predict(self, time_step: ActionTimeStep, state=None, eps=0.1):
         """Predict for one step of observation.

alf/trainers/policy_trainer.py
@@ -227,6 +227,8 @@ class Trainer(object):
 
         self._algorithm = self._algorithm_ctor(
             debug_summaries=self._debug_summaries)
+        self._algorithm.set_summary_settings(
+            summarize_grads_and_vars=self._summarize_grads_and_vars)
         self._algorithm.use_rollout_state = self._config.use_rollout_state
 
         self._driver = self.init_driver()

alf/algorithms/merlin_algorithm.py
@@ -445,7 +445,8 @@ def create_merlin_algorithm(env,
                             latent_dim=3,
                             lstm_size=(4, ),
                             memory_size=20,
-                            learning_rate=1e-1):
+                            learning_rate=1e-1,
+                            debug_summaries=True):
     """Create a simple MerlinAlgorithm
 
     Args:
@@ -483,6 +484,6 @@ def create_merlin_algorithm(env,
         lstm_size=lstm_size,
         memory_size=memory_size,
         optimizer=optimizer,
-        debug_summaries=True)
+        debug_summaries=debug_summaries)
 
     return algorithm

alf/algorithms/merlin_algorithm_test.py
@@ -40,13 +40,9 @@ class MerlinAlgorithmTest(tf.test.TestCase):
             batch_size, steps_per_episode, gap, obs_dim=3)
         env = TFPyEnvironment(env)
 
-        algorithm = create_merlin_algorithm(env, learning_rate=1e-3)
-        driver = OnPolicyDriver(
-            env,
-            algorithm,
-            train_interval=6,
-            debug_summaries=False,
-            summarize_grads_and_vars=False)
+        algorithm = create_merlin_algorithm(
+            env, learning_rate=1e-3, debug_summaries=False)
+        driver = OnPolicyDriver(env, algorithm, train_interval=6)
 
         eval_driver = OnPolicyDriver(env, algorithm, training=False)
 

alf/algorithms/off_policy_algorithm.py
@@ -15,13 +15,21 @@
 
 import abc
 from collections import namedtuple
+from typing import Callable
 
 import tensorflow as tf
+import tensorflow_probability as tfp
 
 from tf_agents.trajectories.policy_step import PolicyStep
 from tf_agents.trajectories.time_step import StepType
+from tf_agents.specs.distribution_spec import DistributionSpec
+from tf_agents.specs.distribution_spec import nested_distributions_from_specs
 
 from alf.algorithms.rl_algorithm import ActionTimeStep, RLAlgorithm
+from alf.algorithms.rl_algorithm import make_training_info
+from alf.experience_replayers.experience_replay import OnetimeExperienceReplayer
+from alf.experience_replayers.experience_replay import SyncUniformExperienceReplayer
+from alf.utils import common
 
 Experience = namedtuple("Experience", [
     'step_type', 'reward', 'discount', 'observation', 'prev_action', 'action',
@@ -80,6 +88,11 @@ class OffPolicyAlgorithm(RLAlgorithm):
                 train_complete(tape, batched_training_info,...)
     ```
     """
+
+    @property
+    def exp_replayer(self):
+        """Return experience replayer."""
+        return self._exp_replayer
 
     def predict(self, time_step: ActionTimeStep, state=None):
         """Default implementation of predict.
@@ -206,3 +219,289 @@ class OffPolicyAlgorithm(RLAlgorithm):
             processed experience
         """
         return experience
+
+    def set_exp_replayer(self, exp_replayer: str):
+        """Set experience replayer."""
+
+        if exp_replayer == "one_time":
+            self._exp_replayer = OnetimeExperienceReplayer()
+        elif exp_replayer == "uniform":
+            self._exp_replayer = SyncUniformExperienceReplayer(
+                self._experience_spec, self._env_batch_size)
+        else:
+            raise ValueError("invalid experience replayer name")
+        self.add_experience_observer(self._exp_replayer.observe)
+
+    def observe(self, exp: Experience):
+        """An algorithm can override to manipulate experience."""
+        for observer in self._exp_observers:
+            observer(exp)
+
+    def prepare_off_policy_specs(self, time_step: ActionTimeStep):
+        """Prepare various tensor specs for off_policy training.
+
+        prepare_off_policy_specs is called by OffPolicyDriver._prepare_spec().
+
+        """
+
+        def extract_spec(nest):
+            return tf.nest.map_structure(
+                lambda t: tf.TensorSpec(t.shape[1:], t.dtype), nest)
+
+        self._env_batch_size = time_step.step_type.shape[0]
+        self._time_step_spec = extract_spec(time_step)
+        initial_state = common.get_initial_policy_state(
+            self._env_batch_size, self.train_state_spec)
+        policy_step = self.rollout(time_step, initial_state)
+        info_spec = extract_spec(policy_step.info)
+
+        def _to_distribution_spec(spec):
+            if isinstance(spec, tf.TensorSpec):
+                return DistributionSpec(
+                    tfp.distributions.Deterministic,
+                    input_params_spec={"loc": spec},
+                    sample_spec=spec)
+            return spec
+
+        self._action_distribution_spec = tf.nest.map_structure(
+            _to_distribution_spec, self.action_distribution_spec)
+        self._action_dist_param_spec = tf.nest.map_structure(
+            lambda spec: spec.input_params_spec,
+            self._action_distribution_spec)
+
+        self._experience_spec = Experience(
+            step_type=self._time_step_spec.step_type,
+            reward=self._time_step_spec.reward,
+            discount=self._time_step_spec.discount,
+            observation=self._time_step_spec.observation,
+            prev_action=self._action_spec,
+            action=self._action_spec,
+            info=info_spec,
+            action_distribution=self._action_dist_param_spec,
+            state=self.train_state_spec if self._use_rollout_state else ())
+
+        action_dist_params = common.zero_tensor_from_nested_spec(
+            self._experience_spec.action_distribution, self._env_batch_size)
+        action_dist = nested_distributions_from_specs(
+            self._action_distribution_spec, action_dist_params)
+
+        exp = Experience(
+            step_type=time_step.step_type,
+            reward=time_step.reward,
+            discount=time_step.discount,
+            observation=time_step.observation,
+            prev_action=time_step.prev_action,
+            action=time_step.prev_action,
+            info=policy_step.info,
+            action_distribution=action_dist,
+            state=initial_state if self._use_rollout_state else ())
+
+        processed_exp = self.preprocess_experience(exp)
+        self._processed_experience_spec = self._experience_spec._replace(
+            info=extract_spec(processed_exp.info))
+
+        policy_step = common.algorithm_step(
+            algorithm_step_func=self.train_step,
+            time_step=exp,
+            state=initial_state)
+        info_spec = extract_spec(policy_step.info)
+        self._training_info_spec = make_training_info(
+            action=self._action_spec,
+            action_distribution=self._action_dist_param_spec,
+            step_type=self._time_step_spec.step_type,
+            reward=self._time_step_spec.reward,
+            discount=self._time_step_spec.discount,
+            info=info_spec,
+            collect_info=self._processed_experience_spec.info,
+            collect_action_distribution=self._action_dist_param_spec)
+
+    def train(self,
+              num_updates=1,
+              mini_batch_size=None,
+              mini_batch_length=None,
+              clear_replay_buffer=True):
+        """Train algorithm.
+
+        Args:
+            num_updates (int): number of optimization steps
+            mini_batch_size (int): number of sequences for each minibatch
+            mini_batch_length (int): the length of the sequence for each
+                sample in the minibatch
+            clear_replay_buffer (bool): whether use all data in replay buffer to
+                perform one update and then wiped clean
+
+        Returns:
+            train_steps (int): the actual number of time steps that have been
+                trained (a step might be trained multiple times)
+        """
+
+        if mini_batch_size is None:
+            mini_batch_size = self._exp_replayer.batch_size
+        if clear_replay_buffer:
+            experience = self._exp_replayer.replay_all()
+            self._exp_replayer.clear()
+        else:
+            experience, _ = self._exp_replayer.replay(
+                sample_batch_size=mini_batch_size,
+                mini_batch_length=mini_batch_length)
+
+        return self._train(experience, num_updates, mini_batch_size,
+                           mini_batch_length)
+
+    @tf.function
+    def _train(self, experience, num_updates, mini_batch_size,
+               mini_batch_length):
+        """Train using experience."""
+
+        experience = self.transform_timestep(experience)
+        experience = self.preprocess_experience(experience)
+
+        length = experience.step_type.shape[1]
+        mini_batch_length = (mini_batch_length or length)
+        assert length % mini_batch_length == 0, (
+            "length=%s not a multiple of mini_batch_length=%s" %
+            (length, mini_batch_length))
+
+        if len(tf.nest.flatten(
+                self.train_state_spec)) > 0 and not self._use_rollout_state:
+            if mini_batch_length == 1:
+                logging.fatal(
+                    "Should use TrainerConfig.use_rollout_state=True "
+                    "for off-policy training of RNN when minibatch_length==1.")
+            else:
+                warning_once(
+                    "Consider using TrainerConfig.use_rollout_state=True "
+                    "for off-policy training of RNN.")
+
+        experience = tf.nest.map_structure(
+            lambda x: tf.reshape(
+                x, common.concat_shape([-1, mini_batch_length],
+                                       tf.shape(x)[2:])), experience)
+
+        batch_size = tf.shape(experience.step_type)[0]
+        mini_batch_size = (mini_batch_size or batch_size)
+
+        def _make_time_major(nest):
+            """Put the time dim to axis=0."""
+            return tf.nest.map_structure(lambda x: common.transpose2(x, 0, 1),
+                                         nest)
+
+        for u in tf.range(num_updates):
+            if mini_batch_size < batch_size:
+                indices = tf.random.shuffle(
+                    tf.range(tf.shape(experience.step_type)[0]))
+                experience = tf.nest.map_structure(
+                    lambda x: tf.gather(x, indices), experience)
+            for b in tf.range(0, batch_size, mini_batch_size):
+                batch = tf.nest.map_structure(
+                    lambda x: x[b:tf.minimum(batch_size, b + mini_batch_size)],
+                    experience)
+                batch = _make_time_major(batch)
+                is_last_mini_batch = tf.logical_and(
+                    tf.equal(u, num_updates - 1),
+                    tf.greater_equal(b + mini_batch_size, batch_size))
+                common.enable_summary(is_last_mini_batch)
+                training_info, loss_info, grads_and_vars = self._update(
+                    batch,
+                    weight=tf.cast(tf.shape(batch.step_type)[1], tf.float32) /
+                    float(mini_batch_size))
+                if is_last_mini_batch:
+                    self.training_summary(training_info, loss_info,
+                                          grads_and_vars)
+
+        common.get_global_counter().assign_add(1)
+        train_steps = batch_size * mini_batch_length * num_updates
+        return train_steps
+
+    def _update(self, experience, weight):
+        batch_size = tf.shape(experience.step_type)[1]
+        counter = tf.zeros((), tf.int32)
+        initial_train_state = common.get_initial_policy_state(
+            batch_size, self.train_state_spec)
+        if self._use_rollout_state:
+            first_train_state = tf.nest.map_structure(
+                lambda state: state[0, ...], experience.state)
+        else:
+            first_train_state = initial_train_state
+        num_steps = tf.shape(experience.step_type)[0]
+
+        def create_ta(s):
+            # TensorArray cannot use Tensor (batch_size) as element_shape
+            ta_batch_size = experience.step_type.shape[1]
+            return tf.TensorArray(
+                dtype=s.dtype,
+                size=num_steps,
+                element_shape=tf.TensorShape([ta_batch_size]).concatenate(
+                    s.shape))
+
+        experience_ta = tf.nest.map_structure(create_ta,
+                                              self._processed_experience_spec)
+        experience_ta = tf.nest.map_structure(
+            lambda elem, ta: ta.unstack(elem), experience, experience_ta)
+        training_info_ta = tf.nest.map_structure(create_ta,
+                                                 self._training_info_spec)
+
+        def _train_loop_body(counter, policy_state, training_info_ta):
+            exp = tf.nest.map_structure(lambda ta: ta.read(counter),
+                                        experience_ta)
+            collect_action_distribution_param = exp.action_distribution
+            collect_action_distribution = nested_distributions_from_specs(
+                self._action_distribution_spec,
+                collect_action_distribution_param)
+            exp = exp._replace(action_distribution=collect_action_distribution)
+
+            policy_state = common.reset_state_if_necessary(
+                policy_state, initial_train_state,
+                tf.equal(exp.step_type, StepType.FIRST))
+
+            policy_step = common.algorithm_step(self.train_step, exp,
+                                                policy_state)
+
+            action_dist_param = common.get_distribution_params(
+                policy_step.action)
+
+            training_info = make_training_info(
+                action=exp.action,
+                action_distribution=action_dist_param,
+                reward=exp.reward,
+                discount=exp.discount,
+                step_type=exp.step_type,
+                info=policy_step.info,
+                collect_info=exp.info,
+                collect_action_distribution=collect_action_distribution_param)
+
+            training_info_ta = tf.nest.map_structure(
+                lambda ta, x: ta.write(counter, x), training_info_ta,
+                training_info)
+
+            counter += 1
+
+            return [counter, policy_step.state, training_info_ta]
+
+        with tf.GradientTape(
+                persistent=True, watch_accessed_variables=False) as tape:
+            tape.watch(self.trainable_variables)
+            [_, _, training_info_ta] = tf.while_loop(
+                cond=lambda counter, *_: tf.less(counter, num_steps),
+                body=_train_loop_body,
+                loop_vars=[counter, first_train_state, training_info_ta],
+                back_prop=True,
+                name="train_loop")
+            training_info = tf.nest.map_structure(lambda ta: ta.stack(),
+                                                  training_info_ta)
+            action_distribution = nested_distributions_from_specs(
+                self._action_distribution_spec,
+                training_info.action_distribution)
+            collect_action_distribution = nested_distributions_from_specs(
+                self._action_distribution_spec,
+                training_info.collect_action_distribution)
+            training_info = training_info._replace(
+                action_distribution=action_distribution,
+                collect_action_distribution=collect_action_distribution)
+
+        loss_info, grads_and_vars = self.train_complete(
+            tape=tape, training_info=training_info, weight=weight)
+
+        del tape
+
+        return training_info, loss_info, grads_and_vars

alf/algorithms/ppo_algorithm_test.py
@@ -84,12 +84,8 @@ class PpoTest(tf.test.TestCase):
         eval_env = TFPyEnvironment(eval_env)
 
         algorithm = create_algorithm(env, learning_rate=learning_rate)
-        driver = SyncOffPolicyDriver(
-            env,
-            algorithm,
-            debug_summaries=DEBUGGING,
-            summarize_grads_and_vars=DEBUGGING)
-        replayer = driver.exp_replayer
+        algorithm.set_summary_settings(summarize_grads_and_vars=DEBUGGING)
+        driver = SyncOffPolicyDriver(env, algorithm)
         eval_driver = OnPolicyDriver(
             eval_env, algorithm, training=False, greedy_predict=True)
 
@@ -103,9 +99,7 @@ class PpoTest(tf.test.TestCase):
                 time_step=time_step,
                 policy_state=policy_state)
 
-            experience = replayer.replay_all()
-            driver.train(experience, num_updates=4, mini_batch_size=25)
-            replayer.clear()
+            algorithm.train(num_updates=4, mini_batch_size=25)
             eval_env.reset()
             eval_time_step, _ = eval_driver.run(
                 max_num_steps=(steps_per_episode - 1) * batch_size)

alf/drivers/async_off_policy_driver.py
@@ -54,10 +54,7 @@ class AsyncOffPolicyDriver(OffPolicyDriver):
                  observers=[],
                  use_rollout_state=False,
                  metrics=[],
-                 exp_replayer="one_time",
-                 debug_summaries=False,
-                 summarize_grads_and_vars=False,
-                 train_step_counter=None):
+                 exp_replayer="one_time"):
         """
         Args:
             envs (list[TFEnvironment]):  list of TFEnvironment
@@ -81,13 +78,6 @@ class AsyncOffPolicyDriver(OffPolicyDriver):
             metrics (list[TFStepMetric]): An optiotional list of metrics.
             exp_replayer (str): a string that indicates which ExperienceReplayer
                 to use.
-            debug_summaries (bool): A bool to gather debug summaries.
-            summarize_grads_and_vars (bool): If True, gradient and network
-                variable summaries will be written during training.
-            train_step_counter (tf.Variable): An optional counter to increment
-                every time the a new iteration is started. If None, it will use
-                tf.summary.experimental.get_step(). If this is still None, a
-                counter will be created.
         """
         super(AsyncOffPolicyDriver, self).__init__(
             env=envs[0],
@@ -95,10 +85,7 @@ class AsyncOffPolicyDriver(OffPolicyDriver):
             exp_replayer=exp_replayer,
             observers=observers,
             use_rollout_state=use_rollout_state,
-            metrics=metrics,
-            debug_summaries=debug_summaries,
-            summarize_grads_and_vars=summarize_grads_and_vars,
-            train_step_counter=train_step_counter)
+            metrics=metrics)
 
         # create threads
         self._coord = tf.train.Coordinator()
@@ -141,6 +128,7 @@ class AsyncOffPolicyDriver(OffPolicyDriver):
             coord=self._coord,
             queue=self._tfq.log_queue)
         self._threads = actor_threads + env_threads + [self._log_thread]
+        algorithm.set_metrics(self.get_metrics())
 
     def get_step_metrics(self):
         """See PolicyDriver.get_step_metrics()"""
@@ -194,7 +182,7 @@ class AsyncOffPolicyDriver(OffPolicyDriver):
             steps (int): the total number of unrolled steps
         """
         exp, env_id, steps = self.get_training_exps()
-        for ob in self._exp_observers:
+        for ob in self._algorithm.exp_observers:
             ob(exp, env_id)
         return steps
 

commit 088d16028e64bd1b32001054ce7f44839e6ffc30

Fix summary
These are bugs introduced in PR #229
1. addded back metric against train step
2. make it possible to configure summarize_action_distributions

alf/algorithms/rl_algorithm.py
@@ -24,6 +24,7 @@ import tensorflow_probability as tfp
 from tf_agents.trajectories.time_step import StepType
 
 from alf.algorithms.algorithm import Algorithm
+import alf.utils.common as common
 from alf.utils.common import ActionTimeStep, namedtuple, LossInfo, make_action_time_step
 from tf_agents.utils import eager_utils
 from tf_agents.metrics import tf_metrics
@@ -205,7 +206,9 @@ class RLAlgorithm(Algorithm):
 
         if self._metrics:
             for metric in self._metrics:
-                metric.tf_summaries(step_metrics=self._metrics[:2])
+                metric.tf_summaries(
+                    train_step=common.get_global_counter(),
+                    step_metrics=self._metrics[:2])
 
         mem = tf.py_function(
             lambda: self._proc.memory_info().rss // 1e6, [],

alf/trainers/policy_trainer.py
@@ -67,6 +67,7 @@ class TrainerConfig(object):
                  summary_max_queue=10,
                  debug_summaries=False,
                  summarize_grads_and_vars=False,
+                 summarize_action_distributions=False,
                  num_steps_per_iter=10000,
                  initial_collect_steps=0,
                  num_updates_per_train_step=4,
@@ -138,6 +139,7 @@ class TrainerConfig(object):
             summary_max_queue=summary_max_queue,
             debug_summaries=debug_summaries,
             summarize_grads_and_vars=summarize_grads_and_vars,
+            summarize_action_distributions=summarize_action_distributions,
             num_steps_per_iter=num_steps_per_iter,
             initial_collect_steps=initial_collect_steps,
             num_updates_per_train_step=num_updates_per_train_step,
@@ -228,7 +230,9 @@ class Trainer(object):
         self._algorithm = self._algorithm_ctor(
             debug_summaries=self._debug_summaries)
         self._algorithm.set_summary_settings(
-            summarize_grads_and_vars=self._summarize_grads_and_vars)
+            summarize_grads_and_vars=self._summarize_grads_and_vars,
+            summarize_action_distributions=self._config.
+            summarize_action_distributions)
         self._algorithm.use_rollout_state = self._config.use_rollout_state
 
         self._driver = self.init_driver()