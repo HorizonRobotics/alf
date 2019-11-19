4a533d26771d52d87ee883b7a6aeeefa877a551f
Le Horizon <le.zhao+git@horizon.ai>
2019-11-19 14:41:24 -0800

    attention as preprocessing layer can run, but due to tensorflow/keras problem, the two conv2d layers inside the Lambda layer is becoming mangled up.
    (cherry picked from commit 959bfd6bbbc1c837c12f9d3ed3bb312df1a8a7a1)

--- alf/networks/target_navigation.py
+++ alf/networks/target_navigation.py
@@ -15,20 +15,91 @@
 
 import collections
 import gin
+import numpy as np
 import tensorflow as tf
 import tf_agents
 from tf_agents.networks.actor_distribution_rnn_network import ActorDistributionRnnNetwork
 from tf_agents.networks.value_rnn_network import ValueRnnNetwork
 
 from alf.utils import common
-from alf.layers import get_identity_layer
+from alf.layers import get_identity_layer, get_first_element_layer
 
 
+def attend_layer(image_layers, input):
+    """
+    Generate the attention layer output
+
+    Args:
+        query: sentence input tensor of shape (batch, seq_length -- 20 from
+            PlayGround.vocab_sequence_length), which will be transformed
+            into an embedding of shape (batch, seq_length, num_embedding_dim), the same
+            dimensionality as that of position embedding (value).
+        key: the output of the conv_layers, which will be flattened into
+            tensor of shape (batch, h * w, channels).
+    """
+    assert isinstance(input, collections.OrderedDict)
+    assert 'sentence' in input, 'attention implies using sentence input'
+    query = input['sentence']
+    image_shape = input['image'].shape
+    key = input['image']
+    print(key.shape)
+    key1 = tf.keras.layers.Conv2D(16, kernel_size=3, strides=2)(key)
+    print(key1.shape)
+    key2 = tf.keras.layers.Conv2D(32, kernel_size=3, strides=2)(key)
+    print(key2.shape)
+    for layer in image_layers:
+        key = layer(key)
+    # reshape [batches, height, width, channels] tensor
+    # into [batches, height * width, channels] tensor
+    # Assumes channels_last.
+    (b, h, w, c) = key.shape
+    flatten_shape = (b, h * w, c)
+    key_embeddings = tf.reshape(key, flatten_shape)
+
+    # create position input tensor
+    # np.asarray is to avoid ValueError: Argument must be a dense tensor: range(0, 400) - got shape [400], but wanted [].
+    p = tf.constant(np.asarray(range(h * w)))
+    p = tf.reshape(p, (h * w, 1))
+    p = tf.keras.backend.repeat(p, n=b)
+    p = tf.transpose(p)
+    p = tf.reshape(p, flatten_shape[0:-1])
+
+    # create position embedding (value) tensor
+    pos_embeddings = tf.keras.layers.Embedding(flatten_shape[1], c)(
+        p)  # p: b, h * w; output: b, h * w, c
+
+    # inner product (Luong style) attention
+
+    # c will be the number of embedding dimensions for the sentence input,
+    # and num_embedding_dims parameter is ignored.  This is because we do
+    # inner product of image and sentence embedding vectors to compute attention.
+    query_embeddings = tf.keras.layers.Embedding(query.shape[1],
+                                                 c)(query)  # seq_len, c
+
+    # generates the position attention of shape (batch, h*w, c)
+    pos_attention_seq = tf.keras.layers.Attention()(
+        [query_embeddings, pos_embeddings, key_embeddings])
+
+    img_attention_seq = tf.keras.layers.Attention()(
+        [query_embeddings, key_embeddings])
+
+    query_encoding = tf.keras.layers.GlobalAveragePooling1D()(query_embeddings)
+    pos_attention = tf.keras.layers.GlobalAveragePooling1D()(pos_attention_seq)
+    img_attention = tf.keras.layers.GlobalAveragePooling1D()(img_attention_seq)
+    return tf.keras.layers.Concatenate()(
+        [query_encoding, pos_attention, img_attention])
+
+
+@gin.configurable
 def get_ac_networks(conv_layer_params=None,
+                    attention=False,
+                    activation_fn=tf.keras.activations.softsign,
+                    kernel_initializer=tf.keras.initializers.GlorotUniform(),
                     num_embedding_dims=None,
                     fc_layer_params=None,
                     num_state_tiles=None,
-                    num_sentence_tiles=None):
+                    num_sentence_tiles=None,
+                    name=None):
     """
     Generate the actor and value networks
 
@@ -36,6 +107,8 @@ def get_ac_networks(conv_layer_params=None,
         conv_layer_params (list[int 3 tuple]): optional convolution layers
             parameters, where each item is a length-three tuple indicating
             (filters, kernel_size, stride).
+        attention (bool): if true, use attention after conv layer.
+            Assumes that sentence is part of the input dictionary.
         num_embedding_dims (int): optional number of dimensions of the
             vocabulary embedding space.
         fc_layer_params (list[int]): optional fully_connected parameters, where
@@ -55,24 +128,6 @@ def get_ac_networks(conv_layer_params=None,
     observation_spec = common.get_observation_spec()
     action_spec = common.get_action_spec()
 
-    conv_layers = tf.keras.Sequential(
-        tf_agents.networks.utils.mlp_layers(
-            conv_layer_params=conv_layer_params))
-
-    preprocessing_layers = {
-        'image': conv_layers,
-    }
-    if common.get_states_shape():
-        state_layers = get_identity_layer()
-        # [image: (1, 12800), sentence: (1, 16 * 800), states: (1, 16 * 800)]
-        # Here, we tile along the last dimension of the input.
-        if num_state_tiles:
-            state_layers = tf.keras.Sequential([
-                tf.keras.layers.Lambda(lambda x: tf.tile(
-                    x, multiples=[1, num_state_tiles]))
-            ])
-        preprocessing_layers['states'] = state_layers
-
     vocab_size = common.get_vocab_size()
     if vocab_size:
         sentence_layers = tf.keras.Sequential([
@@ -83,9 +138,72 @@ def get_ac_networks(conv_layer_params=None,
             sentence_layers.add(
                 tf.keras.layers.Lambda(lambda x: tf.tile(
                     x, multiples=[1, num_sentence_tiles])))
+
+    # image:
+    conv_layers = []
+    conv_layers.extend([
+        tf.keras.layers.Conv2D(
+            filters=filters,
+            kernel_size=kernel_size,
+            strides=strides,
+            activation=activation_fn,
+            kernel_initializer=kernel_initializer,
+            name='/'.join([name, 'conv2d']) if name else None)
+        for (filters, kernel_size, strides) in conv_layer_params
+    ])
+
+    if attention:
+        attention_layers = [
+            tf.keras.layers.Lambda(lambda input: attend_layer(
+                conv_layers, input))
+        ]
+        layers = attention_layers
+    else:
+        layers = conv_layers
+
+    layers.append(tf.keras.layers.Flatten())
+    image_layers = tf.keras.Sequential(layers)
+    if attention:
+        attend_layers = image_layers
+
+    preprocessing_layers = {}
+    obs_spec = common.get_observation_spec()
+    if isinstance(obs_spec, dict) and 'image' in obs_spec:
+        preprocessing_layers['image'] = image_layers
+
+    if isinstance(obs_spec, dict) and 'states' in obs_spec:
+        state_layers = get_identity_layer()
+        # [image: (1, 12800), sentence: (1, 16 * 800), states: (1, 16 * 800)]
+        # Here, we tile along the last dimension of the input.
+        if num_state_tiles:
+            state_layers = tf.keras.Sequential([
+                tf.keras.layers.Lambda(lambda x: tf.tile(
+                    x, multiples=[1, num_state_tiles]))
+            ])
+        preprocessing_layers['states'] = state_layers
+
+    if isinstance(obs_spec, dict) and 'sentence' in obs_spec:
         preprocessing_layers['sentence'] = sentence_layers
 
+    # we assume, grouped image-sentence input implies attention
+    # mechanism is used.
+    if isinstance(obs_spec, dict) and 'image_sentence' in obs_spec:
+        preprocessing_layers['image_sentence'] = attend_layers
+
+    # This makes the code work with internal states input alone as well.
+    if not preprocessing_layers:
+        preprocessing_layers = get_identity_layer()
+
     preprocessing_combiner = tf.keras.layers.Concatenate()
+    if not isinstance(preprocessing_layers, dict):
+        preprocessing_combiner = None
+    elif len(preprocessing_layers) <= 1:
+        # attention has to have a combiner, otherwise EncodingNetwork complains
+        # due to flattened input containing two fields: 'image' and 'sentence'.
+        # This cannot be a Concatenate, as input is an array of a single
+        # tensor.  This cannot be identity, as input is a list while Flatten
+        # postprocessing layer expects a tensor as input.
+        preprocessing_combiner = get_first_element_layer()
 
     actor = ActorDistributionRnnNetwork(
         input_tensor_spec=observation_spec,
@@ -105,6 +223,8 @@ def get_ac_networks(conv_layer_params=None,
 
 @gin.configurable
 def get_actor_network(conv_layer_params=None,
+                      activation_fn=tf.keras.activations.softsign,
+                      kernel_initializer=tf.keras.initializers.GlorotUniform(),
                       num_embedding_dims=None,
                       fc_layer_params=None,
                       num_state_tiles=None,
@@ -114,15 +234,20 @@ def get_actor_network(conv_layer_params=None,
     """
     a, _ = get_ac_networks(
         conv_layer_params=conv_layer_params,
+        activation_fn=activation_fn,
+        kernel_initializer=kernel_initializer,
         num_embedding_dims=num_embedding_dims,
         fc_layer_params=fc_layer_params,
         num_state_tiles=num_state_tiles,
-        num_sentence_tiles=num_sentence_tiles)
+        num_sentence_tiles=num_sentence_tiles,
+        name="actor")
     return a
 
 
 @gin.configurable
 def get_value_network(conv_layer_params=None,
+                      activation_fn=tf.keras.activations.softsign,
+                      kernel_initializer=tf.keras.initializers.GlorotUniform(),
                       num_embedding_dims=None,
                       fc_layer_params=None,
                       num_state_tiles=None,
@@ -132,8 +257,11 @@ def get_value_network(conv_layer_params=None,
     """
     _, c = get_ac_networks(
         conv_layer_params=conv_layer_params,
+        activation_fn=activation_fn,
+        kernel_initializer=kernel_initializer,
         num_embedding_dims=num_embedding_dims,
         fc_layer_params=fc_layer_params,
         num_state_tiles=num_state_tiles,
-        num_sentence_tiles=num_sentence_tiles)
+        num_sentence_tiles=num_sentence_tiles,
+        name="value")
     return c
