5827aa8ff8238a365f4698f055e70f697b68ab03
hnyu <51248379+hnyu@users.noreply.github.com>
2019-12-12 10:12:51 -0800

    put data buffer on CPU (#282)
    * put data buffer on CPU
    
    * address comments
    
    * update

--- alf/utils/data_buffer.py
+++ alf/utils/data_buffer.py
@@ -23,17 +23,23 @@ class DataBuffer(tf.Module):
     """A simple circular buffer supporting random sampling.
     """
 
-    def __init__(self, data_spec: tf.TensorSpec, capacity, name="DataBuffer"):
+    def __init__(self,
+                 data_spec: tf.TensorSpec,
+                 capacity,
+                 device='cpu:*',
+                 name="DataBuffer"):
         """Create a DataBuffer.
 
         Args:
             data_spec (nested TensorSpec): spec for the data item (without batch
                 dimension) to be stored.
             capacity (int): capacity of the buffer.
+            device (str): which device to store the data
             name (str): name of the buffer
         """
         super().__init__()
         self._capacity = capacity
+        self._device = device
 
         def _create_buffer(tensor_spec):
             shape = [capacity] + tensor_spec.shape.as_list()
@@ -44,25 +50,26 @@ class DataBuffer(tf.Module):
                 shape=shape,
                 trainable=False)
 
-        self._buffer = tf.nest.map_structure(_create_buffer, data_spec)
-        self._current_size = tfa_common.create_variable(
-            name=name + "/size",
-            initializer=0,
-            dtype=tf.int32,
-            shape=(),
-            trainable=False)
-        self._current_pos = tfa_common.create_variable(
-            name=name + "/pos",
-            initializer=0,
-            dtype=tf.int32,
-            shape=(),
-            trainable=False)
-        # TF 2.0 checkpoint does not handle tuple. We have to convert
-        # _buffer to a flattened list in order to make the checkpointer save the
-        # content in self._buffer. This seems to be fixed in
-        # tf-nightly-gpu 2.1.0.dev20191119
-        # TODO: remove this after upgrading tensorflow
-        self._flattened_buffer = tf.nest.flatten(self._buffer)
+        with tf.device(self._device):
+            self._current_size = tfa_common.create_variable(
+                name=name + "/size",
+                initializer=0,
+                dtype=tf.int32,
+                shape=(),
+                trainable=False)
+            self._current_pos = tfa_common.create_variable(
+                name=name + "/pos",
+                initializer=0,
+                dtype=tf.int32,
+                shape=(),
+                trainable=False)
+            self._buffer = tf.nest.map_structure(_create_buffer, data_spec)
+            # TF 2.0 checkpoint does not handle tuple. We have to convert
+            # _buffer to a flattened list in order to make the checkpointer save the
+            # content in self._buffer. This seems to be fixed in
+            # tf-nightly-gpu 2.1.0.dev20191119
+            # TODO: remove this after upgrading tensorflow
+            self._flattened_buffer = tf.nest.flatten(self._buffer)
 
     @property
     def current_size(self):
@@ -74,19 +81,21 @@ class DataBuffer(tf.Module):
         Args:
             batch (Tensor): shape should be [batch_size] + tensor_space.shape
         """
-        batch_size = get_nest_batch_size(batch, tf.int32)
-        n = tf.minimum(batch_size, self._capacity)
-        indices = tf.range(self._current_pos,
-                           self._current_pos + n) % self._capacity
-        indices = tf.expand_dims(indices, axis=-1)
-        tf.nest.map_structure(
-            lambda buf, bat: buf.scatter_nd_update(indices,
-                                                   tf.stop_gradient(bat[-n:])),
-            self._buffer, batch)
 
-        self._current_pos.assign((self._current_pos + n) % self._capacity)
-        self._current_size.assign(
-            tf.minimum(self._current_size + n, self._capacity))
+        with tf.device(self._device):
+            batch_size = get_nest_batch_size(batch, tf.int32)
+            n = tf.minimum(batch_size, self._capacity)
+            indices = tf.range(self._current_pos,
+                               self._current_pos + n) % self._capacity
+            indices = tf.expand_dims(indices, axis=-1)
+
+            tf.nest.map_structure(
+                lambda buf, bat: buf.scatter_nd_update(
+                    indices, tf.stop_gradient(bat[-n:])), self._buffer, batch)
+
+            self._current_pos.assign((self._current_pos + n) % self._capacity)
+            self._current_size.assign(
+                tf.minimum(self._current_size + n, self._capacity))
 
     def get_batch(self, batch_size):
         """Get batsh_size random samples in the buffer.
@@ -96,12 +105,13 @@ class DataBuffer(tf.Module):
         Returns:
             Tensor of shape [batch_size] + tensor_spec.shape
         """
-        indices = tf.random.uniform(
-            shape=(batch_size, ),
-            dtype=tf.int32,
-            minval=0,
-            maxval=self._current_size)
-        return self.get_batch_by_indices(indices)
+        with tf.device(self._device):
+            indices = tf.random.uniform(
+                shape=(batch_size, ),
+                dtype=tf.int32,
+                minval=0,
+                maxval=self._current_size)
+            return self.get_batch_by_indices(indices)
 
     def get_batch_by_indices(self, indices):
         """Get the samples by indices
@@ -113,10 +123,12 @@ class DataBuffer(tf.Module):
             Tensor of shape [batch_size] + tensor_spec.shape, where batch_size
                 is indices.shape[0]
         """
-        indices = (indices +
-                   (self._current_pos - self._current_size)) % self._capacity
-        return tf.nest.map_structure(
-            lambda buffer: tf.gather(buffer, indices, axis=0), self._buffer)
+        with tf.device(self._device):
+            indices = (indices + (self._current_pos - self._current_size)
+                       ) % self._capacity
+            return tf.nest.map_structure(
+                lambda buffer: tf.gather(buffer, indices, axis=0),
+                self._buffer)
 
     def get_all(self):
         return self._buffer
