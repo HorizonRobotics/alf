edb2d45687659582b77ae3c4c4628272e13acfad
haonan.yu <haonanu@gmail.com>
2019-12-18 14:53:39 -0800

    update adaptive_normalizer

--- alf/utils/adaptive_normalizer.py
+++ alf/utils/adaptive_normalizer.py
@@ -14,23 +14,38 @@
 
 import tensorflow as tf
 
-from tf_agents.utils.tensor_normalizer import EMATensorNormalizer
 from tf_agents.specs.tensor_spec import TensorSpec
+from alf.utils.averager import AdaptiveAverager
 
 
-class AdaptiveNormalizer(EMATensorNormalizer):
+class AdaptiveNormalizer(tf.Module):
     def __init__(self,
                  tensor_spec,
                  speed=2.0,
                  auto_update=True,
-                 variance_epsilon=1e-3):
-        """Create reward normalizer
+                 variance_epsilon=1e-8,
+                 name="AdaptiveNormalizer"):
+        """Create an adaptive normalizer.
 
         This normalizer gives higher weight to more recent samples for
         calculating mean and variance. Roughly speaking, the weight for each
         sample at time t is proportional to (t/T)^(speed-1), where T is the
         current time step. See docs/streaming_averaging_amd_sampling.py for
         detail.
+
+        Given weights w_i and samples x_i, i = 1...n, let
+
+        m   = \sum_i w_i * x_i
+        m2  = \sum_i w_i * x_i^2
+
+        then
+
+        var = \sum_i w_i * (x_i - m)^2
+            = \sum_i w_i * (x_i^2 + m^2 - 2*x_i*m)
+            = m2 + m^2 - 2m^2
+            = m2 - m^2
+
+        which is the same result with the case when w_1=w_2=...=w_n=(1/n)
 
         Args:
             tensor_spec (TensorSpec): spec of the mean of tensors to be
@@ -39,48 +54,57 @@ class AdaptiveNormalizer(EMATensorNormalizer):
             auto_update (bool): If True, automatically update mean and variance
               for each call to `normalize()`. Otherwise, the user needs to call
               `update()`
+            variance_epislon (float): a small value added to std for normalizing
+            name (str):
         """
-        self._total_env_steps = tf.Variable(
-            int(speed), dtype=tf.int64, trainable=False)
-        self._update_ema_rate = tf.Variable(
-            1.0, dtype=tf.float32, trainable=False)
-        self._speed = speed
+        self._mean_averager = AdaptiveAverager(tensor_spec, speed=speed)
+        self._m2_averager = AdaptiveAverager(tensor_spec, speed=speed)
         self._auto_update = auto_update
         self._variance_epsilon = variance_epsilon
-        super(AdaptiveNormalizer, self).__init__(
-            tensor_spec, norm_update_rate=self._update_ema_rate)
 
-    def normalize(self, tensors, clip_value=-1.0):
-        """Normalized the reward
+    def update(self, tensor):
+        self._mean_averager.update(tf.reduce_mean(tensor))
+        self._m2_averager.update(tf.reduce_mean(tf.square(tensor)))
+
+    def normalize(self, tensor, clip_value=-1.0):
+        """
+        Normalize a tensor with mean and variance
 
         Args:
-            tensors (nested Tensor): tensors to be normalized
-            clip_value (float): the normalized values will be clipped to +/-
-                this value. If it's negative, then ignore
+            tensor (tf.Tensor): with shape [B, ] + tensor_spec.shape
+            clip_value (float): if positive, normalized values will be clipped to
+                [-clip_value, clip_value]
+
         Returns:
-            normalized reward. with mean equal to 0 and variance equal to 1
-              over the time.
+            normalized tensor
         """
         if self._auto_update:
-            self.update(tensors)
-        n_tensors = super(AdaptiveNormalizer, self).normalize(
-            tensors,
-            clip_value=clip_value,
-            center_mean=True,
+            self.update(tensor)
+        m = self._mean_averager.get()
+        m2 = self._m2_averager.get()
+        var = m2 - tf.square(m)
+        normalized_tensor = tf.nn.batch_normalization(
+            tensor,
+            m,
+            var,
+            offset=None,
+            scale=None,
             variance_epsilon=self._variance_epsilon)
-        return n_tensors
-
-    def update(self, tensors):
-        self._update_ema_rate.assign(
-            self._speed / tf.cast(self._total_env_steps, tf.float32))
-        self._total_env_steps.assign_add(1)
-        super(AdaptiveNormalizer, self).update(tensors)
+        if clip_value > 0:
+            normalized_tensor = tf.clip_by_value(normalized_tensor,
+                                                 -clip_value, clip_value)
+        return normalized_tensor
 
 
 class ScalarAdaptiveNormalizer(AdaptiveNormalizer):
-    def __init__(self, speed=2.0, auto_update=True, variance_epsilon=1e-10):
+    def __init__(self,
+                 speed=2.0,
+                 auto_update=True,
+                 variance_epsilon=1e-8,
+                 name="ScalarAdaptiveNormalizer"):
         super(ScalarAdaptiveNormalizer, self).__init__(
             tensor_spec=TensorSpec((), dtype=tf.float32),
             speed=speed,
             auto_update=auto_update,
-            variance_epsilon=variance_epsilon)
+            variance_epsilon=variance_epsilon,
+            name=name)
