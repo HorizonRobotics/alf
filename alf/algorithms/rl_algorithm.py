d6f9f8e725b13a06df887baa23613420a92d16e9
emailweixu <emailweixu@gmail.com>
2020-01-16 14:24:12 -0800

    Remove TODO

--- alf/algorithms/rl_algorithm.py
+++ alf/algorithms/rl_algorithm.py
@@ -116,7 +116,8 @@ class RLAlgorithm(Algorithm):
         self._train_step_info_spec = None
         self._processed_experience_spec = None
 
-    def set_children_property(self, property_name, value):
+    def _set_children_property(self, property_name, value):
+        """Set the property named `property_name` in child RLAlgorithm to `value`."""
         children = self._get_children(lambda obj: isinstance(obj, RLAlgorithm))
         for alg in children:
             alg.__setattr__(property_name, value)
@@ -127,9 +128,8 @@ class RLAlgorithm(Algorithm):
 
     @use_rollout_state.setter
     def use_rollout_state(self, flag):
-        # TODO: need to set the flag for the child algorithms.
         self._use_rollout_state = flag
-        self.set_children_property('use_rollout_state', flag)
+        self._set_children_property('use_rollout_state', flag)
 
     def need_full_rollout_state(self):
         """Whether PolicyStep.state from rollout should be full.
