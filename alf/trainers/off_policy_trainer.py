dea42b1ec644ebdb1adbd8ff6e15977acb95c3a1
haonan.yu <haonanu@gmail.com>
2020-02-05 15:13:45 -0800

    fix a bug

--- alf/trainers/off_policy_trainer.py
+++ alf/trainers/off_policy_trainer.py
@@ -88,8 +88,11 @@ class AsyncOffPolicyTrainer(OffPolicyTrainer):
         for i in range(1, self._config.num_envs):
             # [self._random_seed, self._random_seed + batch_size) has been used
             # in policy_trainer.py
-            self._create_environment(
-                random_seed=self._random_seed + i * common._env.batch_size)
+            if self._random_seed is None:
+                self._create_environment()
+            else:
+                self._create_environment(
+                    random_seed=self._random_seed + i * common._env.batch_size)
         driver = AsyncOffPolicyDriver(
             envs=self._envs,
             algorithm=self._algorithm,
