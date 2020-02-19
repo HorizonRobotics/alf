7c802d521f3e2eccc83b503d83e4b8f32d68426c
haonan.yu <haonanu@gmail.com>
2020-02-18 16:31:36 -0800

    TensorSpec

---
+++ alf/tensor_specs.py
@@ -0,0 +1,228 @@
+# Copyright (c) 2020 Horizon Robotics. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""TensorSpec with PyTorch types; adapted from Tensorflow's tensor_spec.py:
+
+https://github.com/tensorflow/tensorflow/blob/r1.8/tensorflow/python/framework/tensor_spec.py
+"""
+
+import numpy as np
+
+import torch
+
+
+def torch_dtype_to_str(dtype):
+    assert isinstance(dtype, torch.dtype)
+    return dtype.__str__()[6:]
+
+
+class TensorSpec(object):
+    """Describes a torch.Tensor.
+    A TensorSpec allows an API to describe the Tensors that it accepts or
+    returns, before that Tensor exists. This allows dynamic and flexible graph
+    construction and configuration.
+    """
+
+    __slots__ = ["_shape", "_dtype"]
+
+    def __init__(self, shape, dtype):
+        """Creates a TensorSpec.
+        Args:
+            shape (tuple[int]): The shape of the tensor.
+            dtype (str or torch.dtype): The type of the tensor values,
+                e.g., "int32" or torch.int32
+        """
+        self._shape = shape
+        if isinstance(dtype, str):
+            self._dtype = getattr(torch, dtype)
+        else:
+            self._dtype = dtype
+
+    @classmethod
+    def from_spec(cls, spec):
+        assert isinstance(spec, TensorSpec)
+        return cls(spec.shape, spec.dtype)
+
+    @classmethod
+    def from_tensor(cls, tensor):
+        assert isinstance(tensor, torch.Tensor)
+        return TensorSpec(tensor.shape, tensor.dtype)
+
+    @classmethod
+    def is_bounded(cls):
+        del cls
+        return False
+
+    @property
+    def shape(self):
+        """Returns the `TensorShape` that represents the shape of the tensor."""
+        return self._shape
+
+    @property
+    def dtype(self):
+        """Returns the `dtype` of elements in the tensor."""
+        return self._dtype
+
+    @property
+    def is_discrete(self):
+        """Whether spec is discrete."""
+        return not self.dtype.is_floating_point
+
+    @property
+    def is_continuous(self):
+        """Whether spec is continuous."""
+        return self.dtype.is_floating_point
+
+    def __repr__(self):
+        return "TensorSpec(shape={}, dtype={})".format(self.shape,
+                                                       repr(self.dtype))
+
+    def __eq__(self, other):
+        return self.shape == other.shape and self.dtype == other.dtype
+
+    def __ne__(self, other):
+        return not self == other
+
+    def __reduce__(self):
+        return TensorSpec, (self._shape, self._dtype)
+
+    def zeros(self, outer_dims=None):
+        """Create a zero tensor from the spec.
+
+        Args:
+            outer_dims (tuple[int]): an optional list of integers specifying outer
+                dimensions to add to the spec shape before sampling.
+
+        Returns:
+            tensor (torch.Tensor): a tensor of `self._dtype`
+        """
+        shape = self._shape
+        if outer_dims is not None:
+            shape = tuple(outer_dims) + shape
+        return torch.zeros(size=shape, dtype=self._dtype)
+
+
+class BoundedTensorSpec(TensorSpec):
+    """A `TensorSpec` that specifies minimum and maximum values.
+    Example usage:
+    ```python
+    spec = BoundedTensorSpec((1, 2, 3), torch.float32, 0, (5, 5, 5))
+    torch_minimum = torch.as_tensor(spec.minimum, dtype=spec.dtype)
+    torch_maximum = torch.as_tensor(spec.maximum, dtype=spec.dtype)
+    ```
+    Bounds are meant to be inclusive. This is especially important for
+    integer types. The following spec will be satisfied by tensors
+    with values in the set {0, 1, 2}:
+    ```python
+    spec = BoundedTensorSpec((3, 5), torch.int32, 0, 2)
+    ```
+    """
+
+    __slots__ = ("_minimum", "_maximum")
+
+    def __init__(self, shape, dtype, minimum, maximum):
+        """Initializes a new `BoundedTensorSpec`.
+        Args:
+            shape (tuple[int]): The shape of the tensor.
+            dtype (str or torch.dtype): The type of the tensor values,
+                e.g., "int32" or torch.int32
+            minimum: numpy number or sequence specifying the minimum element
+                bounds (inclusive). Must be broadcastable to `shape`.
+            maximum: numpy number or sequence specifying the maximum element
+                bounds (inclusive). Must be broadcastable to `shape`.
+        """
+        super(BoundedTensorSpec, self).__init__(shape, dtype)
+
+        if minimum is None or maximum is None:
+            raise ValueError("minimum and maximum must be provided; but saw "
+                             "'%s' and '%s'" % (minimum, maximum))
+
+        try:
+            np.broadcast(minimum, maximum, np.zeros(self.shape))
+        except ValueError as exception:
+            raise ValueError(
+                "minimum or maximum is not compatible with shape. "
+                "Message: {!r}.".format(exception))
+
+        self._minimum = np.array(
+            minimum, dtype=torch_dtype_to_str(self._dtype))
+        self._minimum.setflags(write=False)
+
+        self._maximum = np.array(
+            maximum, dtype=torch_dtype_to_str(self._dtype))
+        self._maximum.setflags(write=False)
+
+    @classmethod
+    def is_bounded(cls):
+        del cls
+        return True
+
+    @classmethod
+    def from_spec(cls, spec):
+        assert isinstance(spec, BoundedTensorSpec)
+        minimum = getattr(spec, "minimum")
+        maximum = getattr(spec, "maximum")
+        return BoundedTensorSpec(spec.shape, spec.dtype, minimum, maximum)
+
+    @property
+    def minimum(self):
+        """Returns a NumPy array specifying the minimum bounds (inclusive)."""
+        return self._minimum
+
+    @property
+    def maximum(self):
+        """Returns a NumPy array specifying the maximum bounds (inclusive)."""
+        return self._maximum
+
+    def __repr__(self):
+        s = "BoundedTensorSpec(shape={}, dtype={}, minimum={}, maximum={})"
+        return s.format(self.shape, repr(self.dtype), repr(self.minimum),
+                        repr(self.maximum))
+
+    def __eq__(self, other):
+        tensor_spec_eq = super(BoundedTensorSpec, self).__eq__(other)
+        return (tensor_spec_eq and np.allclose(self.minimum, other.minimum)
+                and np.allclose(self.maximum, other.maximum))
+
+    def __reduce__(self):
+        return BoundedTensorSpec, (self._shape, self._dtype, self._minimum,
+                                   self._maximum)
+
+    def sample(self, outer_dims=None):
+        """Sample uniformly given the min/max bounds.
+
+        Args:
+            outer_dims (list[int]): an optional list of integers specifying outer
+                dimensions to add to the spec shape before sampling.
+
+        Returns:
+            tensor (torch.Tensor): a tensor of `self._dtype`
+        """
+        shape = self._shape
+        if outer_dims is not None:
+            shape = tuple(outer_dims) + shape
+
+        if self._dtype.is_floating_point:
+            uniform = torch.rand(shape, dtype=self._dtype)
+            return ((1 - uniform) * torch.as_tensor(self._minimum) +
+                    torch.as_tensor(self._maximum) * uniform)
+        else:
+            # torch.randint cannot have multi-dim lows and highs; currently only
+            # support a scalar minimum and maximum
+            assert (np.shape(self._minimum) == ()
+                    and np.shape(self._maximum) == ())
+            return torch.randint(
+                low=self._minimum.item(),
+                high=self._maximum.item() + 1,
+                size=shape,
+                dtype=self._dtype)
---
+++ alf/tensor_specs_test.py
@@ -0,0 +1,75 @@
+# Copyright (c) 2020 Horizon Robotics. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Test cases adapted from tf_agents' tensor_spec_test.py."""
+
+import numpy as np
+import unittest
+from absl.testing import parameterized
+
+from alf.tensor_specs import TensorSpec, BoundedTensorSpec
+from alf.tensor_specs import torch_dtype_to_str
+
+import torch
+
+TYPE_PARAMETERS = ((torch.int32, ), (torch.int64, ), (torch.float32, ),
+                   (torch.float64, ), (torch.uint8, ))
+
+
+class TensorSpecTest(parameterized.TestCase, unittest.TestCase):
+    def setUp(self):
+        super().setUp()
+        self._shape = (20, 30)
+
+    @parameterized.parameters(*TYPE_PARAMETERS)
+    def testIntegerSamplesIncludeUpperBound(self, dtype):
+        if dtype.is_floating_point:  # Only test on integer dtypes.
+            return
+        spec = BoundedTensorSpec(self._shape, dtype, 3, 3)
+        sample = spec.sample()
+        self.assertEqual(sample.shape, self._shape)
+        self.assertTrue(torch.all(sample == 3))
+
+    @parameterized.parameters(*TYPE_PARAMETERS)
+    def testIntegerSamplesExcludeMaxOfDtype(self, dtype):
+        # Exclude non integer types and uint8 (has special sampling logic).
+        if dtype.is_floating_point or dtype == torch.uint8:
+            return
+        info = np.iinfo(torch_dtype_to_str(dtype))
+        spec = BoundedTensorSpec(self._shape, dtype, info.max - 1,
+                                 info.max - 1)
+        sample = spec.sample(outer_dims=(1, ))
+        self.assertEqual(sample.shape, (1, ) + self._shape)
+        self.assertTrue(torch.all(sample == info.max - 1))
+
+    @parameterized.parameters(*TYPE_PARAMETERS)
+    def testBoundedTensorSpecSample(self, dtype):
+        if not dtype.is_floating_point:
+            return
+        # minimum and maximum shape broadcasting
+        spec = BoundedTensorSpec(self._shape, dtype, (0, ) * 30, 3)
+        sample = spec.sample()
+        self.assertEqual(self._shape, sample.shape)
+        self.assertTrue(torch.all(sample <= 3))
+        self.assertTrue(torch.all(0 <= sample))
+
+    @parameterized.parameters(*TYPE_PARAMETERS)
+    def testTensorSpecZero(self, dtype):
+        spec = TensorSpec(self._shape, dtype)
+        sample = spec.zeros(outer_dims=(3, 10))
+        self.assertEqual(sample.shape, (3, 10) + self._shape)
+        self.assertTrue(torch.all(sample == 0))
+
+
+if __name__ == "__main__":
+    unittest.main()
