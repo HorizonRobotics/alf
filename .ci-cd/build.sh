f5b2ef1b160ba85c0e8f95517ac7d6a44a92c826
haonan.yu <haonanu@gmail.com>
2020-02-18 19:09:13 -0800

    encoding networks

--- .ci-cd/build.sh
+++ .ci-cd/build.sh
@@ -43,6 +43,7 @@ function test() {
     cd alf
     python3 -m unittest -v \
         alf.environments.simple.noisy_array_test \
+        alf.networks.encoding_networks_test \
         alf.tensor_specs_test \
         alf.nest.nest_test \
         alf.data_structures_test
