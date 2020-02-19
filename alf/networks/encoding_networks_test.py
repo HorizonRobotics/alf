14f58aa4735484c7546f733c37f9dda365d26d69
haonan.yu <haonanu@gmail.com>
2020-02-18 19:09:13 -0800

    encoding networks

---
+++ alf/networks/encoding_networks_test.py
@@ -0,0 +1,114 @@
+# Copyright (c) 2020 Horizon Robotics. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Tests for alf.encoding_networks."""
+
+from absl.testing import parameterized
+import numpy as np
+import unittest
+
+import torch
+import torch.nn as nn
+
+from alf.networks.encoding_networks import ImageEncodingNetwork
+from alf.networks.encoding_networks import ImageDecodingNetwork
+from alf.networks.encoding_networks import EncodingNetwork
+from alf.tensor_specs import TensorSpec
+
+
+class EncodingNetworkTest(parameterized.TestCase, unittest.TestCase):
+    def test_empty_layers(self):
+        input_spec = TensorSpec((3, ), torch.float32)
+        network = EncodingNetwork(input_spec)
+        self.assertEmpty(list(network.parameters()))
+        self.assertEmpty(network._fc_layers)
+        self.assertTrue(network._img_encoding_net is None)
+
+    @parameterized.parameters((True, ), (False, ))
+    def test_image_encoding_network(self, flatten_output):
+        input_spec = TensorSpec((3, 32, 32), torch.float32)
+        img = input_spec.zeros(outer_dims=(1, ))
+        network = ImageEncodingNetwork(
+            input_channels=input_spec.shape[0],
+            conv_layer_params=[(16, 2, 1, 1), (15, 2, 1, 1)],
+            activation=torch.tanh,
+            flatten_output=flatten_output)
+
+        self.assertLen(list(network.parameters()), 4)  # two conv2d layers
+
+        output_shape = network.output_shape(input_spec.shape[1:])
+        output = network(img)
+        self.assertEqual(output_shape, tuple(output.size()[1:]))
+
+    @parameterized.parameters((None, ), ((100, 100), ))
+    def test_image_decoding_network(self, preprocessing_fc_layers):
+        input_spec = TensorSpec((100, ), torch.float32)
+        embedding = input_spec.zeros(outer_dims=(1, ))
+        network = ImageDecodingNetwork(
+            input_size=input_spec.shape[0],
+            transconv_layer_params=[(16, 2, 1, 1), (64, 3, 2, 0)],
+            start_decoding_height=20,
+            start_decoding_width=31,
+            start_decoding_channels=8,
+            preprocess_fc_layer_params=preprocessing_fc_layers)
+
+        num_layers = 3 if preprocessing_fc_layers is None else 5
+        self.assertLen(list(network.parameters()), num_layers * 2)
+
+        output_shape = network.output_shape()
+        output = network(embedding)
+        self.assertEqual(output_shape, tuple(output.size()[1:]))
+
+    @parameterized.parameters((None, None), (200, None), (50, torch.relu))
+    def test_encoding_network_nonimg(self, last_layer_size, last_activation):
+        input_spec = TensorSpec((100, ), torch.float32)
+        embedding = input_spec.zeros(outer_dims=(1, ))
+        network = EncodingNetwork(
+            input_tensor_spec=input_spec,
+            fc_layer_params=[30, 40, 50],
+            activation=torch.tanh,
+            last_layer_size=last_layer_size,
+            last_activation=last_activation)
+
+        num_layers = 3 if last_layer_size is None else 4
+        self.assertLen(list(network.parameters()), num_layers * 2)
+
+        if last_activation is None:
+            self.assertEqual(network._fc_layers[-1]._activation, torch.tanh)
+        else:
+            self.assertEqual(network._fc_layers[-1]._activation,
+                             last_activation)
+
+        output = network(embedding)
+        if last_layer_size is None:
+            self.assertEqual(output.size()[1], 50)
+        else:
+            self.assertEqual(output.size()[1], last_layer_size)
+
+    def test_encoding_network_img(self):
+        input_spec = TensorSpec((3, 80, 80), torch.float32)
+        img = input_spec.zeros(outer_dims=(1, ))
+        network = EncodingNetwork(
+            input_tensor_spec=input_spec,
+            conv_layer_params=[(16, 5, 2, 1), (15, 3, 2, 0)])
+
+        self.assertLen(list(network.parameters()), 4)
+
+        output = network(img)
+        output_shape = network._img_encoding_net.output_shape(
+            input_spec.shape[1:])
+        self.assertEqual(output.shape[-1], np.prod(output_shape))
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ alf/networks/initializers.py
@@ -0,0 +1,97 @@
+# Copyright (c) 2020 Horizon Robotics. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import math
+from scipy.stats import truncnorm
+
+import torch
+import torch.nn as nn
+
+
+def variance_scaling_init(tensor,
+                          gain=1.0,
+                          mode="fan_in",
+                          distribution="truncated_normal",
+                          calc_gain_after_activation=True,
+                          nonlinearity="identity",
+                          nonlinearity_param=0.01):
+    """Implements TensorFlow's `VarianceScaling` initializer.
+    https://github.com/tensorflow/tensorflow/blob/e5bf8de410005de06a7ff5393fafdf832ef1d4ad/tensorflow/python/ops/init_ops.py#L437
+
+    A potential benefit of this intializer is that we can sample from a truncated
+    normal distribution: `scipy.stats.truncnorm(a=-2, b=2, loc=0., scale=1.)`
+
+    Also incorporates PyTorch's calculation of the recommended gains that taking
+    nonlinear activations into account, so that after N layers, the final output
+    std (in linear space) will be a constant regardless of N's value (when N is
+    large). This auto gain probably won't make much of a difference if the
+    network is shallow, as in most RL cases.
+
+    Example usage:
+        from alf.networks.initializers import variance_scaling_init
+        layer = nn.Linear(2, 2)
+        variance_scaling_init(layer.weight.data,
+                              nonlinearity="leaky_relu",
+                              nonlinearity_param=0.01)
+        nn.init.zeros_(layer.bias.data)
+
+    Args:
+        tensor (torch.Tensor): the weights to be initialized
+        gain (float): a positive scaling factor for weight std. Different from
+            tf's implementation, this number is applied outside of `math.sqrt`.
+            Note that if `calc_gain_after_activation=True`, this number will be
+            an additional gain factor on top of that.
+        mode (str): one of "fan_in", "fan_out", and "fan_avg"
+        distribution (str): one of "normal" and "truncated_normal". If the latter,
+            the weights will be sampled from a normal distribution truncated at
+            (-2, 2).
+        calc_gain_after_activation (bool): whether automatically calculate the
+            std gain of applying nonlinearity after this layer. A nonlinear
+            activation (e.g., relu) might change std after the transformation,
+            so we need to compensate for that. Only used when mode=="fan_in".
+        nonlinearity (str): one of ['linear', 'conv1d', 'conv2d', 'conv3d',
+            'conv_transpose1d', 'conv_transpose2d', 'conv_transpose3d',
+            'sigmoid', 'tanh', 'relu', 'leaky_relu', 'identity']
+        nonlinearity_param (float): additional parameter of the nonlinearity;
+            currently only used by 'leaky_relu' as the negative slope (pytorch
+            default 0.01)
+    """
+    fan_in, fan_out = nn.init._calculate_fan_in_and_fan_out(tensor)
+
+    assert mode in ["fan_in", "fan_out", "fan_avg"], \
+        "Unrecognized mode %s!" % mode
+    if mode == "fan_in":
+        size = max(1.0, fan_in)
+    elif mode == "fan_out":
+        size = max(1.0, fan_out)
+    else:
+        size = max(1.0, (fan_in + fan_out) / 2.0)
+
+    if (calc_gain_after_activation and mode == "fan_in"
+            and nonlinearity != "identity"):
+        gain *= nn.init.calculate_gain(nonlinearity, nonlinearity_param)
+
+    std = gain / math.sqrt(size)
+    if distribution == "truncated_normal":
+        threshold = 2.0  # truncate within 2 std
+        std /= truncnorm.std(-threshold, threshold)
+        with torch.no_grad():
+            return tensor.copy_(
+                # use as_tensor to avoid a copy (memory shared)
+                torch.as_tensor(
+                    truncnorm.rvs(-threshold, threshold, size=tensor.size()) *
+                    std))
+    else:
+        with torch.no_grad():
+            return tensor.normal_(0, std)
