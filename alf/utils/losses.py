0f268f342edb733e4fd2c798f9787b30f3de18b1
emailweixu <emailweixu@gmail.com>
2020-03-03 12:56:48 -0800

    Fix for huber_loss

--- alf/utils/losses.py
+++ alf/utils/losses.py
@@ -27,8 +27,8 @@ def element_wise_huber_loss(x, y):
     Returns:
         loss (Tensor)
     """
-    loss = torch.nn.SmoothL1Loss(reduction=None)
-    return loss(x, y)
+    loss = torch.nn.SmoothL1Loss(reduction="none")
+    return loss(y, x)
 
 
 @gin.configurable
