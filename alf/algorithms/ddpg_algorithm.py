1aad436b5ca412a6c8ff313a350f39a0b313e97e
Haichao Zhang <hczhang1@gmail.com>
2020-03-12 18:16:44 -0700

    DDPG Algorithm and Action Networks (#447)
    * DDPG
    
    * Update build.sh
    
    * Address comments

--- alf/algorithms/ddpg_algorithm.py
+++ alf/algorithms/ddpg_algorithm.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2019 Horizon Robotics. All Rights Reserved.
+# Copyright (c) 2020 Horizon Robotics. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,26 +13,24 @@
 # limitations under the License.
 """Deep Deterministic Policy Gradient (DDPG)."""
 
-import gin.tf
-import tensorflow as tf
+import numpy as np
+import gin
 
-import tensorflow_probability as tfp
-from tensorflow_probability import distributions as tfd
-from tf_agents.agents.ddpg.actor_network import ActorNetwork
-from tf_agents.agents.ddpg.actor_rnn_network import ActorRnnNetwork
-from tf_agents.agents.ddpg.critic_network import CriticNetwork
-from tf_agents.agents.ddpg.critic_rnn_network import CriticRnnNetwork
+import torch
+import torch.nn as nn
+import torch.distributions as td
+from typing import Callable
 
-from tf_agents.networks.network import Network
-from tf_agents.trajectories.policy_step import PolicyStep
-from tf_agents.utils import common as tfa_common
-
+from alf.algorithms.config import TrainerConfig
+from alf.algorithms.off_policy_algorithm import OffPolicyAlgorithm
 from alf.algorithms.one_step_loss import OneStepTDLoss
 from alf.algorithms.rl_algorithm import RLAlgorithm
-from alf.algorithms.off_policy_algorithm import OffPolicyAlgorithm
-from alf.data_structures import namedtuple
-from alf.data_structures import ActionTimeStep, Experience, LossInfo, TrainingInfo
-from alf.utils import losses, common
+from alf.data_structures import TimeStep, Experience, LossInfo, namedtuple
+from alf.data_structures import AlgStep, TrainingInfo
+from alf.nest import nest
+from alf.networks import ActorNetwork, CriticNetwork
+from alf.tensor_specs import TensorSpec, BoundedTensorSpec
+from alf.utils import losses, common, dist_utils, spec_utils
 
 DdpgCriticState = namedtuple("DdpgCriticState",
                              ['critic', 'target_actor', 'target_critic'])
@@ -56,9 +54,11 @@ class DdpgAlgorithm(OffPolicyAlgorithm):
 
     def __init__(self,
                  observation_spec,
-                 action_spec,
-                 actor_network: Network,
-                 critic_network: Network,
+                 action_spec: BoundedTensorSpec,
+                 actor_network: ActorNetwork,
+                 critic_network: CriticNetwork,
+                 env=None,
+                 config: TrainerConfig = None,
                  ou_stddev=0.2,
                  ou_damping=0.15,
                  critic_loss=None,
@@ -75,9 +75,16 @@ class DdpgAlgorithm(OffPolicyAlgorithm):
         Args:
             action_spec (nested BoundedTensorSpec): representing the actions.
             actor_network (Network):  The network will be called with
-                call(observation, step_type).
+                call(observation).
             critic_network (Network): The network will be called with
-                call(observation, action, step_type).
+                call(observation, action).
+            env (Environment): The environment to interact with. env is a batched
+                environment, which means that it runs multiple simulations
+                simultateously. env only needs to be provided to the root
+                Algorithm.
+            config (TrainerConfig): config for training. config only needs to be
+                provided to the algorithm which performs `train_iter()` by
+                itself.
             ou_stddev (float): Standard deviation for the Ornstein-Uhlenbeck
                 (OU) noise added in the default collect policy.
             ou_damping (float): Damping factor for the OU noise added in the
@@ -91,8 +98,8 @@ class DdpgAlgorithm(OffPolicyAlgorithm):
             dqda_clipping (float): when computing the actor loss, clips the
                 gradient dqda element-wise between [-dqda_clipping, dqda_clipping].
                 Does not perform clipping if dqda_clipping == 0.
-            actor_optimizer (tf.optimizers.Optimizer): The optimizer for actor.
-            critic_optimizer (tf.optimizers.Optimizer): The optimizer for actor.
+            actor_optimizer (torch.optim.optimizer): The optimizer for actor.
+            critic_optimizer (torch.optim.optimizer): The optimizer for critic.
             gradient_clipping (float): Norm length to clip gradients.
             debug_summaries (bool): True if debug summaries should be created.
             name (str): The name of this algorithm.
@@ -110,21 +117,20 @@ class DdpgAlgorithm(OffPolicyAlgorithm):
             observation_spec,
             action_spec,
             train_state_spec=train_state_spec,
-            optimizer=[actor_optimizer, critic_optimizer],
-            trainable_module_sets=[[actor_network], [critic_network]],
+            env=env,
+            config=config,
             gradient_clipping=gradient_clipping,
             debug_summaries=debug_summaries,
             name=name)
 
+        self.add_optimizer(actor_optimizer, [actor_network])
+        self.add_optimizer(critic_optimizer, [critic_network])
+
         self._actor_network = actor_network
         self._critic_network = critic_network
-        self._actor_optimizer = actor_optimizer
-        self._critic_optimizer = critic_optimizer
 
-        self._target_actor_network = actor_network.copy(
-            name='target_actor_network')
-        self._target_critic_network = critic_network.copy(
-            name='target_critic_network')
+        self._target_actor_network = actor_network.copy()
+        self._target_critic_network = critic_network.copy()
 
         self._ou_stddev = ou_stddev
         self._ou_damping = ou_damping
@@ -146,52 +152,41 @@ class DdpgAlgorithm(OffPolicyAlgorithm):
 
         self._dqda_clipping = dqda_clipping
 
-    def predict(self, time_step: ActionTimeStep, state, epsilon_greedy):
+    def predict_step(self, time_step: TimeStep, state, epsilon_greedy=1.):
         action, state = self._actor_network(
-            time_step.observation,
-            step_type=time_step.step_type,
-            network_state=state.actor.actor)
-        empty_state = tf.nest.map_structure(lambda x: (),
-                                            self.train_state_spec)
+            time_step.observation, state=state.actor.actor)
+        empty_state = nest.map_structure(lambda x: (), self.train_state_spec)
 
         def _sample(a, ou):
-            return tf.cond(
-                tf.less(tf.random.uniform((), 0, 1),
-                        epsilon_greedy), lambda: a + ou(), lambda: a)
+            if torch.rand(1) < epsilon_greedy:
+                return a + ou()
+            else:
+                return a
 
-        noisy_action = tf.nest.map_structure(_sample, action, self._ou_process)
-        noisy_action = tf.nest.map_structure(tfa_common.clip_to_spec,
-                                             noisy_action, self._action_spec)
+        noisy_action = nest.map_structure(_sample, action, self._ou_process)
+        noisy_action = nest.map_structure(spec_utils.clip_to_spec,
+                                          noisy_action, self._action_spec)
         state = empty_state._replace(
             actor=DdpgActorState(actor=state, critic=()))
-        return PolicyStep(
-            action=noisy_action,
+        return AlgStep(
+            output=noisy_action,
             state=state,
             info=DdpgInfo(action_distribution=action))
 
-    def rollout(self,
-                time_step: ActionTimeStep,
-                state=None,
-                mode=RLAlgorithm.ROLLOUT):
+    def rollout_step(self, time_step: TimeStep, state=None):
         if self.need_full_rollout_state():
             raise NotImplementedError("Storing RNN state to replay buffer "
                                       "is not supported by DdpgAlgorithm")
-        return self.predict(time_step, state, epsilon_greedy=1.0)
+        return self.predict_step(time_step, state, epsilon_greedy=1.0)
 
     def _critic_train_step(self, exp: Experience, state: DdpgCriticState):
         target_action, target_actor_state = self._target_actor_network(
-            exp.observation,
-            step_type=exp.step_type,
-            network_state=state.target_actor)
+            exp.observation, state=state.target_actor)
         target_q_value, target_critic_state = self._target_critic_network(
-            (exp.observation, target_action),
-            step_type=exp.step_type,
-            network_state=state.target_critic)
+            (exp.observation, target_action), state=state.target_critic)
 
         q_value, critic_state = self._critic_network(
-            (exp.observation, exp.action),
-            step_type=exp.step_type,
-            network_state=state.critic)
+            (exp.observation, exp.action), state=state.critic)
 
         state = DdpgCriticState(
             critic=critic_state,
@@ -204,29 +199,28 @@ class DdpgAlgorithm(OffPolicyAlgorithm):
 
     def _actor_train_step(self, exp: Experience, state: DdpgActorState):
         action, actor_state = self._actor_network(
-            exp.observation, exp.step_type, network_state=state.actor)
+            exp.observation, state=state.actor)
 
-        with tf.GradientTape(watch_accessed_variables=False) as tape:
-            tape.watch(action)
-            q_value, critic_state = self._critic_network(
-                (exp.observation, action), network_state=state.critic)
+        q_value, critic_state = self._critic_network((exp.observation, action),
+                                                     state=state.critic)
 
-        dqda = tape.gradient(q_value, action)
+        dqda = nest.pack_sequence_as(
+            action,
+            list(torch.autograd.grad(q_value.sum(), nest.flatten(action))))
 
         def actor_loss_fn(dqda, action):
             if self._dqda_clipping:
-                dqda = tf.clip_by_value(dqda, -self._dqda_clipping,
-                                        self._dqda_clipping)
+                dqda = torch.clamp(dqda, -self._dqda_clipping,
+                                   self._dqda_clipping)
             loss = 0.5 * losses.element_wise_squared_loss(
-                tf.stop_gradient(dqda + action), action)
-            loss = tf.reduce_sum(loss, axis=list(range(1, len(loss.shape))))
+                (dqda + action).detach(), action)
+            loss = loss.sum(list(range(1, loss.ndim)))
             return loss
 
-        actor_loss = tf.nest.map_structure(actor_loss_fn, dqda, action)
+        actor_loss = nest.map_structure(actor_loss_fn, dqda, action)
         state = DdpgActorState(actor=actor_state, critic=critic_state)
-        info = LossInfo(
-            loss=tf.add_n(tf.nest.flatten(actor_loss)), extra=actor_loss)
-        return PolicyStep(action=action, state=state, info=info)
+        info = LossInfo(loss=sum(nest.flatten(actor_loss)), extra=actor_loss)
+        return AlgStep(output=action, state=state, info=info)
 
     def train_step(self, exp: Experience, state: DdpgState):
         critic_state, critic_info = self._critic_train_step(
@@ -235,7 +229,7 @@ class DdpgAlgorithm(OffPolicyAlgorithm):
         return policy_step._replace(
             state=DdpgState(actor=policy_step.state, critic=critic_state),
             info=DdpgInfo(
-                action_distribution=policy_step.action,
+                action_distribution=policy_step.output,
                 critic=critic_info,
                 actor_loss=policy_step.info))
 
@@ -252,7 +246,7 @@ class DdpgAlgorithm(OffPolicyAlgorithm):
             extra=DdpgLossInfo(
                 critic=critic_loss.extra, actor=actor_loss.extra))
 
-    def after_train(self, training_info):
+    def after_update(self, training_info):
         self._update_target()
 
     def _trainable_attributes_to_ignore(self):
@@ -273,15 +267,11 @@ def create_ou_process(action_spec, ou_stddev, ou_damping):
     Returns:
         nested OUProcess with the same structure as action_spec.
     """
-    # todo with seed None
-    seed_stream = tfp.util.SeedStream(seed=None, salt='ou_noise')
 
     def _create_ou_process(action_spec):
-        return tfa_common.OUProcess(
-            lambda: tf.zeros(action_spec.shape, dtype=action_spec.dtype),
-            ou_damping,
-            ou_stddev,
-            seed=seed_stream())
+        return dist_utils.OUProcess(
+            torch.zeros(action_spec.shape, dtype=action_spec.dtype),
+            ou_damping, ou_stddev)
 
-    ou_process = tf.nest.map_structure(_create_ou_process, action_spec)
+    ou_process = nest.map_structure(_create_ou_process, action_spec)
     return ou_process
