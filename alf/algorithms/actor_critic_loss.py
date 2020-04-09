fix entropy computation in ActorCriticLoss (#527)

alf/algorithms/actor_critic_loss.py
@@ -137,10 +137,10 @@ class ActorCriticLoss(nn.Module):
 
         entropy_loss = ()
         if self._entropy_regularization is not None:
-            entropy = dist_utils.compute_entropy(
+            entropy, entropy_for_gradient = dist_utils.entropy_with_fallback(
                 training_info.info.action_distribution)
             entropy_loss = -entropy
-            loss -= self._entropy_regularization * entropy
+            loss -= self._entropy_regularization * entropy_for_gradient
 
         return LossInfo(
             loss=loss,