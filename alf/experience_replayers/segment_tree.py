f5ebef776527a2b300bb38026dc985665b12ddf4
emailweixu <emailweixu@gmail.com>
2020-05-27 20:25:58 -0700

    SegmentTree

---
+++ alf/experience_replayers/segment_tree.py
@@ -0,0 +1,216 @@
+# Copyright (c) 2020 Horizon Robotics. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""SegmentTree."""
+
+import math
+import torch
+import torch.nn as nn
+
+import alf
+from alf.nest.utils import convert_device
+
+
+class SegmentTree(nn.Module):
+    """
+    Data structure to allow efficient calculate the summary statistics over a
+    segment of elements.
+    See https://en.wikipedia.org/wiki/Segment_tree for detail.
+
+    In this implementation, ``values[1]`` is the root. ``values[capacity: 2*capacity]``
+    are the leaves. The two children of an internal node ``values[i]`` are ``values[2*i]``
+    and ``values[2*i+1]``. And ``values[i]`` is set to ``op(values[2*i], values[2*i+1])``
+    """
+
+    def __init__(self,
+                 capacity,
+                 op,
+                 dtype=torch.float32,
+                 device="cpu",
+                 name="SegmentTree"):
+        super().__init__()
+        self._name = name
+        self._device = device
+        with alf.device(self._device):
+            self.register_buffer("_values",
+                                 torch.zeros((2 * capacity, ), dtype=dtype))
+        self._op = op
+        self._capacity = capacity
+        self._leftest_leaf = 1
+        self._depth = 0
+        while self._leftest_leaf < capacity:
+            self._leftest_leaf *= 2
+            if self._leftest_leaf < capacity:
+                self._depth += 1
+
+    def __setitem__(self, indices, values):
+        """Set the value of leaves and update the internal nodes.
+
+        Args:
+            indices (Tensor): 1-D int64 Tensor. Its values should be in range
+                [0, capacity).
+            values (Tensor): 1-D Tensor with the same shape as ``indices``
+        """
+
+        def _step(indices):
+            indices = indices / 2
+            indices = torch.unique(indices)
+            left = self._values[indices * 2]
+            right = self._values[indices * 2 + 1]
+            self._values[indices] = op(left, right)
+            return indices
+
+        with alf.device(self._device):
+            indices = convert_device(indices)
+            values = convert_device(values)
+
+            assert indices.ndim == 1
+            assert values.ndim == 1
+            assert indices.shape == values.shape, (
+                "indices and values should be 1-D tensor with the length. Got "
+                "%s and %s." % (indices.shape, values.shape))
+            op = self._op
+            indices, order = torch.sort(indices)
+            values = values[order]
+            assert indices[-1] < self._capacity
+            indices = self._index_to_leaf(indices)
+            self._values[indices] = values
+
+            num_large = (indices >= self._leftest_leaf).to(torch.int64).sum()
+            if num_large > 0:
+                large_indices = indices[:num_large]
+                small_indices = indices[num_large:]
+                large_indices = _step(large_indices)
+                indices = torch.cat([large_indices, small_indices])
+
+            for _ in range(self._depth):
+                indices = _step(indices)
+
+    def __getitem__(self, idx):
+        """Get the values of leaves.
+
+        Args:
+            idx (Tensor): 1-D int64 Tensor. Its values should be in range
+                [0, capacity).
+        Returns:
+            Tensor: with same shaps as idx.
+        """
+        with alf.device(self._device):
+            idx = convert_device(idx)
+            assert 0 <= idx.min()
+            assert idx.max() < self._capacity
+            result = self._values[self._index_to_leaf(idx)]
+        return convert_device(result)
+
+    def _index_to_leaf(self, idx):
+        """
+        Make sure idx=0 is the leftest leaf.
+        """
+        idx += self._leftest_leaf
+        idx = torch.where(idx >= 2 * self._capacity, idx - self._capacity, idx)
+        return idx
+
+    def _leaf_to_index(self, leaf):
+        idx = leaf - self._leftest_leaf
+        idx = torch.where(idx < 0, idx + self._capacity, idx)
+        return idx
+
+    def summary(self):
+        """The summary of the tree.
+
+        If ``op`` is ``torch.add``, it's the sum of all values.
+        If ``op`` is ``torch.min``, it's the min of all values.
+        If ``op`` is ``torch.max``, it's the max of all values.
+
+        Returns:
+            a scalar
+        """
+        return convert_device(self._values[1])
+
+
+class SumSegmentTree(SegmentTree):
+    """SegmentTree with sum operation."""
+
+    def __init__(self,
+                 capacity,
+                 dtype=torch.float32,
+                 device="cpu",
+                 name="SumSegmentTree"):
+        super().__init__(
+            capacity, torch.add, dtype=dtype, device=device, name=name)
+
+    def __setitem__(self, indices, values):
+        assert values.min() >= 0
+        super().__setitem__(indices, values)
+
+    def find_sum_bound(self, thresholds):
+        """
+        The result is an int64 Tensor with the same shape as `thresholds`.
+        result[i] is the minimum idx such that
+            thresholds[i] < values[0] + ... + values[idx]
+
+        values[result[i]] will never be 0.
+
+        Args:
+            thresholds (Tensor): 1-D Tensor. All the elements in `thresholds`
+                should be smaller than self.summary()
+        Returns:
+            Tensor: 1-D int64 Tensor with the same shape as ``thesholds``
+        """
+
+        def _step(indices, thresholds):
+            indices *= 2
+            left = self._values[indices]
+            greater = thresholds >= left
+            indices = torch.where(greater, indices + 1, indices)
+            thresholds = torch.where(greater, thresholds - left, thresholds)
+            return indices, thresholds
+
+        with alf.device(self._device):
+            thresholds = convert_device(thresholds)
+            indices = torch.ones_like(thresholds, dtype=torch.int64)
+            for _ in range(self._depth):
+                indices, thresholds = _step(indices, thresholds)
+
+            is_small = indices < self._capacity
+            num_small = is_small.to(torch.int64).sum()
+            if num_small > 0:
+                i = torch.nonzero(is_small)
+                small_indices = indices[i]
+                small_thresholds = thresholds[i]
+                small_indices, _ = _step(small_indices, small_thresholds)
+                indices[i] = small_indices
+
+        return convert_device(self._leaf_to_index(indices))
+
+
+class MinSegmentTree(SegmentTree):
+    """SegmentTree with min operation."""
+
+    def __init__(self,
+                 capacity,
+                 dtype=torch.float32,
+                 device="cpu",
+                 name="MinSegmentTree"):
+        super().__init__(capacity, torch.min, dtype, device=device, name=name)
+
+
+class MaxSegmentTree(SegmentTree):
+    """SegmentTree with max operation."""
+
+    def __init__(self,
+                 capacity,
+                 dtype=torch.float32,
+                 device="cpu",
+                 name="MaxSegmentTree"):
+        super().__init__(capacity, torch.max, dtype, device=device, name=name)
---
+++ alf/experience_replayers/segment_tree_test.py
@@ -0,0 +1,68 @@
+# Copyright (c) 2020 Horizon Robotics. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import random
+import torch
+
+import alf
+from alf.experience_replayers.segment_tree import SumSegmentTree, MaxSegmentTree
+
+
+class SegmentTreeTest(alf.test.TestCase):
+    def test_max_tree(self):
+        for size in [1, 2, 3, 4, 7, 8, 9, 15, 16, 128]:
+            tree = MaxSegmentTree(size)
+            vals = torch.zeros(size, dtype=torch.float32)
+            for _ in range(1000):
+                n = random.randint(1, size)
+                i = torch.randint(size, size=(n, ), dtype=torch.int64)
+                i, _ = torch.sort(i)
+                i = torch.unique(i)
+                i = alf.math.shuffle(i)
+                v = torch.randint(0, 10000, size=i.shape, dtype=torch.float32)
+                vals[i] = v
+                tree[i] = v
+                self.assertEqual(tree.summary(), vals.max())
+            i = torch.arange(size, dtype=torch.int64)
+            self.assertEqual(tree[i], vals)
+
+    def test_sum_tree(self):
+        for size in [3, 1, 2, 3, 4, 7, 8, 9, 15, 16, 128]:
+            tree = SumSegmentTree(size)
+            vals = torch.zeros(size, dtype=torch.float32)
+            for _ in range(1000):
+                n = random.randint(1, size)
+                i = torch.randint(size, size=(n, ), dtype=torch.int64)
+                i, _ = torch.sort(i)
+                i = torch.unique(i)
+                i = alf.math.shuffle(i)
+                v = torch.randint(0, 10000, size=i.shape, dtype=torch.float32)
+                vals[i] = v
+                tree[i] = v
+                self.assertEqual(tree.summary(), vals.sum())
+
+            s = torch.cumsum(vals, 0)
+            s = torch.cat([torch.tensor([0.]), s[:-1]])
+            for _ in range(1000):
+                n = random.randint(1, size)
+                i = torch.randint(size, size=(n, ), dtype=torch.int64)
+                i, _ = torch.sort(i)
+                i = torch.unique(i)
+                i = alf.math.shuffle(i)
+                v = s[i] + vals[i] * torch.rand(i.shape) * 0.999
+                self.assertEqual(tree.find_sum_bound(v), i)
+
+
+if __name__ == '__main__':
+    alf.test.main()
