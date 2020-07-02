HorizonRobotics/alf

commit ff8d77633bc08dc248fa38231983f9ee5d7be035

ALF performance optimization (#606)
* ALF performance optimization

* Fix train_play_test for super mario

* Update comments

* Bug fixes and a few more optimizations

* Bug fix

alf/algorithms/rl_algorithm.py
@@ -456,9 +456,9 @@ class RLAlgorithm(Algorithm):
             next_time_step = self._env.step(action)
             env_step_time += time.time() - t0
 
-            self.observe_for_metrics(time_step)
+            self.observe_for_metrics(time_step.cpu())
 
-            exp = make_experience(time_step, policy_step, policy_state)
+            exp = make_experience(time_step.cpu(), policy_step, policy_state)
 
             t0 = time.time()
             self.observe_for_replay(exp)

alf/__init__.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 
 from . import metrics
+from . import module
 from . import networks
 from . import nest
 from . import optimizers

alf/algorithms/algorithm.py
@@ -153,6 +153,10 @@ class Algorithm(nn.Module):
         else:
             self._predict_state_spec = self._rollout_state_spec
 
+        self._initial_train_states = {}
+        self._initial_rollout_states = {}
+        self._initial_predict_states = {}
+
         self._experience_spec = None
         self._train_info_spec = None
         self._processed_experience_spec = None
@@ -608,13 +612,27 @@ class Algorithm(nn.Module):
         return state
 
     def get_initial_predict_state(self, batch_size):
-        return spec_utils.zeros_from_spec(self._predict_state_spec, batch_size)
+        r = self._initial_predict_states.get(batch_size)
+        if r is None:
+            r = spec_utils.zeros_from_spec(self._predict_state_spec,
+                                           batch_size)
+            self._initial_predict_states[batch_size] = r
+        return r
 
     def get_initial_rollout_state(self, batch_size):
-        return spec_utils.zeros_from_spec(self._rollout_state_spec, batch_size)
+        r = self._initial_rollout_states.get(batch_size)
+        if r is None:
+            r = spec_utils.zeros_from_spec(self._rollout_state_spec,
+                                           batch_size)
+            self._initial_rollout_states[batch_size] = r
+        return r
 
     def get_initial_train_state(self, batch_size):
-        return spec_utils.zeros_from_spec(self._train_state_spec, batch_size)
+        r = self._initial_train_states.get(batch_size)
+        if r is None:
+            r = spec_utils.zeros_from_spec(self._train_state_spec, batch_size)
+            self._initial_train_states[batch_size] = r
+        return r
 
     @common.add_method(nn.Module)
     def state_dict(self, destination=None, prefix='', visited=None):
@@ -1269,7 +1287,7 @@ class Algorithm(nn.Module):
                         lambda x: x[indices], batch_info)
             for b in range(0, batch_size, mini_batch_size):
                 if update_counter_every_mini_batch:
-                    alf.summary.get_global_counter().add_(1)
+                    alf.summary.increment_global_counter()
                 is_last_mini_batch = (u == num_updates - 1
                                       and b + mini_batch_size >= batch_size)
                 do_summary = (is_last_mini_batch

alf/algorithms/off_policy_algorithm.py
@@ -61,7 +61,7 @@ class OffPolicyAlgorithm(RLAlgorithm):
         config: TrainerConfig = self._config
 
         if not config.update_counter_every_mini_batch:
-            alf.summary.get_global_counter().add_(1)
+            alf.summary.increment_global_counter()
 
         with torch.set_grad_enabled(config.unroll_with_grad):
             with record_time("time/unroll"):

alf/algorithms/on_policy_algorithm.py
@@ -65,7 +65,7 @@ class OnPolicyAlgorithm(OffPolicyAlgorithm):
 
     def _train_iter_on_policy(self):
         """User may override this for their own training procedure."""
-        alf.summary.get_global_counter().add_(1)
+        alf.summary.increment_global_counter()
 
         with record_time("time/unroll"):
             experience = self.unroll(self._config.unroll_length)

alf/algorithms/trac_algorithm.py
@@ -226,8 +226,7 @@ class TracAlgorithm(OnPolicyAlgorithm):
         state = nest_map(lambda x: x[0], exp_array.state)
         # exp_array.state is no longer needed
         exp_array = exp_array._replace(state=())
-        initial_state = common.zero_tensor_from_nested_spec(
-            self.predict_state_spec, batch_size)
+        initial_state = self.get_initial_predict_state(batch_size)
         total_dists = nest_map(lambda _: torch.tensor(0.), self.action_spec)
         for t in range(num_steps):
             exp = nest_map(lambda x: x[t], exp_array)

alf/data_structures.py
@@ -107,6 +107,22 @@ class TimeStep(
     def is_last(self):
         return self.step_type == StepType.LAST
 
+    def cuda(self):
+        """Get the cuda version of this data structure."""
+        r = getattr(self, "_cuda", None)
+        if r is None:
+            r = nest.map_structure(lambda x: x.cuda(), self)
+            self._cuda = r
+        return r
+
+    def cpu(self):
+        """Get the cpu version of this data structure."""
+        r = getattr(self, "_cpu", None)
+        if r is None:
+            r = nest.map_structure(lambda x: x.cpu(), self)
+            self._cpu = r
+        return r
+
 
 class Experience(
         namedtuple(
@@ -447,7 +463,7 @@ LossInfo = namedtuple(
         # Priority for each sample. This will be used to update the priority in
         # the replay buffer so that in the future, this sample will be sampled
         # with probability proportional to this weight powered to
-        # config.priority_replay_alpha. If not empty, its shape should be (B,).
+        # config.priority_replay_alpha.  If not empty, its shape should be (B,).
         "priority",
     ],
     default_value=())

alf/device_ctx.py
@@ -70,7 +70,9 @@ class device(object):
 
     def __enter__(self):
         self._prev_device_name = get_default_device()
-        set_default_device(self._device_name)
+        if self._prev_device_name != self._device_name:
+            set_default_device(self._device_name)
 
     def __exit__(self, type, value, traceback):
-        set_default_device(self._prev_device_name)
+        if self._prev_device_name != self._device_name:
+            set_default_device(self._prev_device_name)

commit f59ea6a9c04f6c1148e4f42e30f101842a18f16f

Fix suite_dmlab_test.py
TODO: add dmlab to docker image for CI
Also fix a bug for "one_time"replay caused by PR #606

alf/algorithms/rl_algorithm.py
@@ -458,7 +458,11 @@ class RLAlgorithm(Algorithm):
 
             self.observe_for_metrics(time_step.cpu())
 
-            exp = make_experience(time_step.cpu(), policy_step, policy_state)
+            if self._exp_replayer_type == "one_time":
+                exp = make_experience(time_step, policy_step, policy_state)
+            else:
+                exp = make_experience(time_step.cpu(), policy_step,
+                                      policy_state)
 
             t0 = time.time()
             self.observe_for_replay(exp)

alf/environments/suite_dmlab_test.py
@@ -101,7 +101,7 @@ class SuiteDMLabTest(parameterized.TestCase, alf.test.TestCase):
 
         constructor = functools.partial(ctor, scene)
 
-        self._env = parallel_environment.ParallelEnvironment(
+        self._env = parallel_environment.ParallelAlfEnvironment(
             [constructor] * env_num)
         self.assertTrue(self._env.batched)
         self.assertEqual(self._env.batch_size, env_num)
@@ -120,7 +120,8 @@ class SuiteDMLabTest(parameterized.TestCase, alf.test.TestCase):
 
         constructor = functools.partial(ctor, scene)
 
-        self._env = parallel_environment.ParallelEnvironment([constructor] * 5)
+        self._env = parallel_environment.ParallelAlfEnvironment(
+            [constructor] * 5)
         self.assertEqual((3, 84, 84), self._env.observation_spec().shape)
 
         for _ in range(10):