9cb90e0fce683b6e54f71a570dd0642dc2ba573c
Le Horizon <le.zhao+git@horizon.ai>
2020-09-10 09:49:49 -0700

    fix playing for multi dim reward

--- alf/trainers/policy_trainer.py
+++ alf/trainers/policy_trainer.py
@@ -671,7 +671,7 @@ def play(root_dir,
             env.render(mode='human')
             time.sleep(sleep_time_per_step)
 
-        episode_reward += float(time_step.reward)
+        episode_reward += float(torch.sum(time_step.reward))
 
         if time_step.is_last() or episode_length >= max_episode_length > 0:
             logging.info("episode_length=%s episode_reward=%s" %
