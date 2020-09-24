e2eeb1cb8840b4029ee1ab3d97b966fed92196fc
Haichao Zhang <hczhang1@gmail.com>
2020-09-24 09:44:10 -0700

    show full reward if it's a vector

--- alf/trainers/policy_trainer.py
+++ alf/trainers/policy_trainer.py
@@ -668,13 +668,9 @@ def play(root_dir,
             env.render(mode='human')
             time.sleep(sleep_time_per_step)
 
-        if time_step.reward.numel() > 1:
-            # use sum of individual rewards when a vector reward is provided
-            time_step_reward = time_step.reward.sum()
-        else:
-            time_step_reward = time_step.reward
+        time_step_reward = time_step.reward.view(-1).float().cpu().numpy()
 
-        episode_reward += float(time_step.reward)
+        episode_reward += time_step_reward
 
         if time_step.is_last() or episode_length >= max_episode_length > 0:
             logging.info("episode_length=%s episode_reward=%s" %
