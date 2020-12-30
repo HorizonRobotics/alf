658834b64b98c9c64bbd6e97b4462ee04a224c4a
Haichao Zhang <hczhang1@gmail.com>
2020-12-30 10:23:07 -0800

    Suite highway environment

---
+++ alf/environments/suite_highway.py
@@ -0,0 +1,144 @@
+# Copyright (c) 2020 Horizon Robotics. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Suite for loading highway environments.
+    Installation:
+    pip install git+https://github.com/eleurent/highway-env
+"""
+
+import collections
+import gin
+import gym
+import gym.spaces
+
+from alf.environments import suite_gym, alf_wrappers, gym_wrappers, process_environment
+
+try:
+    import highway_env
+except ImportError:
+    highway_env = None
+
+
+def is_available():
+    return highway_env is not None
+
+
+class VectorizeObservation(gym_wrappers.BaseObservationWrapper):
+    def transform_space(self, observation_space):
+        return gym.spaces.Box(
+            low=-observation_space.low.ravel(),
+            high=observation_space.high.ravel())
+
+    def transform_observation(self, observation):
+        return observation.ravel()
+
+
+class RemoveActionEnvInfo(gym.Wrapper):
+    """Remove action from EnvInfo if exist
+    """
+
+    def step(self, action):
+        obs, reward, done, env_info = self.env.step(action)
+        env_info.pop('action', None)
+        return obs, reward, done, env_info
+
+
+@gin.configurable
+def load(environment_name,
+         env_id=None,
+         discount=1.0,
+         max_episode_steps=None,
+         gym_env_wrappers=(),
+         alf_env_wrappers=(),
+         env_config=None):
+    """Loads the selected environment and wraps it with the specified wrappers.
+
+    Note that by default a TimeLimit wrapper is used to limit episode lengths
+    to the default benchmarks defined by the registered environments.
+
+    Args:
+        environment_name (str): Name for the environment to load.
+        env_id (int): (optional) ID of the environment.
+        discount (float): Discount to use for the environment.
+        max_episode_steps (int): If None the max_episode_steps will be set to the
+            default step limit defined in the environment's spec. No limit is applied
+            if set to 0 or if there is no max_episode_steps set in the environment's
+            spec.
+        gym_env_wrappers (Iterable): Iterable with references to gym_wrappers
+            classes to use directly on the gym environment.
+        alf_env_wrappers (Iterable): Iterable with references to alf_wrappers
+            classes to use on the ALF environment.
+        env_config (dict|None): a dictionary for configuring some aspects of the
+            environment. If is None, the default configuration will be used.
+            Please refer to the ``default_env_config`` below for
+            an example config and the doc for more details:
+            https://highway-env.readthedocs.io/en/latest/user_guide.html
+
+    Returns:
+        An AlfEnvironment instance.
+    """
+    assert environment_name in {
+        "highway-v0", "merge-v0", "roundabout-v0", "intersection-v0",
+        "parking-v0"
+    }, "wrong highway environment name"
+
+    gym_spec = gym.spec(environment_name)
+    gym_env = gym.make(environment_name)
+
+    if env_config is None:
+        default_env_config = {
+            "observation": {
+                "type":
+                    "Kinematics",
+                "vehicles_count":
+                    5,
+                "features": [
+                    "presence", "x", "y", "vx", "vy", "cos_h", "sin_h"
+                ],
+                "features_range": {
+                    "x": [-100, 100],
+                    "y": [-100, 100],
+                    "vx": [-20, 20],
+                    "vy": [-20, 20]
+                },
+                "absolute":
+                    False,
+                "order":
+                    "sorted"
+            },
+            "action": {
+                "type": "ContinuousAction"
+            }
+        }
+        env_config = default_env_config
+
+    gym_env.configure(env_config)
+    _ = gym_env.reset()
+
+    # currently vectorize the observations, will support other ways later
+    gym_env = VectorizeObservation(gym_env)
+    gym_env = RemoveActionEnvInfo(gym_env)
+
+    if max_episode_steps is None:
+        if gym_spec.max_episode_steps is not None:
+            max_episode_steps = gym_spec.max_episode_steps
+        else:
+            max_episode_steps = 0
+
+    return suite_gym.wrap_env(
+        gym_env,
+        env_id=env_id,
+        discount=discount,
+        max_episode_steps=max_episode_steps,
+        gym_env_wrappers=gym_env_wrappers,
+        alf_env_wrappers=alf_env_wrappers)
---
+++ alf/environments/suite_highway_test.py
@@ -0,0 +1,121 @@
+# Copyright (c) 2020 Horizon Robotics. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import functools
+import gin
+import torch
+
+import alf
+from alf.environments import suite_highway, alf_environment
+from alf.environments import thread_environment, parallel_environment
+import alf.nest as nest
+from alf.tensor_specs import BoundedTensorSpec
+
+
+class SuiteHighwayTest(alf.test.TestCase):
+    def setUp(self):
+        super().setUp()
+        if not suite_highway.is_available():
+            self.skipTest('suite_highway is not available.')
+        else:
+            gin.clear_config()
+
+    def tearDown(self):
+        super().tearDown()
+        self._env.close()
+
+    def test_unwrapped_env(self):
+        self._env = suite_highway.load(environment_name='highway-v0')
+        self.assertIsInstance(self._env, alf_environment.AlfEnvironment)
+        self.assertEqual(torch.float32, self._env.observation_spec().dtype)
+
+        actions = self._env.action_spec().sample()
+        for _ in range(10):
+            # unwrapped env (not thread_env or parallel_env) needs to convert
+            # from tensor to array
+            time_step = self._env.step(actions.cpu().numpy())
+
+    def test_thread_env(self):
+        self._env = thread_environment.ThreadEnvironment(
+            lambda: suite_highway.load(environment_name='highway-v0'))
+        self.assertIsInstance(self._env, alf_environment.AlfEnvironment)
+        self.assertEqual(torch.float32, self._env.observation_spec().dtype)
+
+        actions = self._env.action_spec().sample()
+        for _ in range(10):
+            time_step = self._env.step(actions)
+
+    def test_parallel_env(self):
+        env_num = 5
+
+        def ctor(env_id=None):
+            return suite_highway.load(environment_name='highway-v0')
+
+        constructor = functools.partial(ctor)
+
+        self._env = parallel_environment.ParallelAlfEnvironment(
+            [constructor] * env_num)
+
+        self.assertTrue(self._env.batched)
+        self.assertEqual(self._env.batch_size, env_num)
+        self.assertEqual(torch.float32, self._env.observation_spec().dtype)
+
+        actions = self._env.action_spec().sample(outer_dims=(env_num, ))
+        for _ in range(10):
+            time_step = self._env.step(actions)
+
+    def test_env_info(self):
+        self._env = suite_highway.load(environment_name="highway-v0")
+        actions = self._env.action_spec().sample()
+        time_step = self._env.step(actions.cpu().numpy())
+        env_info = time_step.env_info
+        for field in env_info:
+            self.assertEqual(env_info[field].size, 1)
+        self.assertFalse('action' in time_step.env_info.keys())
+
+    def test_env_config(self):
+
+        env = suite_highway.load(environment_name="highway-v0")
+        self.assertEqual(env.observation_spec().shape, (35, ))
+        self.assertTrue(env.action_spec().is_continuous)
+        self.assertEqual(env.action_spec().shape, (2, ))
+        env.close()
+
+        # test env with specified config
+        env_config = {
+            "observation": {
+                "type":
+                    "Kinematics",
+                "vehicles_count":
+                    3,
+                "features": [
+                    "presence", "x", "y", "vx", "vy", "cos_h", "sin_h"
+                ],
+            },
+            "action": {
+                "type": "DiscreteMetaAction"
+            }
+        }
+
+        env = suite_highway.load(
+            environment_name="highway-v0", env_config=env_config)
+        self.assertEqual(env.observation_spec().shape, (21, ))
+        self.assertTrue(env.action_spec().is_discrete)
+        self.assertEqual(env.action_spec().numel, 1)
+
+        self._env = env
+
+
+if __name__ == '__main__':
+    alf.test.main()
