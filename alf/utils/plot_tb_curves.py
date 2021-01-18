395e2efb9a9a79318508eadfd882acce7ac102a9
haonan.yu <haonanu@gmail.com>
2021-01-18 12:08:41 -0800

    plotting normalized score

--- alf/utils/plot_tb_curves.py
+++ alf/utils/plot_tb_curves.py
@@ -22,19 +22,21 @@ from scipy.signal import savgol_filter
 import matplotlib
 import matplotlib.pyplot as plt
 # Style gallery: https://tonysyu.github.io/raw_content/matplotlib-style-gallery/gallery.html
-plt.style.use('seaborn-colorblind')
+plt.style.use('seaborn-dark')
 
 import alf.nest as nest
 from alf.data_structures import namedtuple
 
 HOME = os.getenv("HOME")
 
+# A namedtuple that represents a curve with mean and upper/lower bounds
 MeanCurve = namedtuple(
     "MeanCurve", ['x', 'y', 'min_y', 'max_y', 'name'], default_value=())
 
 
 class MeanCurveReader(object):
-    """Read and compute a MeanCurve from one or multiple TB event files.
+    """Read and compute a ``MeanCurve`` from one or multiple TB event files. A
+    ``MeanCurveReader`` is suitable for one method on one task with multiple runs.
     """
     _SIZE_GUIDANCE = {
         'compressedHistograms': 10,
@@ -57,6 +59,7 @@ class MeanCurveReader(object):
 
     def __init__(self,
                  event_file,
+                 fraction=0.,
                  name="MeanCurve",
                  smoothing=None,
                  variance_mode="std",
@@ -68,6 +71,8 @@ class MeanCurveReader(object):
                 "eval/" or "train/". The curves of these files will be averaged.
                 It's the user's responsibility to ensure that it's meaningful to
                 group these event files and show their mean and variance.
+            fraction (float): if >0 (e.g., 0.5), then only the first ``fraction``
+                of the curve will be loaded.
             name (str): name of the mean curve.
             smoothing (int): if None, no smoothing is applied; otherwise this
                 is the window width of a Savitzky-Golay filter
@@ -87,6 +92,11 @@ class MeanCurveReader(object):
         if max_n_scalars is not None:
             self._SIZE_GUIDANCE['scalars'] = max_n_scalars
 
+        assert 0 <= fraction <= 1, "`fraction` should be in [0,1]!"
+        if fraction > 0:
+            n_scalars = self._SIZE_GUIDANCE['scalars']
+            self._SIZE_GUIDANCE['scalars'] = int(n_scalars / fraction)
+
         x, ys = None, []
         for ef in event_file:
             event_acc = EventAccumulator(ef, self._SIZE_GUIDANCE)
@@ -94,6 +104,11 @@ class MeanCurveReader(object):
             # 'scalar_events' is a list of ScalarEvent(wall_time, step, value),
             # with a maximal length specified by _SIZE_GUIDANCE
             scalar_events = event_acc.Scalars(self._get_metric_name())
+            if fraction > 0:
+                # truncate the ``scalar_events`` list to ``fracion```
+                scalar_events = scalar_events[:max(
+                    1, int(fraction * len(scalar_events)))]
+
             steps, values = zip(*[(se.step, se.value) for se in scalar_events])
             if x is None:
                 x = np.array(steps)
@@ -120,6 +135,11 @@ class MeanCurveReader(object):
                 raise ValueError("Invalid variance mode: %s" % variance_mode)
             self._mean_curve = MeanCurve(
                 x=x, y=y, min_y=min_y, max_y=max_y, name=name)
+            self._name = name
+
+    @property
+    def name(self):
+        return self._name
 
     def __call__(self):
         return self._mean_curve
@@ -229,31 +249,94 @@ class IterationsSuccessReader(MeanCurveReader):
         return "Success Rate"
 
 
+class MeanCurveGroupReader(object):
+    r"""Group several ``MeanCurveReader`` results. A ``MeanCurveGroupReader`` is
+    suitable for one method on multiple tasks, each task with multiple runs.
+    To aggregate across tasks, each task must be provided with a performance
+    range :math:`(y_0, y_1)` that will be used to normalize performance for that
+    task as :math:`\frac{y - y_0}{y_1 - y_0}`.
+    """
+
+    def __init__(self, mean_curve_readers, task_performance_ranges, name):
+        """
+        Args:
+            mean_curve_readers (list[MeanCurveReader]): a list of
+                ``MeanCurveReader`` of multiple tasks for one method.
+            task_performance_ranges (list[tuple(float)]): a list of tuples, where
+                each tuple is a pair of floats used for normalizing the corresponding
+                task.
+            name (str): name of the method
+        """
+
+        def _normalize(y, y0, y1):
+            return (y - y0) / (y1 - y0)
+
+        assert len(mean_curve_readers) == len(task_performance_ranges)
+        curves = [reader() for reader in mean_curve_readers]
+        y, min_y, max_y = [], [], []
+
+        for c, (y0, y1) in zip(curves, task_performance_ranges):
+            assert len(c.x) == len(curves[0].x)
+            y.append(_normalize(c.y, y0, y1))
+            min_y.append(_normalize(c.min_y, y0, y1))
+            max_y.append(_normalize(c.max_y, y0, y1))
+
+        self._mean_curve = MeanCurve(
+            x=curves[0].x,
+            name=curves[0].name,
+            y=np.mean(y, axis=0),
+            min_y=np.mean(min_y,
+                          axis=0),  # TODO: might have a better way than mean()
+            max_y=np.mean(max_y, axis=0))
+
+        self._x_label = mean_curve_readers[0].x_label
+        self._name = name
+
+    @property
+    def x_label(self):
+        return self._x_label
+
+    @property
+    def y_label(self):
+        return "Aggregated Score"
+
+    @property
+    def name(self):
+        return self._name
+
+    def __call__(self):
+        return self._mean_curve
+
+
 class CurvesPlotter(object):
     """Plot several MeanCurves in a figure. The curve colors will form
     a cycle over 10 default colors.
     """
     _COLORS = ['C%d' % i for i in range(10)]
-    _LINE_WIDTH = 2
 
     def __init__(self,
                  mean_curves,
+                 y_clipping=None,
                  x_range=None,
                  y_range=None,
-                 y_clipping=None,
                  x_label=None,
                  y_label=None,
-                 x_scaled_and_aligned=True,
+                 x_scaled_and_aligned=False,
+                 linestyle='-',
+                 linewidth=2,
+                 alpha=0.3,
                  title=None):
-        """
+        r"""
         Args:
-            mean_curves (MeanCurve|list[MeanCurve]):
+            mean_curves (MeanCurve|list[MeanCurve]): each ``MeanCurve`` should
+                correspond to a different method.
             x_range (tuple[float]): a tuple of ``(min_x, max_x)`` for showing on
                 the figure. If None, then ``(0, 1)`` will be used. This argument is
                 only used when ``x_scaled_and_aligned==True``.
             y_range (tuple[float]): a tuple of ``(min_y, max_y)`` for showing on
                 the figure. If None, then it will be decided according to the
-                y values.
+                ``y`` values. Note that this range won't change ``y`` data; it's
+                only used by matplotlib for drawing ``y`` limits.
             y_clipping (tuple[float]): the y values will be clipped to this range
                 if not None. Because of smoothing in ``MeanCurveReader`` and/or
                 std region, the input y values might be out of this range.
@@ -262,6 +345,11 @@ class CurvesPlotter(object):
             x_scaled_and_aligned (bool): If True, the x axes of all MeanCurves
                 will be scaled and aligned to ``x_range``; otherwise, the x axes
                 will be plot according to ``x`` of each MeanCurve.
+            linestyle (str): the line style to plot. Possible values:
+                '-' ('solid'), '--' ('dashed'), '-.' (dashdot), and ':' ('dotted').
+            linewidth (int): the thickness of lines to plot. Default: 2.
+            alpha (float): the transparency value for plotting shaded area around
+                a curve.
             title (str): title of the figure
         """
         self._fig, ax = plt.subplots(1)
@@ -293,17 +381,19 @@ class CurvesPlotter(object):
                 x,
                 _clip_y(c.y),
                 color=color,
-                lw=self._LINE_WIDTH,
+                lw=linewidth,
+                linestyle=linestyle,
                 label=c.name)
             ax.fill_between(
                 x,
                 _clip_y(c.max_y),
                 _clip_y(c.min_y),
                 facecolor=color,
-                alpha=0.3)
+                alpha=alpha)
 
         ax.legend(loc='best')
         ax.grid(linestyle='--')
+        ax.ticklabel_format(axis="x", style="sci", scilimits=(0, 0))
 
         if y_range:
             ax.set_ylim(y_range)
@@ -314,7 +404,7 @@ class CurvesPlotter(object):
         if title:
             ax.set_title(title)
 
-    def plot(self, output_path, dpi=200, transparent=False):
+    def plot(self, output_path, dpi=200, transparent=False, close_fig=True):
         """Plot curves and save the figure to disk.
 
         Args:
@@ -323,10 +413,13 @@ class CurvesPlotter(object):
                 resolution.
             transparent (bool): If True, then the figure has a transparent
                 background.
+            close_fig (bool): whether to close/release this figure after plotting.
+                If ``False``, the user has to close it manually.
         """
         self._fig.savefig(
             output_path, dpi=dpi, transparent=transparent, bbox_inches='tight')
-        plt.close(self._fig)
+        if close_fig:
+            plt.close(self._fig)
 
 
 def _get_curve_path(dir):
