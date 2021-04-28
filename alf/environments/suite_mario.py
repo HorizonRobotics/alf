5a6b54fb388a35410b7b0bd142489bd265677786
haonan.yu <haonanu@gmail.com>
2021-04-28 11:36:43 -0700

    fix mario example

--- alf/environments/suite_mario.py
+++ alf/environments/suite_mario.py
@@ -17,7 +17,7 @@ import gin
 import gym
 
 from alf.environments import suite_gym, alf_wrappers, process_environment
-from alf.environments.gym_wrappers import FrameSkip, FrameStack
+from alf.environments.gym_wrappers import FrameSkip
 from alf.environments.mario_wrappers import MarioXReward, \
     LimitedDiscreteActions, ProcessFrame84, FrameFormat
 from alf.environments.utils import UnwrappedEnvChecker
@@ -47,7 +47,6 @@ def load(game,
          discount=1.0,
          wrap_with_process=False,
          frame_skip=4,
-         frame_stack=4,
          data_format='channels_last',
          record=False,
          crop=True,
@@ -62,7 +61,6 @@ def load(game,
         wrap_with_process (bool): Whether wrap env in a process
         discount (float): Discount to use for the environment.
         frame_skip (int): the frequency at which the agent experiences the game
-        frame_stack (int): Stack k last frames
         data_format (str): one of `channels_last` (default) or `channels_first`.
                     The ordering of the dimensions in the inputs.
         record (bool): Record the gameplay , see retro.retro_env.RetroEnv.record
@@ -91,8 +89,6 @@ def load(game,
         if frame_skip:
             env = FrameSkip(env, frame_skip)
         env = ProcessFrame84(env, crop=crop)
-        if frame_stack:
-            env = FrameStack(env, stack_size=frame_stack)
         env = FrameFormat(env, data_format=data_format)
         env = LimitedDiscreteActions(env, buttons)
         return suite_gym.wrap_env(
