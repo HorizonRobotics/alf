HorizonRobotics/alf

commit ade596d94b227d558374403f0e5fd5d2e2843bed

Parallel version of some networks (#968)
* init

* Fix code reviews.

alf/algorithms/actor_critic_algorithm.py
@@ -103,8 +103,7 @@ class ActorCriticAlgorithm(OnPolicyAlgorithm):
         value_network = value_network_ctor(input_tensor_spec=observation_spec)
 
         if reward_spec.numel > 1:
-            value_network = alf.networks.NaiveParallelNetwork(
-                value_network, reward_spec.numel)  # value->[B,n]
+            value_network = value_network.make_parallel(reward_spec.numel) # value->[B,n] 
 
         super(ActorCriticAlgorithm, self).__init__(
             observation_spec=observation_spec,

alf/algorithms/trac_algorithm.py
@@ -38,20 +38,15 @@ TracInfo = namedtuple(
 @alf.configurable
 class TracAlgorithm(RLAlgorithm):
     """Trust-region actor-critic.
-
     It compares the action distributions after the SGD with the action
     distributions from the previous model. If the average distance is too big,
     the new parameters are shrinked as:
-
     .. code-block:: python
-
         w_new' = old_w + 0.9 * distance_clip / distance * (w_new - w_old)
-
     If the distribution is ``Categorical``, the distance is
     :math:`||logits_1 - logits_2||^2`, and if the distribution is
     ``Deterministic``, it is :math:`||loc_1 - loc_2||^2`,  otherwise it's
     :math:`KL(d1||d2) + KL(d2||d1)`.
-
     The reason of using :math:`||logits_1 - logits_2||^2` for categorical
     distributions is that KL can be small even if there are large differences in
     logits when the entropy is small. This means that KL cannot fully capture
@@ -69,7 +64,6 @@ class TracAlgorithm(RLAlgorithm):
                  debug_summaries=False,
                  name="TracAlgorithm"):
         """
-
         Args:
             action_spec (nested BoundedTensorSpec): representing the actions.
             ac_algorithm_cls (type): Actor Critic Algorithm cls.
@@ -178,9 +172,7 @@ class TracAlgorithm(RLAlgorithm):
     @torch.no_grad()
     def _calc_change(self, exp_array):
         """Calculate the distance between old/new action distributions.
-
         The distance is:
-
         - :math:`||logits_1 - logits_2||^2` for Categorical distribution
         - :math:`||loc_1 - loc_2||^2` for Deterministic distribution
         - :math:`KL(d1||d2) + KL(d2||d1)` for others

alf/networks/actor_distribution_networks.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2020 Horizon Robotics and ALF Contributors. All Rights Reserved.
+# Copyright (c) 2021 Horizon Robotics and ALF Contributors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -134,6 +134,44 @@ class ActorDistributionNetwork(Network):
             state: empty
         """
         encoding, state = self._encoding_net(observation, state)
+        act_dist = nest.map_structure(lambda proj: proj(encoding)[0],
+                                      self._projection_net)
+        return act_dist, state
+
+    def make_parallel(self, n):
+        """Create a ``ParallelActorDistributionNetwork`` using ``n`` replicas of ``self``.
+        The initialized network parameters will be different.
+        """
+        return ParallelActorDistributionNetwork(self, n, "parallel_" + self._name)
+
+class ParallelActorDistributionNetwork(Network):
+    """Perform ``n`` actor distribution computations in parallel."""
+    def __init__(self,
+                actor_network: ActorDistributionNetwork,
+                n: int,
+                name="ParallelActorDistributionNetwork"):
+        """
+        It creates a parallelized version of ``actor_network``.
+        Args:
+            actor_network (ActorDistributionNetwork): non-parallelized actor network
+            n (int): make ``n`` replicas from ``actor_network`` with different
+                initialization.
+            name (str):
+        """
+        
+        super().__init__(input_tensor_spec=actor_network.input_tensor_spec, 
+                        name=name)
+        self._encoding_net = actor_network._encoding_net.make_parallel(n)
+        self._projection_net = actor_network._projection_net.make_parallel(n)
+        self._output_spec = self._projection_net.output_spec
+       
+    def forward(self, observation, state=()):
+        """Computes action distribution given a batch of observations.
+        Args:
+            inputs (tuple):  A tuple of Tensors consistent with `input_tensor_spec``.
+            state (tuple): Empty for API consistent with ``ActorDistributionRNNNetwork``.
+        """
+        encoding, _ = self._encoding_net(observation, state)
         act_dist = nest.map_structure(lambda proj: proj(encoding)[0],
                                       self._projection_net)
         return act_dist, state

alf/networks/projection_networks.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2020 Horizon Robotics and ALF Contributors. All Rights Reserved.
+# Copyright (c) 2021 Horizon Robotics and ALF Contributors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -78,6 +78,68 @@ class CategoricalProjectionNetwork(Network):
         else:
             return td.Categorical(logits=logits), state
 
+    def make_parallel(self, n):
+        """Creates a ``ParallelCategoricalProjectionNetwork`` using ``n``
+        replicas of ``self``. The initialized layer parameters will
+        be different.
+        """
+        parallel_proj_net_args = dict(**self.saved_args)
+        parallel_proj_net_args.update(n=n, name="parallel_" + self.name)
+        return ParallelCategoricalProjectionNetwork(**parallel_proj_net_args)
+
+@alf.configurable
+class ParallelCategoricalProjectionNetwork(Network):
+    def __init__(self,
+                 input_size,
+                 action_spec,
+                 n,
+                 logits_init_output_factor=0.1,
+                 name="ParallelCategoricalProjectionNetwork"):
+        """Creates an instance of ParallelCategoricalProjectionNetwork.
+
+
+        Args:
+            input_size (int): input vector dimension
+            action_spec (TensorSpec): a tensor spec containing the information
+                of the output distribution.
+            n (int): number of the parallel networks
+            name (str): name of this network.
+        """
+        super(ParallelCategoricalProjectionNetwork, self).__init__(
+            input_tensor_spec=TensorSpec((input_size, )), name=name)
+
+        assert isinstance(action_spec, TensorSpec)
+
+        unique_num_actions = np.unique(action_spec.maximum -
+                                       action_spec.minimum + 1)
+        if len(unique_num_actions) > 1 or np.any(unique_num_actions <= 0):
+            raise ValueError(
+                'Bounds on discrete actions must be the same for all '
+                'dimensions and have at least 1 action. Projection '
+                'Network requires num_actions to be equal across '
+                'action dimensions. Implement a more general '
+                'categorical projection if you need more flexibility.')
+                
+        output_shape = action_spec.shape + (int(unique_num_actions), )
+        self._output_shape = output_shape
+
+        self._projection_layer = layers.ParallelFC(
+            input_size,
+            np.prod(output_shape),
+            n,
+            kernel_init_gain=logits_init_output_factor)
+        self._n = n
+
+
+    def forward(self, inputs, state=()):
+        logits = self._projection_layer(inputs)
+        logits = logits.reshape(inputs.shape[0], self._n, *self._output_shape)
+        if len(self._output_shape) > 1:
+            return td.Independent(
+                td.Categorical(logits=logits),
+                reinterpreted_batch_ndims=len(self._output_shape) - 1), state
+        else:
+            return td.Categorical(logits=logits), state
 
 @alf.configurable
 class NormalProjectionNetwork(Network):

alf/networks/value_networks.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2020 Horizon Robotics and ALF Contributors. All Rights Reserved.
+# Copyright (c) 2021 Horizon Robotics and ALF Contributors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -115,6 +115,48 @@ class ValueNetwork(PreprocessorNetwork):
         value = value.reshape(value.shape[0], *self._output_spec.shape)
         return value, state
 
+    def make_parallel(self, n):
+        """Create a ``ParallelValueNetwork`` using ``n`` replicas of ``self``.
+        The initialized network parameters will be different.
+        """
+        return ParallelValueNetwork(self, n, "parallel_" + self._name)
+
+class ParallelValueNetwork(PreprocessorNetwork):
+    """Perform ``n`` value computations in parallel."""
+
+    def __init__(self,
+                value_network: ValueNetwork,
+                n: int,
+                name="ParallelValueNetwork"):
+        """
+        It creates a parallelized version of ``value_network``.
+        Args:
+            value_network (ValueNetwork): non-parallelized value network
+            n (int): make ``n`` replicas from ``value_network`` with different
+                initialization.
+            name (str):
+        """
+
+        # TODO: handle input_preprocessors
+        assert value_network._input_preprocessors is None
+        
+        super().__init__(input_tensor_spec=value_network.input_tensor_spec, 
+                        name=name)
+        self._encoding_net = value_network._encoding_net.make_parallel(n)
+        self._output_spec = TensorSpec((n, ) +
+                                       value_network.output_spec.shape)
+
+    def forward(self, observation, state=()):
+        """Computes values given a batch of observations.
+        Args:
+            inputs (tuple):  A tuple of Tensors consistent with `input_tensor_spec``.
+            state (tuple): Empty for API consistent with ``ValueRNNNetwork``.
+        """
+
+        observation, _ = super().forward(observation, state, max_outer_rank=2)
+        value, _ = self._encoding_net(observation)
+        value = value.reshape(value.shape[0], *self._output_spec.shape)
+        return value, state
 
 @alf.configurable
 class ValueRNNNetwork(PreprocessorNetwork):

alf/networks/actor_distribution_networks_test.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2020 Horizon Robotics and ALF Contributors. All Rights Reserved.
+# Copyright (c) 2021 Horizon Robotics and ALF Contributors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,9 +13,11 @@
 # limitations under the License.
 """Tests for alf.networks.actor_distribution_networks."""
 
+from alf.networks.actor_distribution_networks import ParallelActorDistributionNetwork
 from absl.testing import parameterized
 import functools
-
+from absl import logging
+import time
 import torch
 import torch.distributions as td
 
@@ -157,6 +159,60 @@ class TestActorDistributionNetworks(parameterized.TestCase, alf.test.TestCase):
         else:
             self.assertEqual(len(state), len(lstm_hidden_size))
 
+    def test_make_parallel(self):
+        obs_spec = TensorSpec((3, 20, 20), torch.float32)
+        network_ctor, _ = self._init(None)
+        replicas = 4
+        batch_size = 128
+
+        # test continuous action
+        action_spec = BoundedTensorSpec((3, ), torch.float32)
+        actor_dist_net = network_ctor(
+            obs_spec,
+            action_spec,
+            conv_layer_params=self._conv_layer_params,
+            fc_layer_params=self._fc_layer_params,
+            continuous_projection_net_ctor=functools.partial(
+                NormalProjectionNetwork, scale_distribution=True))
+
+        pnet = actor_dist_net.make_parallel(replicas)
+        self.assertTrue(isinstance(pnet, ParallelActorDistributionNetwork))
+        self.assertEqual(pnet.name, "parallel_" + actor_dist_net.name)
+        self.assertTrue(
+            isinstance(actor_dist_net.output_spec, DistributionSpec))
+        act_dist, _  = pnet(obs_spec.randn((batch_size, )))
+        actions = act_dist.sample()
+        self.assertEqual(actions.shape, (batch_size, replicas) + action_spec.shape)
+        self.assertTrue(
+            torch.all(actions >= torch.as_tensor(action_spec.minimum)))
+        self.assertTrue(
+            torch.all(actions <= torch.as_tensor(action_spec.maximum)))
+
+        # test discrete action
+        
+        action_spec = TensorSpec((), torch.int32)
+        # action_spec is not bounded
+        self.assertRaises(
+            AttributeError,
+            network_ctor,
+            obs_spec,
+            action_spec,
+            conv_layer_params=self._conv_layer_params)
+
+        action_spec = BoundedTensorSpec((), torch.int32)
+        actor_dist_net = network_ctor(
+            obs_spec,
+            action_spec,
+            conv_layer_params=self._conv_layer_params)
+
+        pnet = actor_dist_net.make_parallel(replicas)
+        act_dist, _  = pnet(obs_spec.randn((batch_size, )))
+        actions = act_dist.sample()
+        self.assertEqual(actions.shape, (batch_size, replicas) + action_spec.shape)
+        self.assertTrue(
+            torch.all(actions >= torch.as_tensor(action_spec.minimum)))
+        self.assertTrue(
+            torch.all(actions <= torch.as_tensor(action_spec.maximum)))
 
 if __name__ == "__main__":
     alf.test.main()

alf/networks/projection_networks_test.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2020 Horizon Robotics and ALF Contributors. All Rights Reserved.
+# Copyright (c) 2021 Horizon Robotics and ALF Contributors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -59,6 +59,23 @@ class TestCategoricalProjectionNetwork(parameterized.TestCase,
         self.assertTrue(dists.base_dist.probs.std() > 0)
         self.assertTrue(
             torch.isclose(dists.base_dist.probs.mean(), torch.as_tensor(0.2)))
+
+    def test_make_parallel(self):
+        replicas = 4
+        outer_dim = 3
+        input_spec = TensorSpec((10, ), torch.float32)
+        embedding = input_spec.ones(outer_dims=(outer_dim, ))
+
+        net = CategoricalProjectionNetwork(
+            input_size=input_spec.shape[0],
+            action_spec=BoundedTensorSpec((1, ), minimum=0, maximum=4),
+            logits_init_output_factor=0)
+        parallel_net = net.make_parallel(replicas)
+        dist, _ = parallel_net(embedding)
+        self.assertTrue(isinstance(parallel_net.output_spec, DistributionSpec))
+        self.assertEqual(dist.batch_shape, (outer_dim,replicas))
+        self.assertEqual(dist.base_dist.batch_shape, (outer_dim,replicas, 1))
+        self.assertTrue(torch.all(dist.base_dist.probs == 0.2))
 
 
 class TestNormalProjectionNetwork(parameterized.TestCase, alf.test.TestCase):

alf/networks/value_networks_test.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2020 Horizon Robotics and ALF Contributors. All Rights Reserved.
+# Copyright (c) 2021 Horizon Robotics and ALF Contributors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,8 +14,9 @@
 """Tests for alf.networks.value_networks."""
 
 from absl.testing import parameterized
+from absl import logging
 import functools
-
+import time
 import torch
 
 import alf
@@ -72,7 +73,34 @@ class TestValueNetworks(parameterized.TestCase, alf.test.TestCase):
         self.assertEqual(value_net.output_spec, TensorSpec(()))
         # (batch_size,)
         self.assertEqual(value.shape, (1, ))
+    
+    def test_make_parallel(self):
+        obs_spec = TensorSpec((20, ), torch.float32)
+        value_net = ValueNetwork(obs_spec, fc_layer_params=(256,))
 
+        replicas = 4
+        batch_size = 128
+
+        def _train(pnet, name):
+            t0 = time.time()
+            optimizer = alf.optimizers.AdamTF(lr=1e-4)
+            optimizer.add_param_group({'params': list(pnet.parameters())})
+            for _ in range(100):
+                obs = obs_spec.randn((batch_size, ))
+                values = pnet(obs)[0]
+                target = torch.randn_like(values)
+                cost = ((values - target)**2).sum()
+                optimizer.zero_grad()
+                cost.backward()
+                optimizer.step()
+            logging.info(
+                "%s time=%s cost=%s" % (name, time.time() - t0, float(cost)))
+
+        pnet = value_net.make_parallel(replicas)
+        _train(pnet, "ParallelValueNetwork")
+
+        pnet = alf.networks.network.NaiveParallelNetwork(value_net, replicas)
+        _train(pnet, "NaiveParallelNetwork")
 
 if __name__ == "__main__":
     alf.test.main()

commit b6dd1cd2151932f87443c3255559f1554354dce2

Fix code style
Caused by https://github.com/HorizonRobotics/alf/pull/968

Fixed by running `pre-commit run --all-files`

alf/algorithms/actor_critic_algorithm.py
@@ -103,7 +103,8 @@ class ActorCriticAlgorithm(OnPolicyAlgorithm):
         value_network = value_network_ctor(input_tensor_spec=observation_spec)
 
         if reward_spec.numel > 1:
-            value_network = value_network.make_parallel(reward_spec.numel) # value->[B,n] 
+            value_network = value_network.make_parallel(
+                reward_spec.numel)  # value->[B,n]
 
         super(ActorCriticAlgorithm, self).__init__(
             observation_spec=observation_spec,

alf/networks/actor_distribution_networks.py
@@ -142,14 +142,17 @@ class ActorDistributionNetwork(Network):
         """Create a ``ParallelActorDistributionNetwork`` using ``n`` replicas of ``self``.
         The initialized network parameters will be different.
         """
-        return ParallelActorDistributionNetwork(self, n, "parallel_" + self._name)
+        return ParallelActorDistributionNetwork(self, n,
+                                                "parallel_" + self._name)
+
 
 class ParallelActorDistributionNetwork(Network):
     """Perform ``n`` actor distribution computations in parallel."""
+
     def __init__(self,
-                actor_network: ActorDistributionNetwork,
-                n: int,
-                name="ParallelActorDistributionNetwork"):
+                 actor_network: ActorDistributionNetwork,
+                 n: int,
+                 name="ParallelActorDistributionNetwork"):
         """
         It creates a parallelized version of ``actor_network``.
         Args:
@@ -158,13 +161,13 @@ class ParallelActorDistributionNetwork(Network):
                 initialization.
             name (str):
         """
-        
-        super().__init__(input_tensor_spec=actor_network.input_tensor_spec, 
-                        name=name)
+
+        super().__init__(
+            input_tensor_spec=actor_network.input_tensor_spec, name=name)
         self._encoding_net = actor_network._encoding_net.make_parallel(n)
         self._projection_net = actor_network._projection_net.make_parallel(n)
         self._output_spec = self._projection_net.output_spec
-       
+
     def forward(self, observation, state=()):
         """Computes action distribution given a batch of observations.
         Args:

alf/networks/projection_networks.py
@@ -87,6 +87,7 @@ class CategoricalProjectionNetwork(Network):
         parallel_proj_net_args.update(n=n, name="parallel_" + self.name)
         return ParallelCategoricalProjectionNetwork(**parallel_proj_net_args)
 
+
 @alf.configurable
 class ParallelCategoricalProjectionNetwork(Network):
     def __init__(self,
@@ -119,7 +120,7 @@ class ParallelCategoricalProjectionNetwork(Network):
                 'Network requires num_actions to be equal across '
                 'action dimensions. Implement a more general '
                 'categorical projection if you need more flexibility.')
-                
+
         output_shape = action_spec.shape + (int(unique_num_actions), )
         self._output_shape = output_shape
 
@@ -130,7 +131,6 @@ class ParallelCategoricalProjectionNetwork(Network):
             kernel_init_gain=logits_init_output_factor)
         self._n = n
 
-
     def forward(self, inputs, state=()):
         logits = self._projection_layer(inputs)
         logits = logits.reshape(inputs.shape[0], self._n, *self._output_shape)
@@ -140,6 +140,7 @@ class ParallelCategoricalProjectionNetwork(Network):
                 reinterpreted_batch_ndims=len(self._output_shape) - 1), state
         else:
             return td.Categorical(logits=logits), state
+
 
 @alf.configurable
 class NormalProjectionNetwork(Network):

alf/networks/value_networks.py
@@ -121,13 +121,14 @@ class ValueNetwork(PreprocessorNetwork):
         """
         return ParallelValueNetwork(self, n, "parallel_" + self._name)
 
+
 class ParallelValueNetwork(PreprocessorNetwork):
     """Perform ``n`` value computations in parallel."""
 
     def __init__(self,
-                value_network: ValueNetwork,
-                n: int,
-                name="ParallelValueNetwork"):
+                 value_network: ValueNetwork,
+                 n: int,
+                 name="ParallelValueNetwork"):
         """
         It creates a parallelized version of ``value_network``.
         Args:
@@ -139,12 +140,11 @@ class ParallelValueNetwork(PreprocessorNetwork):
 
         # TODO: handle input_preprocessors
         assert value_network._input_preprocessors is None
-        
-        super().__init__(input_tensor_spec=value_network.input_tensor_spec, 
-                        name=name)
+
+        super().__init__(
+            input_tensor_spec=value_network.input_tensor_spec, name=name)
         self._encoding_net = value_network._encoding_net.make_parallel(n)
-        self._output_spec = TensorSpec((n, ) +
-                                       value_network.output_spec.shape)
+        self._output_spec = TensorSpec((n, ) + value_network.output_spec.shape)
 
     def forward(self, observation, state=()):
         """Computes values given a batch of observations.
@@ -157,6 +157,7 @@ class ParallelValueNetwork(PreprocessorNetwork):
         value, _ = self._encoding_net(observation)
         value = value.reshape(value.shape[0], *self._output_spec.shape)
         return value, state
+
 
 @alf.configurable
 class ValueRNNNetwork(PreprocessorNetwork):

alf/networks/actor_distribution_networks_test.py
@@ -180,16 +180,17 @@ class TestActorDistributionNetworks(parameterized.TestCase, alf.test.TestCase):
         self.assertEqual(pnet.name, "parallel_" + actor_dist_net.name)
         self.assertTrue(
             isinstance(actor_dist_net.output_spec, DistributionSpec))
-        act_dist, _  = pnet(obs_spec.randn((batch_size, )))
+        act_dist, _ = pnet(obs_spec.randn((batch_size, )))
         actions = act_dist.sample()
-        self.assertEqual(actions.shape, (batch_size, replicas) + action_spec.shape)
+        self.assertEqual(actions.shape,
+                         (batch_size, replicas) + action_spec.shape)
         self.assertTrue(
             torch.all(actions >= torch.as_tensor(action_spec.minimum)))
         self.assertTrue(
             torch.all(actions <= torch.as_tensor(action_spec.maximum)))
 
         # test discrete action
-        
+
         action_spec = TensorSpec((), torch.int32)
         # action_spec is not bounded
         self.assertRaises(
@@ -201,18 +202,18 @@ class TestActorDistributionNetworks(parameterized.TestCase, alf.test.TestCase):
 
         action_spec = BoundedTensorSpec((), torch.int32)
         actor_dist_net = network_ctor(
-            obs_spec,
-            action_spec,
-            conv_layer_params=self._conv_layer_params)
+            obs_spec, action_spec, conv_layer_params=self._conv_layer_params)
 
         pnet = actor_dist_net.make_parallel(replicas)
-        act_dist, _  = pnet(obs_spec.randn((batch_size, )))
+        act_dist, _ = pnet(obs_spec.randn((batch_size, )))
         actions = act_dist.sample()
-        self.assertEqual(actions.shape, (batch_size, replicas) + action_spec.shape)
+        self.assertEqual(actions.shape,
+                         (batch_size, replicas) + action_spec.shape)
         self.assertTrue(
             torch.all(actions >= torch.as_tensor(action_spec.minimum)))
         self.assertTrue(
             torch.all(actions <= torch.as_tensor(action_spec.maximum)))
+
 
 if __name__ == "__main__":
     alf.test.main()

alf/networks/projection_networks_test.py
@@ -73,8 +73,8 @@ class TestCategoricalProjectionNetwork(parameterized.TestCase,
         parallel_net = net.make_parallel(replicas)
         dist, _ = parallel_net(embedding)
         self.assertTrue(isinstance(parallel_net.output_spec, DistributionSpec))
-        self.assertEqual(dist.batch_shape, (outer_dim,replicas))
-        self.assertEqual(dist.base_dist.batch_shape, (outer_dim,replicas, 1))
+        self.assertEqual(dist.batch_shape, (outer_dim, replicas))
+        self.assertEqual(dist.base_dist.batch_shape, (outer_dim, replicas, 1))
         self.assertTrue(torch.all(dist.base_dist.probs == 0.2))
 
 

alf/networks/value_networks_test.py
@@ -73,10 +73,10 @@ class TestValueNetworks(parameterized.TestCase, alf.test.TestCase):
         self.assertEqual(value_net.output_spec, TensorSpec(()))
         # (batch_size,)
         self.assertEqual(value.shape, (1, ))
-    
+
     def test_make_parallel(self):
         obs_spec = TensorSpec((20, ), torch.float32)
-        value_net = ValueNetwork(obs_spec, fc_layer_params=(256,))
+        value_net = ValueNetwork(obs_spec, fc_layer_params=(256, ))
 
         replicas = 4
         batch_size = 128
@@ -101,6 +101,7 @@ class TestValueNetworks(parameterized.TestCase, alf.test.TestCase):
 
         pnet = alf.networks.network.NaiveParallelNetwork(value_net, replicas)
         _train(pnet, "NaiveParallelNetwork")
+
 
 if __name__ == "__main__":
     alf.test.main()