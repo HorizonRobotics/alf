2cd49db3f02a08c52d5f9cd4c75ea5b683acc2ce
Break Yang <breakds@gmail.com>
2021-09-14 11:27:03 -0700

    This proves memory consumption mainly comes from forwarding the network

--- alf/bin/run_mem.py
+++ alf/bin/run_mem.py
@@ -12,24 +12,75 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import alf
 import torch
 import gym3
 import time
-from gym3 import types_np
 from procgen import ProcgenGym3Env
+from alf.examples import procgen_conf
+from alf.algorithms.data_transformer import create_data_transformer
+from alf.algorithms.data_transformer import ImageScaleTransformer
+
+from alf.algorithms.ppg import DisjointPolicyValueNetwork
+from alf.examples.networks import impala_cnn_encoder
 
 
-def run_raw_gym3():
-    env = ProcgenGym3Env(num=64, env_name='bossfight', render_mode='rgb_array')
-    env = gym3.ExtractDictObWrapper(env, "rgb")
+def encoding_network_ctor(input_tensor_spec, kernel_initializer):
+    encoder_output_size = 256
+    return impala_cnn_encoder.create(
+        input_tensor_spec=input_tensor_spec,
+        cnn_channel_list=(16, 32, 32),
+        num_blocks_per_stack=2,
+        output_size=encoder_output_size,
+        kernel_initializer=kernel_initializer)
+
+
+def run_wrapped(num_envs=64, unroll_length=256, run_network=True):
+    alf.config(
+        'create_environment',
+        env_name='bossfight',
+        num_parallel_environments=num_envs)
+    env = alf.get_env()
+    data_transformer = create_data_transformer([ImageScaleTransformer],
+                                               env.observation_spec())
+
+    observation_spec = data_transformer.transformed_observation_spec
+    print(f'Transformed observation spec: {observation_spec}')
+
+    if run_network:
+        dual_actor_value_network = DisjointPolicyValueNetwork(
+            observation_spec=observation_spec,
+            action_spec=env.action_spec(),
+            encoding_network_ctor=encoding_network_ctor,
+            is_sharing_encoder=False)
+
     step = 0
+    replay_buffer = []
+    result_buffer = []
     while True:
-        env.act(types_np.sample(env.ac_space, bshape=(env.num, )))
-        reward, obs, first = env.observe()
-        print(f"step {step} reward {reward[0]} first {first[0]}")
+        actions = env.action_spec().ones(outer_dims=(num_envs, ))
+        time_step = env.step(actions)
+        time_step, _ = data_transformer.transform_timestep(time_step, ())
+        if run_network:
+            (action_distribution, value,
+             aux), state = dual_actor_value_network(
+                 time_step.observation, state=())
+            result_buffer = []
+            result_buffer.append((action_distribution, value, aux))
+
+        replay_buffer.append(time_step)
         step += 1
-        time.sleep(1)
+
+        if step % unroll_length == 0:
+            print(
+                f'step: {step}, replay_buffer size: {len(replay_buffer)}, shape: {replay_buffer[0].observation.shape}, dtype={replay_buffer[0].observation.dtype}'
+            )
+            replay_buffer = []
+            result_buffer = []
+            time.sleep(1)
 
 
 if __name__ == '__main__':
-    run_raw_gym3()
+    if torch.cuda.is_available():
+        alf.set_default_device('cuda')
+    run_wrapped()
