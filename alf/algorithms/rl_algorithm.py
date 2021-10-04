0b09b4007be635ec589161b6bd104aade30536a1
Break Yang <breakds@gmail.com>
2021-10-04 14:27:55 -0700

    Revert "[Refactor] Retire OnetimeExperienceReplayer (#1012)"
    This reverts commit 4068b3d5d66436bcc0f88509b4336378d3b1672a.

--- alf/algorithms/rl_algorithm.py
+++ alf/algorithms/rl_algorithm.py
@@ -204,7 +204,11 @@ class RLAlgorithm(Algorithm):
         self._current_transform_state = None
 
         if self._env is not None and not self.on_policy:
-            self.set_exp_replayer(self._env.batch_size,
+            if config.whole_replay_buffer_training and config.clear_replay_buffer:
+                replayer = "one_time"
+            else:
+                replayer = "uniform"
+            self.set_exp_replayer(replayer, self._env.batch_size,
                                   config.replay_buffer_length,
                                   config.priority_replay)
 
@@ -491,7 +495,12 @@ class RLAlgorithm(Algorithm):
 
             self.observe_for_metrics(time_step.cpu())
 
-            exp = make_experience(time_step.cpu(), policy_step, policy_state)
+            if self._exp_replayer_type == "one_time":
+                exp = make_experience(transformed_time_step, policy_step,
+                                      policy_state)
+            else:
+                exp = make_experience(time_step.cpu(), policy_step,
+                                      policy_state)
 
             t0 = time.time()
             self.observe_for_replay(exp)
