5dc436cdf0f8a2acbe0ac0dc85acc013479be50f
Le Horizon <le.zhao+git@horizon.ai>
2021-11-05 11:38:32 -0700

    revert unused change

--- alf/experience_replayers/replay_buffer.py
+++ alf/experience_replayers/replay_buffer.py
@@ -61,9 +61,6 @@ class ReplayBuffer(RingBuffer):
                  device="cpu",
                  allow_multiprocess=False,
                  keep_episodic_info=None,
-                 record_episodic_return=False,
-                 default_return=-100000.,
-                 gamma=.99,
                  step_type_field="step_type",
                  enable_checkpoint=False,
                  name="ReplayBuffer"):
