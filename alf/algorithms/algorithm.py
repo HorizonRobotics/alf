b977d832bfd1f70a56fcfd0c98d72930ff59170d
emailweixu <emailweixu@gmail.com>
2021-11-16 14:30:55 -0800

    Allow using schedulers for priority_replay_alpha/beta (#1069)
    * Allow using schedulers for priority_replay_alpha/beta
    
    These two parameters often have a schedule, so we add support for it.
    
    * Fix typo

--- alf/algorithms/algorithm.py
+++ alf/algorithms/algorithm.py
@@ -572,6 +572,9 @@ class Algorithm(AlgorithmInterface):
 
         TODO: for a subalgorithm that's an ignored attribute, its optimizer info
         won't be obtained.
+
+        Returns:
+            str: the json string of the information about all the optimizers.
         """
         unhandled = self._setup_optimizers()
 
@@ -1009,9 +1012,9 @@ class Algorithm(AlgorithmInterface):
         """
         masks = None
         if (batch_info is not None and batch_info.importance_weights != ()
-                and self._config.priority_replay_beta != 0):
+                and self._config.priority_replay):
             masks = batch_info.importance_weights.pow(
-                -self._config.priority_replay_beta).unsqueeze(0)
+                -self._config.priority_replay_beta()).unsqueeze(0)
 
         if valid_masks is not None:
             if masks is not None:
@@ -1476,8 +1479,9 @@ class Algorithm(AlgorithmInterface):
 
         loss_info = self.calc_loss(train_info)
         if loss_info.priority != ():
-            priority = (loss_info.priority**self._config.priority_replay_alpha
-                        + self._config.priority_replay_eps)
+            priority = (
+                loss_info.priority**self._config.priority_replay_alpha() +
+                self._config.priority_replay_eps)
             self._replay_buffer.update_priority(batch_info.env_ids,
                                                 batch_info.positions, priority)
             if self._debug_summaries and alf.summary.should_record_summaries():
