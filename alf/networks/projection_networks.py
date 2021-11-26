531a698228a3d7d66784fdea78f059ebb2ff6dac
emailweixu <emailweixu@gmail.com>
2021-11-25 16:28:53 -0800

    Remove unnecessary import of functools.partial

--- alf/networks/projection_networks.py
+++ alf/networks/projection_networks.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from functools import partial
 import math
 import numpy as np
 from typing import Callable
