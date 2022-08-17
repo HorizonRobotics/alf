df5fcba11e69c14274d727948a7809bfcb8ae576
Haichao Zhang <hczhang1@gmail.com>
2022-08-16 18:18:46 -0700

    Cyclical Scheduler (#1373)
    * CyclicalScheduler
    
    * Address comments and add more tests
    
    * Apply rounding only when necessary

--- alf/utils/schedulers.py
+++ alf/utils/schedulers.py
@@ -12,8 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 """Schedulers."""
-from typing import Callable
+from functools import partial
 from numbers import Number
+from typing import Callable
 
 import alf
 
@@ -192,6 +193,100 @@ class ExponentialScheduler(Scheduler):
         return "ExponentialScheduler('%s', initial_value=%s, decay_rate=%s, decay_time=%s)" % (
             self._progress_type, self._initial_value, self._decay_rate,
             self._decay_time)
+
+
+@alf.configurable
+class CyclicalScheduler(Scheduler):
+    """The cyclical scheduler where the value changes cyclically between two bounds.
+    Reference:
+    ::
+        Leslie N. Smith Cyclical Learning Rates for Training Neural Networks, 2017
+        (https://arxiv.org/pdf/1506.01186.pdf)
+
+    This implementation generalizes the original methods in two ways: 1) the
+    initial value can start from either the lower-bound (as in the original method),
+    or upper bound; 2) apart from the linear switching between the bounds, we
+    also support step mode of switching.
+
+    In terms of applications, beyond the standard case of using a cyclical
+    learning rate to improve the learning behavior during NN training, this
+    scheduler is also useful in other cases. One example is in reinforcement
+    learning, sometimes we want to update the parameters of different modules at
+    difference paces. For example, in TD3, we want to update the policy every other
+    updates. In this case, we can use a ``CyclicalScheduler`` with ``step``
+    switching mode to achieve this. Similar cases also appears in Dreamer.
+    """
+
+    def __init__(self,
+                 progress_type,
+                 base_lr,
+                 bound_lr,
+                 half_cycle_size,
+                 switch_mode='step'):
+        """
+        Args:
+            progress_type (str): one of "percent", "iterations", "env_steps"
+            base_lr (float): the base learning rate, representing the starting
+                value.
+            bound_lr (float): the value of the learning rate on the other bound.
+                The value of ``bound_lr`` could be either larger or smaller than
+                the value of ``base_lr``.
+            half_cycle_size (int|float): the length of half a cycle. Its actual
+                length is based on the ``progress_type``. For example, if in
+                "iterations" mode, it means the lr value will reach the opposite
+                bound every ``half_cycle_size`` iterations.
+            switch_mode (str): the way to switch from one bound to the other.
+                Currently support the following modes:
+                - step: directly jump from one mode to the other every half cycle
+                - linear: linearly move from one mode to the other every half cycle
+        """
+        super().__init__(progress_type)
+        self._base_lr = base_lr
+        self._bound_lr = bound_lr
+        self._half_cycle_size = half_cycle_size
+        self._cycle_size = half_cycle_size * 2
+        assert switch_mode in {
+            "step", "linear"
+        }, ("unsupportted switch mode {}".format(switch_mode))
+        self._switch_mode = switch_mode
+        self._current_value = base_lr
+        # Apply rounding the the calculated progress in cycle and half-cycle
+        # when progress_type is ``percent`` to avoid the issue in stage
+        # transition due to numerical reasons.
+        # For the other progress_types, no rounding is applied.
+        self._rounding_func = partial(round, ndigits=10) \
+                            if progress_type == "percent" else lambda x: x
+
+    def __call__(self):
+        progress = self.progress()
+
+        progress_in_half_cycle = self._rounding_func(
+            (progress % self._half_cycle_size / self._half_cycle_size)) % 1
+        progress_in_cycle = self._rounding_func(
+            (progress % self._cycle_size / self._cycle_size)) % 1
+
+        if self._switch_mode == "step":
+            # step mode changes value at half-cycle point
+            if progress_in_cycle < 0.5:
+                self._current_value = self._base_lr
+            else:
+                self._current_value = self._bound_lr
+
+            return self._current_value
+
+        elif self._switch_mode == "linear":
+            if progress_in_cycle < 0.5:
+                return (1 - progress_in_half_cycle) * self._base_lr + \
+                    progress_in_half_cycle * self._bound_lr
+            else:
+                return progress_in_half_cycle * self._base_lr + \
+                    (1 - progress_in_half_cycle) * self._bound_lr
+
+    def __repr__(self):
+        return ("CyclicalScheduler('%s', base_lr=%s, bound_lr=%s,"
+                "half_cycle_size=%s, switch_mode=%s)") % (
+                    self._progress_type, self._base_lr, self._bound_lr,
+                    self._half_cycle_size, self._switch_mode)
 
 
 def as_scheduler(value_or_scheduler):
---
+++ alf/utils/schedulers_test.py
@@ -0,0 +1,186 @@
+# Copyright (c) 2022 Horizon Robotics. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from absl.testing import parameterized
+import unittest
+import torch
+import alf
+from alf.trainers.policy_trainer import Trainer
+from alf.utils.schedulers import CyclicalScheduler
+import numpy as np
+
+
+class CyclicalSchedulerTest(parameterized.TestCase, unittest.TestCase):
+    """Tests for CyclicalScheduler.
+    """
+
+    @parameterized.parameters(
+        ("iterations", 1, "step"),
+        ("iterations", 2, "step"),
+        ("iterations", 3, "step"),
+        ("iterations", 1, "linear"),
+        ("iterations", 2, "linear"),
+        ("iterations", 3, "linear"),
+        ("env_steps", 1, "step"),
+        ("env_steps", 2, "step"),
+        ("env_steps", 3, "step"),
+        ("env_steps", 1, "linear"),
+        ("env_steps", 2, "linear"),
+        ("env_steps", 3, "linear"),
+        ("percent", 0.1, "step"),
+        ("percent", 0.2, "step"),
+        ("percent", 0.3, "step"),
+        ("percent", 0.1, "linear"),
+        ("percent", 0.2, "linear"),
+        ("percent", 0.3, "linear"),
+    )
+    def test_step_switch_iterations(self, progress_type, half_cycle_size,
+                                    switch_mode):
+
+        trainer_progress = Trainer._trainer_progress
+        # set the trainer_progress mode
+        if progress_type in {"iterations", "percent"}:
+            num_iterations = 10
+            num_env_steps = 0
+        elif progress_type == "env_steps":
+            num_iterations = 0
+            num_env_steps = 10
+
+        trainer_progress.set_termination_criterion(
+            num_iterations=num_iterations, num_env_steps=num_env_steps)
+
+        scheduler = CyclicalScheduler(
+            progress_type=progress_type,
+            base_lr=0,
+            bound_lr=1,
+            half_cycle_size=half_cycle_size,
+            switch_mode=switch_mode)
+
+        effective_half_cycle_size = (
+            half_cycle_size if half_cycle_size >= 1 else
+            half_cycle_size * max(num_iterations, num_env_steps))
+
+        if switch_mode == "step":
+            if effective_half_cycle_size == 1:
+                expected_values = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
+            elif effective_half_cycle_size == 2:
+                expected_values = [0, 0, 1, 1, 0, 0, 1, 1, 0, 0]
+            elif effective_half_cycle_size == 3:
+                expected_values = [0, 0, 0, 1, 1, 1, 0, 0, 0, 1]
+        elif switch_mode == "linear":
+            if effective_half_cycle_size == 1:
+                expected_values = [0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
+            elif effective_half_cycle_size == 2:
+                expected_values = [
+                    0.0, 0.5, 1, 0.5, 0.0, 0.5, 1, 0.5, 0.0, 0.5
+                ]
+            elif effective_half_cycle_size == 3:
+                expected_values = [
+                    0.0, 0.3333333333333333, 0.6666666666666666, 1,
+                    0.6666666666666667, 0.33333333333333337, 0.0,
+                    0.3333333333333333, 0.6666666666666666, 1
+                ]
+
+        values = []
+        for i in range(max(num_iterations, num_env_steps)):
+            if progress_type in {"iterations", "percent"}:
+                trainer_progress.update(iter_num=i)
+            elif progress_type == "env_steps":
+                trainer_progress.update(env_steps=i)
+            value = scheduler()
+            values.append(value)
+
+        self.assertTrue(np.allclose(expected_values, values, atol=5e-2))
+
+    @parameterized.parameters(
+        ("iterations", 1, "step"),
+        ("iterations", 2, "step"),
+        ("iterations", 3, "step"),
+        ("iterations", 1, "linear"),
+        ("iterations", 2, "linear"),
+        ("iterations", 3, "linear"),
+        ("env_steps", 1, "step"),
+        ("env_steps", 2, "step"),
+        ("env_steps", 3, "step"),
+        ("env_steps", 1, "linear"),
+        ("env_steps", 2, "linear"),
+        ("env_steps", 3, "linear"),
+        ("percent", 0.1, "step"),
+        ("percent", 0.2, "step"),
+        ("percent", 0.3, "step"),
+        ("percent", 0.1, "linear"),
+        ("percent", 0.2, "linear"),
+        ("percent", 0.3, "linear"),
+    )
+    def test_step_switch_iterations_upper_bound_as_base(
+            self, progress_type, half_cycle_size, switch_mode):
+
+        trainer_progress = Trainer._trainer_progress
+        # set the trainer_progress mode
+        if progress_type in {"iterations", "percent"}:
+            num_iterations = 10
+            num_env_steps = 0
+        elif progress_type == "env_steps":
+            num_iterations = 0
+            num_env_steps = 10
+
+        trainer_progress.set_termination_criterion(
+            num_iterations=num_iterations, num_env_steps=num_env_steps)
+
+        scheduler = CyclicalScheduler(
+            progress_type=progress_type,
+            base_lr=1,
+            bound_lr=0,
+            half_cycle_size=half_cycle_size,
+            switch_mode=switch_mode)
+
+        effective_half_cycle_size = (
+            half_cycle_size if half_cycle_size >= 1 else
+            half_cycle_size * max(num_iterations, num_env_steps))
+
+        if switch_mode == "step":
+            if effective_half_cycle_size == 1:
+                expected_values = [1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
+            elif effective_half_cycle_size == 2:
+                expected_values = [1, 1, 0, 0, 1, 1, 0, 0, 1, 1]
+            elif effective_half_cycle_size == 3:
+                expected_values = [1, 1, 1, 0, 0, 0, 1, 1, 1, 0]
+        elif switch_mode == "linear":
+            if effective_half_cycle_size == 1:
+                expected_values = [1, 0, 1, 0, 1, 0, 1, 0, 1, 0]
+            elif effective_half_cycle_size == 2:
+                expected_values = [
+                    1.0, 0.5, 0, 0.5, 1.0, 0.5, 0, 0.5, 1.0, 0.5
+                ]
+            elif effective_half_cycle_size == 3:
+                expected_values = [
+                    1.0, 0.6666666666666667, 0.33333333333333337, 0,
+                    0.3333333333333333, 0.6666666666666666, 1.0,
+                    0.6666666666666667, 0.33333333333333337, 0
+                ]
+
+        values = []
+        for i in range(max(num_iterations, num_env_steps)):
+            if progress_type in {"iterations", "percent"}:
+                trainer_progress.update(iter_num=i)
+            elif progress_type == "env_steps":
+                trainer_progress.update(env_steps=i)
+            value = scheduler()
+            values.append(value)
+
+        self.assertTrue(np.allclose(expected_values, values, atol=5e-2))
+
+
+if __name__ == '__main__':
+    unittest.main()
