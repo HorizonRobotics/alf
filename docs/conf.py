30b05851274902b2ac9019e500007cdc5a80f9a7
Haonan Yu <51248379+hnyu@users.noreply.github.com>
2022-09-21 09:50:50 -0700

    rtd furo theme (#1393)

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,7 @@ sys.path.insert(0, os.path.abspath('../'))
 # -- Project information -----------------------------------------------------
 
 project = 'Agent Learning Framework (ALF)'
-copyright = '2020, HorizonRobotics'
+copyright = '2022, HorizonRobotics'
 author = 'HorizonRobotics'
 
 # -- General configuration ---------------------------------------------------
@@ -57,7 +57,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'furo'
+html_theme_option = {'logo_only': True}
+html_logo = "_static/logo.png"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
