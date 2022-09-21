* fixed total_std and opt_std computation bugs
* add deterministic_explore option in bayes_oac

* update multibootstrap gen_input_mask

alf/algorithms/actor_bayes_critic_algorithm.py
@@ -374,22 +374,24 @@ class AbcAlgorithm(OffPolicyAlgorithm):
             explore (bool): whether or not to include UCB-like bonus for 
                 exploration.
             info (namedtuple): info of critic_step. If critic_module is 
-                ``MultiBoostEnsemble``, it contains ``total_std`` and
-                ``opt_std``.
+                ``MultiBoostEnsemble``, it contains ``total_var`` and
+                ``opt_var``.
 
         Returns:
             q_value (Tensor): the q_value for actor training.
         """
         q_mean = critics.mean(1)
-        if hasattr(info, "total_std"):
-            q_total_std = info.total_std
+        if hasattr(info, "total_var"):
+            q_total_var = info.total_var
         else:
-            q_total_std = critics.std(1)  # [bs, d_out] or [bs]
-        if hasattr(info, "opt_std"):
-            q_opt_std = info.opt_std  # [bs, d_out] or [bs]
-            q_epi_std = q_total_std - q_opt_std
+            q_total_var = critics.var(1)  # [bs, d_out] or [bs]
+        q_total_std = torch.sqrt(q_total_var)
+        if hasattr(info, "opt_var"):
+            q_opt_var = info.opt_var  # [bs, d_out] or [bs]
+            q_epi_var = q_total_var - q_opt_var
+            q_epi_std = torch.sqrt(q_epi_var)
         else:
-            q_opt_std = None
+            q_opt_var = None
             q_epi_std = q_total_std
 
         if explore:
@@ -405,10 +407,10 @@ class AbcAlgorithm(OffPolicyAlgorithm):
             safe_mean_hist_summary(prefix + "critics_batch_mean",
                                    q_mean)
             safe_mean_hist_summary(
-                prefix + "critics_total_std", q_total_std)
-            if q_opt_std is not None:
+                prefix + "critics_total_var", q_total_var)
+            if q_opt_var is not None:
                 safe_mean_hist_summary(
-                    prefix + "critic_opt_std", q_opt_std)
+                    prefix + "critic_opt_var", q_opt_var)
 
         return q_value, q_epi_std
 
@@ -756,9 +758,9 @@ class AbcAlgorithm(OffPolicyAlgorithm):
                                 td_target, neglogp[..., i], suffix)
 
         # reweight training (s, a) paris with opt_std
-        if hasattr(critics_info, "opt_std") and \
+        if hasattr(critics_info, "opt_var") and \
             self._critic_training_weight is not None:
-            weights = critics_info.opt_std  # [bs, d_out] or [bs]
+            weights = torch.sqrt(critics_info.opt_var)  # [bs, d_out] or [bs]
             weights = weights.reshape(
                 self._mini_batch_length-1, -1, *weights.shape[1:])
             weights = weights ** self._critic_training_weight

alf/algorithms/bayes_oac_algorithm.py
@@ -200,15 +200,17 @@ class BayesOacAlgorithm(AbcAlgorithm):
 
     def _consensus_q_for_actor_train(self, critics, explore, info=()):
         q_mean = critics.mean(1)
-        if hasattr(info, "total_std"):
-            q_total_std = info.total_std
+        if hasattr(info, "total_var"):
+            q_total_var = info.total_var
         else:
-            q_total_std = critics.std(1)  # [bs, d_out] or [bs]
-        if hasattr(info, "opt_std"):
-            q_opt_std = info.opt_std  # [bs, d_out] or [bs]
-            q_epi_std = q_total_std - q_opt_std
+            q_total_var = critics.var(1)  # [bs, d_out] or [bs]
+        q_total_std = torch.sqrt(q_total_var)
+        if hasattr(info, "opt_var"):
+            q_opt_var = info.opt_var  # [bs, d_out] or [bs]
+            q_epi_var = q_total_var - q_opt_var
+            q_epi_std = torch.sqrt(q_epi_var)
         else:
-            q_opt_std = None
+            q_opt_var = None
             q_epi_std = q_total_std
 
         if explore:
@@ -224,9 +226,9 @@ class BayesOacAlgorithm(AbcAlgorithm):
                 safe_mean_hist_summary("critics_batch_mean",
                                        q_mean)
                 safe_mean_hist_summary(
-                    "critics_total_std", q_total_std)
-                if q_opt_std is not None:
+                    "critics_total_var", q_total_var)
+                if q_opt_var is not None:
                     safe_mean_hist_summary(
-                        "critic_opt_std", q_opt_std)
+                        "critic_opt_var", q_opt_var)
 
         return q_value, q_epi_std

alf/algorithms/multi_bootstrap_ensemble.py
@@ -38,7 +38,7 @@ from alf.utils.sl_utils import classification_loss, regression_loss, auc_score
 from alf.utils.sl_utils import predict_dataset
 
 MbeInfo = namedtuple("MbeInfo",
-                     ["total_std", "opt_std"],
+                     ["total_var", "opt_var"],
                      default_value=())
 
 @alf.configurable 
@@ -246,25 +246,27 @@ class MultiBootstrapEnsemble(FuncParVIAlgorithm):
             - info (MbeInfo)
         """
         self._param_net.set_parameters(self.particles)
-        outputs, _ = self._param_net(inputs)  # [bs, n_particles, d_out]
-        # [bs, n_particles, d_out] or [bs, n_particles]
+        outputs, _ = self._param_net(inputs)  # [bs, np, d_out]
+        # [bs, np, d_out] or [bs, np]
         if isinstance(outputs, td.Distribution):
             outputs_mean = outputs.mean  
         else:
             outputs_mean = outputs
-        total_std = outputs_mean.std(1)  # [bs, d_out] or [bs]
+        total_var = outputs_mean.var(1)  # [bs, d_out] or [bs]
         outputs_mean = outputs_mean.reshape(
             outputs_mean.shape[0],
             self.num_basins, 
             self.num_particles_per_basin, 
-            *outputs_mean.shape[2:])
-        # [bs, n_basins, d_out] or [bs, n_basins]
-        opt_std = outputs_mean.std(2)  
-        opt_std = opt_std.mean(1)  # [bs, d_out] or [bs]
+            *outputs_mean.shape[2:])  # [bs, nb, np, d_out] or [bs, nb, np]
+        basin_means = outputs_mean.mean(2)  # [bs, nb, d_out] or [bs, nb]
+        sse = (outputs_mean - basin_means.unsqueeze(2)) ** 2 
+        # [bs, d_out] or [bs]
+        opt_var = sse.sum(dim=(1,2)) / \
+            (self.num_basins * self.num_particles_per_basin - 1)
 
         return AlgStep(output=outputs,
                        state=(), 
-                       info=MbeInfo(total_std=total_std, opt_std=opt_std))
+                       info=MbeInfo(total_var=total_var, opt_var=opt_var))
 
     def gen_input_mask(self, batchsize):
         """generate input mask for all particles. 