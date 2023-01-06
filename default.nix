c99378ee144d14ef94a59847f045d92dbd7a6a3e
Break Yang <breakds@gmail.com>
2023-01-05 17:37:50 -0800

    Add flake to export the package

---
+++ default.nix
@@ -0,0 +1,104 @@
+{ lib
+, buildPythonPackage
+, pythonRelaxDepsHook
+, pytorchWithCuda11
+, torchvisionWithCuda11
+, numpy
+, einops
+, wandb
+, tensorboard
+, absl-py
+, pyglet
+, opencv4
+, pathos
+, pillow
+, psutil
+, gin-config
+, cnest
+, fasteners
+, rectangle-packer
+, pybox2d
+, matplotlib
+, tqdm
+, gym
+, pybullet
+, atari-py-with-rom
+, procgen
+, highway-env
+, metadrive-simulator
+}:
+
+buildPythonPackage rec {
+  pname = "alf";
+  version = "0.0.6";
+
+  src = ./.;
+
+  nativeBuildInputs = [
+    pythonRelaxDepsHook
+  ];
+
+  pythonRelaxDeps = [
+    "matplotlib"
+    "torchvision"
+    "h5py"
+    "pillow"
+    "pyglet"
+    "pybullet"
+    "torch"
+    "pathos"
+    "atari-py"
+    "rectangle-packer"
+    "procgen"
+    "tensorboard"
+    "gym"
+  ];
+
+  propagatedBuildInputs = [
+    # Machine Learning
+    pytorchWithCuda11
+    torchvisionWithCuda11
+    numpy
+    einops
+    wandb
+    tensorboard
+
+    # Utils
+    absl-py
+    pyglet
+    opencv4
+    pathos
+    pillow
+    psutil
+    gin-config
+    cnest
+    fasteners
+    rectangle-packer
+    pybox2d
+    matplotlib
+    tqdm
+
+    # Simulators
+    gym
+    pybullet
+    atari-py-with-rom
+    procgen
+    highway-env
+    metadrive-simulator
+  ];
+
+  doCheck = false;
+
+  pythonImportsCheck = [ "alf" ];
+
+  meta = with lib; {
+    description = ''
+      Agent Learning Framework (ALF) is a reinforcement learning framework
+      emphasizing on the flexibility and easiness of implementing complex
+      algorithms involving many different components
+    '';
+    homepage = "https://alf.readthedocs.io/en/latest/index.html";
+    license = licenses.asl20;
+    maintainers = with maintainers; [ breakds ];
+  };
+}
