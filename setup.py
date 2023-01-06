500a3ef9c2cd39fcae52e5426e12fc7ddb28a6e3
Break Yang <breakds@gmail.com>
2023-01-05 17:37:50 -0800

    Add flake to export the package

--- setup.py
+++ setup.py
@@ -22,17 +22,11 @@ setup(
     install_requires=[
         'atari_py==0.1.7',
         # used by Box2D-based environments (e.g. BipedalWalker, LunarLander)
-        'box2d-py',
-        'cpplint',
-        'clang-format==9.0',
         'fasteners',
-        'gin-config@git+https://github.com/HorizonRobotics/gin-config.git',
         'gym==0.15.4',
         'gym3==0.3.3',
-        'h5py==3.5.0',
         'matplotlib==3.4.1',
         'numpy',
-        'opencv-python',
         'pathos==0.2.4',
         # with python3.7, the default version of pillow (PIL) is 8.2.0,
         # which breaks some pyglet based rendering in gym
@@ -42,16 +36,9 @@ setup(
         'pybullet==2.5.0',
         'pyglet==1.3.2',  # higher version breaks classic control rendering
         'rectangle-packer==2.0.0',
-        'sphinx==3.0',
-        'sphinx-autobuild',
-        'sphinx-autodoc-typehints@git+https://github.com/hnyu/sphinx-autodoc-typehints.git',
-        'sphinxcontrib-napoleon==0.7',
-        'sphinx-rtd-theme==0.4.3',  # used to build html docs locally
         'tensorboard==2.6.0',
         'torch==1.8.1',
         'torchvision==0.9.1',
-        'torchtext==0.9.1',
-        'cnest==1.0.8',
     ],  # And any other dependencies alf needs
     extras_require={
         'metadrive': ['metadrive-simulator==0.2.5.1', ],
