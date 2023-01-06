192fe6e1bba332da54232e92ba93b1e7bf32732a
Break Yang <breakds@gmail.com>
2023-01-05 17:37:50 -0800

    Add flake to export the package

--- flake.nix
+++ flake.nix
@@ -2,18 +2,33 @@
   description = "Agent Learning Framework Development Environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/21.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
 
     utils.url = "github:numtide/flake-utils";
-    utils.inputs.nixpkgs.follows = "nixpkgs";
 
     alf-devenv.url = "github:HorizonRobotics/alf-nix-devenv";
     alf-devenv.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: inputs.utils.lib.eachSystem [
-    "x86_64-linux" "i686-linux" "aarch64-linux" "x86_64-darwin"
+  outputs = { self, nixpkgs, ... }@inputs: {
+    overlays.default = final: prev: {
+      pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
+        (python-final: python-prev: {
+          alf = python-final.callPackage ./default.nix {};
+        })
+      ];
+    };
+  } // inputs.utils.lib.eachSystem [
+    "x86_64-linux"
   ] (system: {
-    devShell = inputs.alf-devenv.devShell."${system}";
+    devShells.default = inputs.alf-devenv.devShells."${system}".default;
+    packages.default = let pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfree = true;
+      overlays = [
+        inputs.alf-devenv.overlays.default        
+        self.overlays.default
+      ];
+    }; in pkgs.python3Packages.alf;
   });
 }
