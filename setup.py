b37aa2c3122f4b420c35ecd988a364ccb5f27c62
haonan.yu <haonanu@gmail.com>
2023-06-15 08:55:21 -0700

    upgrade cnest version

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
         'torch == 1.8.1',
         'torchvision == 0.9.1',
         'torchtext == 0.9.1',
-        'cnest == 1.0.4',
+        'cnest == 1.1.1',
     ],  # And any other dependencies alf needs
     package_data={'': ['*.gin']},
     packages=find_packages(),
