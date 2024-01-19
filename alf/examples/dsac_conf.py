10ed219a116483457eae2f26b1aa45ff92fd65f3
Jerry Bai <qinxun.bai@gmail.com>
2024-01-18 21:21:37 -0800

    update dsac to work with temporally dependent train_step

--- alf/examples/dsac_conf.py
+++ alf/examples/dsac_conf.py
@@ -140,7 +140,7 @@ alf.config(
     evaluate=True,
     eval_interval=5000,
     num_eval_episodes=5,
-    debug_summaries=True,
+    debug_summaries=False,
     random_seed=0,
     summarize_grads_and_vars=False,
     summary_interval=1000,
