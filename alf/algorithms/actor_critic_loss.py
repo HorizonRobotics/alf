c79292f0c679de23f10c805ee37bcc728f3540e8
Break Yang <breakds@gmail.com>
2024-06-10 15:35:25 -0700

    Revert "Use running statistics to normalize advantages for PPO and AC (#1655)"
    This reverts commit 1de48a9cdacd4a380a5500823ce75da5890cf76c.

--- alf/algorithms/actor_critic_loss.py
+++ alf/algorithms/actor_critic_loss.py
@@ -28,33 +28,15 @@ ActorCriticLossInfo = namedtuple("ActorCriticLossInfo",
                                  ["pg_loss", "td_loss", "neg_entropy"])
 
 
-def normalize(batch_norm, x):
-    batch_norm.train()
-    momentum = batch_norm.momentum
-    if batch_norm.num_batches_tracked * momentum < 1.0:
-        # For the first few batches, we do cumulative moving average
-        batch_norm.momentum = None
-    batch_norm(x)
-    batch_norm.momentum = momentum
-    # We use the running mean and variance of the advantages to normalize
-    # since the batch may not be large enough to properly normalize within
-    # the batch.
-    batch_norm.eval()
-    return batch_norm(x)
-
-
 @alf.configurable
 class ActorCriticLoss(Loss):
     def __init__(self,
-                 reward_dim=1,
                  gamma=0.99,
                  td_error_loss_fn=element_wise_squared_loss,
                  use_gae=False,
                  td_lambda=0.95,
                  use_td_lambda_return=True,
                  normalize_advantages=False,
-                 normalize_scalar_advantages=False,
-                 advantage_norm_momentum=0.9,
                  advantage_clip=None,
                  entropy_regularization=None,
                  td_loss_weight=1.0,
@@ -69,7 +51,6 @@ class ActorCriticLoss(Loss):
             - entropy_regularization * entropy)
 
         Args:
-            reward_dim (int): dimension of the reward.
             gamma (float|list[float]): A discount factor for future rewards. For
                 multi-dim reward, this can also be a list of discounts, each
                 discount applies to a reward dim.
@@ -84,14 +65,8 @@ class ActorCriticLoss(Loss):
                 ``(td_lambda_return = gae_advantage + value_predictions)``.
             td_lambda (float): Lambda parameter for TD-lambda computation.
             normalize_advantages (bool): If True, normalize advantage to zero
-                mean and unit variance within batch for calculating policy
+                mean and unit variance within batch for caculating policy
                 gradient. This is commonly used for PPO.
-            normalize_scalar_advantages (bool): If False, the normalization is
-                performed for each reward dimension. If True, the normalization
-                is performed for the weighted sum of advantages using reward_weights.
-                Note that this will take precedence over `normalize_advantages`.
-            advantage_norm_momentum (float): Momentum for moving average of
-                mean and variance of advantages (same as the momentum for nn.BatchNorm1d).
             advantage_clip (float): If set, clip advantages to :math:`[-x, x]`
             entropy_regularization (float): Coefficient for entropy
                 regularization loss term.
@@ -106,23 +81,13 @@ class ActorCriticLoss(Loss):
         self._use_gae = use_gae
         self._lambda = td_lambda
         self._use_td_lambda_return = use_td_lambda_return
-        if normalize_scalar_advantages:
-            self._adv_norm = torch.nn.BatchNorm1d(
-                num_features=1,
-                eps=1e-8,
-                momentum=advantage_norm_momentum,
-                affine=False,
-                track_running_stats=True)
-            normalize_advantages = False
-        elif normalize_advantages:
-            self._adv_norm = torch.nn.BatchNorm1d(
-                num_features=reward_dim,
-                eps=1e-8,
-                momentum=advantage_norm_momentum,
-                affine=False,
-                track_running_stats=True)
         self._normalize_advantages = normalize_advantages
-        self._normalize_scalar_advantages = normalize_scalar_advantages
+        if normalize_advantages:
+            # Note that onvert_sync_batchnorm does not work with LazyBatchNorm
+            # in general. Fortunately, it works for affine=False and track_running_stats=False
+            # since no parameter needs to be created.
+            self._adv_norm = torch.nn.LazyBatchNorm1d(
+                eps=1e-8, affine=False, track_running_stats=False)
         assert advantage_clip is None or advantage_clip > 0, (
             "Clipping value should be positive!")
         self._advantage_clip = advantage_clip
@@ -136,10 +101,6 @@ class ActorCriticLoss(Loss):
     @property
     def normalizing_advantages(self):
         return self._normalize_advantages
-
-    @property
-    def normalizing_scalar_advantages(self):
-        return self._normalize_scalar_advantages
 
     def forward(self, info):
         """Calculate actor critic loss. The first dimension of all the tensors is
@@ -179,27 +140,20 @@ class ActorCriticLoss(Loss):
                         suffix = '/' + str(i)
                         _summarize(value[..., i], returns[..., i],
                                    advantages[..., i], suffix)
+
         if self._normalize_advantages:
             if hasattr(info, "normalized_advantages"):
                 advantages = info.normalized_advantages
             else:
                 bt = advantages.shape[0] * advantages.shape[1]
-                adv = normalize(self._adv_norm, advantages.reshape(bt, -1))
-                advantages = adv.reshape_as(advantages)
-        elif self._normalize_scalar_advantages:
-            if hasattr(info, "normalized_advantages"):
-                advantages = info.normalized_advantages
-            else:
-                advantages = (advantages * info.reward_weights).sum(-1)
-                adv = normalize(self._adv_norm, advantages.reshape(-1, 1))
+                adv = self._adv_norm(advantages.reshape(bt, -1))
                 advantages = adv.reshape_as(advantages)
 
         if self._advantage_clip:
             advantages = torch.clamp(advantages, -self._advantage_clip,
                                      self._advantage_clip)
 
-        if info.reward_weights != () and not self._normalize_scalar_advantages:
-            # reward_weights has already been applied for self._normalize_scalar_advantages
+        if info.reward_weights != ():
             advantages = (advantages * info.reward_weights).sum(-1)
         pg_loss = self._pg_loss(info, advantages.detach())
 
