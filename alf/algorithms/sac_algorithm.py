3688f96a00b90bbac0815fe4bf21d29f38b85a4b
Haichao Zhang <hczhang1@gmail.com>
2024-06-10 14:08:56 -0700

    revert soft action bound

--- alf/algorithms/sac_algorithm.py
+++ alf/algorithms/sac_algorithm.py
@@ -816,19 +816,6 @@ class SacAlgorithm(OffPolicyAlgorithm):
                 dqda = torch.clamp(dqda, -self._dqda_clipping,
                                    self._dqda_clipping)
                 
-            # if self._action_scale is not None:
-            #     means, magnitudes = spec_utils.spec_means_and_magnitudes(action_spec)
-            #     # apply the action bounding loss
-            #     action_scale = torch.tensor(self._action_scale().astype(np.float32))
-            #     action_bound_max = means + action_scale * magnitudes
-            #     action_bound_min = means - action_scale * magnitudes
-            #     positive_mask = dqda > 0
-            #     action_range = action_bound_max - action_bound_min
-            #     pos_grad_mask = (action_bound_max - action) / action_range
-            #     neg_grad_mask = (action - action_bound_min) / action_range
-            #     grad_mask = torch.where(positive_mask, pos_grad_mask, neg_grad_mask)
-            #     dqda = dqda * grad_mask
-
             if self._action_scale is not None:
                 means, magnitudes = spec_utils.spec_means_and_magnitudes(action_spec)
                 # apply the action bounding loss
@@ -836,16 +823,33 @@ class SacAlgorithm(OffPolicyAlgorithm):
                 soft_action_bound_weight = np.float32(self._soft_action_bound_weight())
                 action_bound_max = means + action_scale * magnitudes
                 action_bound_min = means - action_scale * magnitudes
-                
-                # out of pos bound
-                M_pos = action > action_bound_max
-                M_neg = action < action_bound_min
-                M_out = M_pos | M_neg
+                positive_mask = dqda > 0
                 action_range = action_bound_max - action_bound_min
-                pos_grad = (action_bound_max - action) / action_range
-                neg_grad = (action_bound_min - action) / action_range
-                # weight the gradient outside of action boundary
-                dqda = dqda * ~M_out + (M_pos * pos_grad + M_neg * neg_grad) * soft_action_bound_weight
+                pos_grad_mask = (action_bound_max - action) / action_range
+                neg_grad_mask = (action - action_bound_min) / action_range
+                grad_mask = torch.where(positive_mask, pos_grad_mask, neg_grad_mask) * soft_action_bound_weight
+
+                composed_mask = (1 - soft_action_bound_weight) * torch.ones_like(grad_mask) \
+                                + soft_action_bound_weight * grad_mask 
+                dqda = dqda * composed_mask
+
+            # if self._action_scale is not None:
+            #     means, magnitudes = spec_utils.spec_means_and_magnitudes(action_spec)
+            #     # apply the action bounding loss
+            #     action_scale = torch.tensor(self._action_scale().astype(np.float32))
+            #     soft_action_bound_weight = np.float32(self._soft_action_bound_weight())
+            #     action_bound_max = means + action_scale * magnitudes
+            #     action_bound_min = means - action_scale * magnitudes
+                
+            #     # out of pos bound
+            #     M_pos = action > action_bound_max
+            #     M_neg = action < action_bound_min
+            #     M_out = M_pos | M_neg
+            #     action_range = action_bound_max - action_bound_min
+            #     pos_grad = (action_bound_max - action) / action_range
+            #     neg_grad = (action_bound_min - action) / action_range
+            #     # weight the gradient outside of action boundary
+            #     dqda = dqda * ~M_out + (M_pos * pos_grad + M_neg * neg_grad) * soft_action_bound_weight
                 
                 
                 
