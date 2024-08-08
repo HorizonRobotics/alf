6e4fc7d11acbea4c43bdd11af661325dddef2c36
haonan.yu <haonanu@gmail.com>
2024-08-08 14:11:03 -0700

    update

--- alf/algorithms/algorithm.py
+++ alf/algorithms/algorithm.py
@@ -904,7 +904,8 @@ class Algorithm(AlgorithmInterface):
                 state dictionary.
             visited (set): a set keeping track of the visited objects.
             kwargs: additional keyword arguments for backward compatibility (
-                e.g., 'keep_vars'). They are not used in newer pytorch versions.
+                e.g., 'keep_vars' for torch<0.4). They are not used in newer
+                pytorch versions.
 
         Returns:
             OrderedDict: the dictionary including both model state and optimizers'
