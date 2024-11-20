dd5f16cc06f8ebf36c05b856580e0c2afa548a88
haonan.yu <haonanu@gmail.com>
2024-11-19 16:20:13 -0800

    flow matching for imitation learning

---
+++ alf/algorithms/flow_matching_algorithm.py
@@ -0,0 +1,207 @@
+# Copyright (c) 2024 Horizon Robotics and ALF Contributors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import numpy as np
+
+import torch
+from typing import Optional, Tuple, Callable, Union
+
+import alf
+from alf.data_structures import AlgStep, namedtuple
+from alf.networks.encoding_networks import EncodingNetwork
+from alf.algorithms.algorithm import Algorithm
+from alf.utils.dist_utils import Beta
+from alf.utils import losses
+
+FlowMatchingInfo = namedtuple(
+    'FlowMatchingInfo', ['loss', 'denoise_vec', 'pred_denoise_vec'],
+    default_value=())
+
+
+class FlowMatchingAlgorithm(Algorithm):
+    """Implement flow matching with a simple linear Gaussian (or optimal transport)
+    probability path, as described in
+
+        π0: A Vision-Language-Action Flow Model for General Robot Control, Physical Intelligence, 2024.
+
+    The general idea is to assume a corruption process of the form
+
+    .. math::
+
+        I^t = t*I + (1-t)*e
+
+    where :math:`I` is the clean image, :math:`e` is the noise from :math:`N(0,1)`,
+    and :math:`t` is the time step from 0 to 1.
+
+    By taking the derivative of the above equation with respect to time, we get
+    the vector field
+
+    .. math::
+
+        u^t = I - e
+
+    which is a constant independent of the time step.
+
+    The purpose of this algorithm is to learn a network :math:`v(I^t,t)` to regress
+    this vector field given the corrupted image :math:`I^t` and the time step :math:`t`.
+
+    When generating a new output, we first sample a noise as :math:`I^0\sim N(0,1)`,
+    then use Euler integration to generate a denoised output as
+
+    .. math::
+
+        I^{t+dt} = I^t + v(I^t,t) dt
+
+    The vector field network also supports conditional inputs, in a scenario where
+    the generated output is a function of the conditional inputs (e.g., actions
+    conditioned on robot observations).
+    """
+
+    def __init__(self,
+                 output_tensor_spec: alf.TensorSpec,
+                 noise_tensor_spec: Optional[alf.TensorSpec] = None,
+                 cond_input_tensor_spec: alf.NestedTensorSpec = None,
+                 vector_field_network_ctor: Callable = EncodingNetwork,
+                 tau_beta_paras: Tuple[float] = (1., 1.),
+                 noise_std: float = 1.,
+                 euler_integration_steps: int = 10,
+                 loss_fn: Callable = losses.element_wise_huber_loss,
+                 name: str = "FlowMatchingAlgorithm"):
+        """
+        Args:
+            output_tensor_spec: the tensor spec for the output to be denoised. It
+                should be a single tensor spec with shape ``[N,D1,D2,...]`` where
+                ``N`` is the feature dim and ``D1,D2,...`` are the spatial dims.
+            noise_tensor_spec: if provided, must have the same number of dims with
+                ``output_tensor_spec``. Also the feature dim ``N`` should be the
+                same with ``output_tensor_spec``. Potentially it can have smaller
+                tailing dims than ``output_tensor_spec``.
+                If None, ``output_tensor_spec`` will be used.
+            cond_input_tensor_spec: nested tensor spec for arbitrary conditional
+                inputs.
+            vector_field_network_ctor: constructor for the vector field network.
+                The constructor is expected to take a pair ``(noisy_output, tau)``
+                if ``cond_input_tensor_spec`` is None, or a triplet
+                ``(noisy_output, tau, cond_input)`` if ``cond_input_tensor_spec``
+                is not None, where ``tau`` is a [0,1] scalar of shape ``[B]``.
+            tau_beta_paras: beta distribution parameters a and b for sampling tau
+                which indicates the denoising time step. A bigger ``a`` puts more
+                emphasis on training earlier time steps (denoising noisier outputs).
+            noise_std: the standard deviation of the noise to construct corrupted
+                outputs. A larger std will make the denoising process slower (i.e.,
+                more steps on noisier outputs).
+            euler_integration_steps: the number of integration steps for euler
+                integration when generating a new output.
+            loss_fn: loss function for predicting the vector field.
+            name: the name of the algorithm.
+        """
+        super().__init__(name=name)
+        self._tau_beta = Beta(
+            torch.tensor(tau_beta_paras[0]), torch.tensor(tau_beta_paras[1]))
+        self._int_steps = euler_integration_steps
+        self._noise_spec = noise_tensor_spec or output_tensor_spec
+        self._noise_std = noise_std
+        self._output_spec = output_tensor_spec
+        assert (len(self._noise_spec.shape) == len(self._output_spec.shape)
+                and self._noise_spec.shape[0] == self._output_spec.shape[0])
+        tau_spec = alf.TensorSpec(shape=(), dtype=torch.float32)
+        net_input_spec = (output_tensor_spec, tau_spec)
+        if cond_input_tensor_spec is not None:
+            net_input_spec += (cond_input_tensor_spec, )
+        self._vector_field_net = vector_field_network_ctor(
+            input_tensor_spec=net_input_spec)
+        self._loss_fn = loss_fn
+
+    def _get_random_noise(self, batch_size):
+        # Sample a random noise at t=0
+        noise = self._noise_spec.randn(
+            outer_dims=(batch_size, )) * self._noise_std
+        return self._resize_noise(noise)
+
+    def _resize_noise(self, noise):
+        if self._noise_spec is not self._output_spec:
+            # TODO: might be incompatible with TensorRT
+            noise = torch.nn.functional.interpolate(
+                noise, size=self._output_spec.shape[1:], mode='bilinear')
+        return noise
+
+    def train_step(self,
+                   inputs: Union[Tuple[alf.nest.NestedTensor, torch.
+                                       Tensor], alf.nest.NestedTensor],
+                   state=()):
+        """Perform a training step of the flow matching algorithm.
+
+        Args:
+            inputs: either a tuple of ``(output, cond_input)`` or a single
+                ``output``.
+        """
+        if isinstance(inputs, tuple):
+            output, cond_input = inputs
+        else:
+            output, cond_input = inputs, None
+        batch_size = alf.nest.get_nest_batch_size(output)
+
+        # Construct corrupted output
+        noise = self._get_random_noise(batch_size)
+        tau = self._tau_beta.sample((batch_size, ))
+        tau_ = tau.reshape(-1, *([1] * (output.ndim - 1)))
+        noisy_output = output * tau_ + noise * (1 - tau_)
+        # Cmpute the vector field
+        denoising_vec = output - noise
+        # Predict the vector field
+        inputs = (
+            noisy_output,
+            tau,
+        )
+        if cond_input is not None:
+            inputs += (cond_input, )
+        pred_denoising_vec = self._vector_field_net(inputs)[0]
+
+        loss = self._loss_fn(denoising_vec, pred_denoising_vec)
+        loss = loss.sum(list(range(1, loss.ndim)))
+        return AlgStep(
+            info=FlowMatchingInfo(
+                loss=loss,
+                denoise_vec=denoising_vec,
+                pred_denoise_vec=pred_denoising_vec))
+
+    def generate(self,
+                 cond_input: alf.nest.NestedTensor = None,
+                 batch_size: int = 1,
+                 return_intermediate_steps: bool = False):
+        """Generate new outputs.
+
+        Args:
+            cond_input: the conditional input. If provided, its batch size will
+                be used instead of ``batch_size``.
+            batch_size: the batch size of the generated outputs.
+            return_intermediate_steps: whether to return the intermediate outputs.
+                If True, return all ``self._int_steps`` outputs in a list; otherwise
+                return the final output as a list.
+        """
+        if cond_input is not None:
+            batch_size = alf.nest.get_nest_batch_size(cond_input)
+        output = self._get_random_noise(batch_size)
+        outputs = [output]
+        delta = 1. / self._int_steps
+        for t in np.arange(0, 1, delta):
+            tau = torch.full((batch_size, ), t)
+            inputs = (output, tau)
+            if cond_input is not None:
+                inputs += (cond_input, )
+            output = output + delta * self._vector_field_net(inputs)[0]
+            outputs.append(output)
+        if return_intermediate_steps:
+            return outputs
+        return outputs[-1:]
---
+++ alf/algorithms/flow_matching_algorithm_test.py
@@ -0,0 +1,160 @@
+# Copyright (c) 2024 Horizon Robotics and ALF Contributors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import alf
+from alf.utils.datagen import load_cifar10, load_mnist
+from alf.algorithms.flow_matching_algorithm import FlowMatchingAlgorithm
+from alf.algorithms.monet_algorithm import MoNetUNet
+
+import torch
+from torchvision.transforms import ToPILImage
+from torchvision.transforms import Resize
+
+
+class _VectorFieldNetwork(alf.networks.Network):
+    """A U-Net to predict a vector field from a corrupted image, time step, and
+    a label (optional).
+
+    The time step and label embedding (optional) are first broadcast to the image
+    spatial domain and then concatenated along the image channels.
+    """
+
+    def __init__(self, input_tensor_spec):
+        super().__init__(
+            input_tensor_spec=input_tensor_spec, name="VectorFieldNetwork")
+        self._img_spec = input_tensor_spec[0]
+        label_spec = None
+        in_channels = self._img_spec.shape[0] + 1
+        if len(input_tensor_spec) == 3:
+            label_spec = input_tensor_spec[-1]
+        if label_spec is not None:
+            self._label_embedding = torch.nn.Embedding(
+                num_embeddings=label_spec.maximum + 1, embedding_dim=4)
+            in_channels += 4
+        self._unet_input_resize = Resize((32, 32))
+        self._unet = MoNetUNet(
+            input_tensor_spec=alf.TensorSpec((in_channels, 32, 32)),
+            filters=(32, 64, 64, 128, 256),
+            nonskip_fc_layers=(512, ),
+            output_channels=self._img_spec.shape[0])
+        self._unet_output_resize = Resize(self._img_spec.shape[-2:])
+
+    def forward(self, inputs, state=()):
+        _, H, W = self._img_spec.shape
+        label = None
+        img, tau = inputs[:2]
+        if len(inputs) == 3:
+            label = inputs[-1]
+        tau = tau.reshape(-1, 1, 1, 1).expand(-1, -1, H, W)  # [B] -> [B,1,H,W]
+        if label is not None:
+            # [B] -> [B,4]
+            label = self._label_embedding(label)
+            label = label[..., None, None].expand(-1, -1, H,
+                                                  W)  # [B,4] -> [B,4,H,W]
+            inputs = torch.cat([img, tau, label], dim=1)
+        else:
+            inputs = torch.cat([img, tau], dim=1)
+        output = self._unet(self._unet_input_resize(inputs))[0]
+        output = self._unet_output_resize(output)
+        return output, state
+
+
+class FlowMatchingAlgorithmTest(alf.test.TestCase):
+    def test_cond_gen_images(self, name="mnist"):
+        """Test the conditional generation of images using flow matching.
+
+        Args:
+            name: either "mnist" or "cifar10". Note for "cifar10" the training data
+                is not enough and thus the generation result usually doesn't resemble
+                any training sample.
+        """
+
+        if name == "mnist":
+            image_spec = alf.TensorSpec((1, 28, 28))
+        else:
+            image_spec = alf.TensorSpec((3, 32, 32))
+
+        device = alf.get_default_device()
+        flow_match_alg = FlowMatchingAlgorithm(
+            output_tensor_spec=image_spec,
+            cond_input_tensor_spec=alf.BoundedTensorSpec((),
+                                                         maximum=9,
+                                                         dtype=torch.int64),
+            vector_field_network_ctor=_VectorFieldNetwork,
+            tau_beta_paras=(1., 1.5),
+            noise_std=2.,
+            euler_integration_steps=30)
+
+        # Train
+        optimizer = torch.optim.Adam(
+            list(flow_match_alg.parameters()), lr=1e-2)
+        if name == "mnist":
+            train_loader, test_loader = load_mnist(train_bs=128)
+        else:
+            train_loader, test_loader = load_cifar10(train_bs=128)
+        epochs = 15
+        for e in range(epochs):
+            epoch_losses = []
+            data_iter = iter(train_loader)
+            while True:
+                try:
+                    # No good way to let the data loader directly load CUDA tensors,
+                    # so we have to manually set the device.
+                    alf.set_default_device('cpu')
+                    img, label = next(data_iter)
+                    img, label = img.cuda(), label.cuda()
+                except StopIteration:
+                    break
+                finally:
+                    alf.set_default_device(device)
+                optimizer.zero_grad()
+                alg_step = flow_match_alg.train_step((img, label))
+                loss = alg_step.info.loss.mean()
+                loss.backward()
+                epoch_losses.append(loss)
+                optimizer.step()
+            print(f"Epoch {e} loss: ", sum(epoch_losses) / len(epoch_losses))
+
+        # Generate
+        flow_match_alg.eval()
+        samples_per_class = 2
+        with torch.no_grad():
+            classes = torch.tensor(list(range(10)), dtype=torch.int64)
+            classes = torch.repeat_interleave(classes, samples_per_class)
+            # [B,1,H,W]
+            imgs = flow_match_alg.generate(
+                classes, return_intermediate_steps=True)
+            # take the denoising steps every two
+            imgs = imgs[::2]
+
+            image = torch.cat(imgs, dim=-1)  # [B,C,H,NW]
+            image = torch.transpose(image, 0, 1)  # [C,B,H,NW]
+            image = torch.flatten(image, 1, 2)  # [C,BH,NW]
+
+            # The image has been normalized with mean and std.
+            # To convert it to [0,1], we need to unnormalize it.
+            if name == "mnist":
+                mean = torch.tensor([0.1307])
+                std = torch.tensor([0.3081])
+            else:
+                mean = torch.tensor([0.4914, 0.4822, 0.4465])
+                std = torch.tensor([0.2023, 0.1994, 0.2010])
+            image = image * std[:, None, None] + mean[:, None, None]
+
+            image = ToPILImage()(torch.clip(image, 0, 1))
+            image.save("/tmp/flow_match_generation.png")
+
+
+if __name__ == "__main__":
+    alf.test.main()
