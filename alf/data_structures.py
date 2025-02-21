39c9c0a13b00b78c2b14731129dd4dbf137783b1
Haichao Zhang <hczhang1@gmail.com>
2025-02-21 12:43:42 -0800

    Improve prev_action docstring

--- alf/data_structures.py
+++ alf/data_structures.py
@@ -99,7 +99,9 @@ class TimeStep(
     - reward: a ``Tensor`` of reward values from executing 'prev_action'.
     - discount: A discount value in the range :math:`[0, 1]`.
     - observation: A (nested) ``Tensor`` for observation.
-    - prev_action: A (nested) ``Tensor`` for action from previous time step.
+    - prev_action: A (nested) ``Tensor`` for action from previous time step. Note that
+        it should be the action that was sent to the environment in the previous time step,
+        meaning it is in the action space defined by the environment's action_spec.
     - env_id: A scalar ``Tensor`` of the environment ID of the time step.
     - untransformed: a nest that represents the entire time step itself *before*
       any transformation (e.g., observation or reward transformation); used for
