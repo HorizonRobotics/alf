a789563ee591494fb2c834de18028c40604b8cef
Haichao Zhang <hczhang1@gmail.com>
2025-04-02 12:34:45 -0700

    Address comments

--- alf/algorithms/distributed_off_policy_algorithm.py
+++ alf/algorithms/distributed_off_policy_algorithm.py
@@ -769,13 +769,14 @@ class DistributedUnroller(DistributedOffPolicyAlgorithm):
         with self._shared_mem_lock:
             if self._shared_alg_params.buf[0] == 1:
                 with record_time(
-                        "time/unroller_params_update/1_buffer_from_io"):
+                        "time/dist_unroller_params_update/1_buffer_from_io"):
                     buffer = io.BytesIO(self._shared_alg_params.buf[1:])
         if buffer is not None:
-            with record_time("time/unroller_params_update/2_load_to_cpu"):
+            with record_time("time/dist_unroller_params_update/2_load_to_cpu"):
                 state_dict = torch.load(buffer, map_location='cpu')
             # We might only update part of the params
-            with record_time("time/unroller_params_update/3_load_state_dict"):
+            with record_time(
+                    "time/dist_unroller_params_update/3_load_state_dict"):
                 self._core_alg.load_state_dict(state_dict, strict=False)
             logging.debug("Params updated from the trainer.")
             with self._shared_mem_lock:
@@ -791,7 +792,7 @@ class DistributedUnroller(DistributedOffPolicyAlgorithm):
         """
         # Copied from super().train_iter()
         if self._config.empty_cache:
-            with record_time("time/unroller_train_iter/0_empty_cache"):
+            with record_time("time/dist_unroller_train_iter/0_empty_cache"):
                 torch.cuda.empty_cache()
 
         if self._unroller_only:
@@ -813,8 +814,10 @@ class DistributedUnroller(DistributedOffPolicyAlgorithm):
             self._registered = True
 
         # Experience will be sent to the trainer in this function
-        with record_time("time/unroller_train_iter/1_unroll_iter_off_policy"):
+        with record_time(
+                "time/dist_unroller_train_iter/1_unroll_iter_off_policy"):
             self._unroll_iter_off_policy()
-        with record_time("time/unroller_train_iter/2_check_params_update"):
+        with record_time(
+                "time/dist_unroller_train_iter/2_check_params_update"):
             self._check_params_update()
         return 0
