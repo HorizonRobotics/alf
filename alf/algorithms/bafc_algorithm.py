move batch_to_graphs into actor_graph forward, and correct the stop gradient logics of actor and critic training in bafc_algorithm

alf/algorithms/bafc_algorithm.py
@@ -67,46 +67,6 @@ BafcLossInfo = namedtuple(
     'BafcLossInfo', ('actor', 'critic'), default_value=())
 
 
-def batch_to_graphs(
-    weights,  # list of shape [bs, out, in] tensors
-    biases,   # list of shape [bs, out] tensors
-    weights_mean=None,
-    weights_std=None,
-    biases_mean=None,
-    biases_std=None,
-):
-    device = weights[0].device
-    bsz = weights[0].shape[0]
-    num_nodes = weights[0].shape[2] + sum(w.shape[1] for w in weights)
-
-    node_features = torch.zeros(bsz, num_nodes, device=device)
-    edge_features = torch.zeros(
-        bsz, num_nodes, num_nodes, device=device
-    )
-
-    row_offset = 0
-    col_offset = weights[0].shape[2]  # no edge to input nodes
-    for i, w in enumerate(weights):
-        _, num_out, num_in = w.shape
-        w_mean = weights_mean[i] if weights_mean is not None else 0
-        w_std = weights_std[i] if weights_std is not None else 1
-        edge_features[
-            :, col_offset:col_offset + num_out, row_offset:row_offset + num_in
-        ] = (w - w_mean) / w_std
-        row_offset += num_in
-        col_offset += num_out
-
-    row_offset = weights[0].shape[2]  # no bias in input nodes
-    for i, b in enumerate(biases):
-        _, num_out = b.shape
-        b_mean = biases_mean[i] if biases_mean is not None else 0
-        b_std = biases_std[i] if biases_std is not None else 1
-        node_features[:, row_offset:row_offset + num_out] = (b - b_mean) / b_std
-        row_offset += num_out
-
-    return node_features, edge_features
-
-
 @alf.configurable
 class BafcAlgorithm(OffPolicyAlgorithm):
     r"""Boostrapped Actor and Functional Critic algorithm, 
@@ -321,26 +281,27 @@ class BafcAlgorithm(OffPolicyAlgorithm):
             state=state._replace(action=action_state),
             info=BafcInfo(action=action))
 
-    def _encode_actor(self):
-        eval_out = self._actor_networks(
-            self._actor_eval_samples, full_neurons=True)[0]
-        node_features, edge_features = batch_to_graphs(
-            self._actor_networks.weight_params,
-            self._actor_networks.bias_params
-        ) 
-        inputs = (node_features, edge_features, eval_out)
+    def _encode_actor(self, params, eval_out):
+        inputs = (params, eval_out)
         graph_inputs = self._actor_graph(inputs)
         return self._graph_network(graph_inputs)
 
-    def _actor_train_step(self, observation, action, actor_encoding, state):
+    def _actor_train_step(self, observation, action, state):
         """Compute the full off-policy policy gradient from the functional critic,
-        which consists of two terms, 
+        which consists of three terms, 
 
         1. the gradient w.r.t. input action, as in standard actor-critic algorithms.
 
-        2. the gradient w.r.t. input actor encoding, obtainable only with functional 
-           critic.
+        2. the gradient w.r.t. eval action, i.e., actor_networks' outputs for 
+           self._actor_eval_samples.
+
+        3. the gradient w.r.t. actor parameters.
         """
+        actor_params = (self._actor_networks.weight_params, 
+                        self._actor_networks.bias_params) 
+        eval_action = self._actor_networks(
+            self._actor_eval_samples, full_neurons=True)[0]
+        actor_encoding = self._encode_actor(actor_params, eval_action)
         # [B * n_actor, d]
         critic_observation = observation.repeat_interleave(
             self._num_bootstrapped_actors, dim=0)
@@ -348,10 +309,12 @@ class BafcAlgorithm(OffPolicyAlgorithm):
             (actor_encoding, (critic_observation, action)), state) 
 
         # This sum() will reduce all dims so q_value can be any rank
-        action_grad = nest_utils.grad(action, q_value.sum(), retain_graph=True)
-        encoding_grad = nest_utils.grad(actor_encoding, q_value.sum()) #, retain_graph=True)
+        dqda = nest_utils.grad(action, q_value.sum(), retain_graph=True)
+        eval_action_in_graph = eval_action[1:]
+        dqde = nest_utils.grad(eval_action_in_graph, 
+                               q_value.sum(), retain_graph=True)
 
-        def critic_input_loss_fn(dqda, a_in):
+        def action_loss_fn(dqda, a_in):
             if self._dqda_clipping:
                 dqda = torch.clamp(dqda, -self._dqda_clipping,
                                    self._dqda_clipping)
@@ -359,28 +322,41 @@ class BafcAlgorithm(OffPolicyAlgorithm):
                 (dqda + a_in).detach(), a_in)
             return loss.sum(list(range(1, loss.ndim)))
 
-        # loss corresponding to input action
-        action_loss = nest.map_structure(critic_input_loss_fn, action_grad, action)
+        # 1. loss corresponding to input action
+        action_loss = nest.map_structure(action_loss_fn, dqda, action)
         action_loss = action_loss.reshape(
             -1, self._num_bootstrapped_actors).sum(-1)
         # action_loss = math_ops.add_n(nest.flatten(action_loss))
 
-        # loss corresponding to input actor encoding
-        encoding_loss = nest.map_structure(
-            critic_input_loss_fn, encoding_grad, actor_encoding)
+        # 2. loss corresponding to input eval_action
+        eval_action_loss = nest.map_structure(
+            action_loss_fn, dqde, eval_action_in_graph)
         # ALF workaround: reduce to scalar_loss and repeat to [T*B]
         # Will be averaged to a scalar_loss in calc_loss
-        encoding_loss = encoding_loss.sum().repeat(
+        eval_action_loss = math_ops.add_n(eval_action_loss).mean().repeat(
+            action_loss.shape[0])
+
+        # 3. loss corresponding to input actor_params
+        dqdw = nest_utils.grad(actor_params, q_value.sum(), retain_graph=True)
+        param_loss = nest.map_structure(
+            lambda x, y: torch.mean(x * y), dqdw, actor_params)
+        # ALF workaround: reduce to scalar_loss and repeat
+        param_loss = math_ops.add_n(nest.flatten(param_loss)).repeat(
             action_loss.shape[0])
 
         actor_info = LossInfo(
             loss=action_loss,
-            extra=BafcActorInfo(action_loss=action_loss, 
-                                param_loss=encoding_loss))
+            extra=BafcActorInfo(action_loss=eval_action_loss, 
+                                param_loss=param_loss))
         return critic_state, actor_info
 
     def _critic_train_step(self, observation, state: BafcCriticState, 
-                           rollout_info: BafcInfo, action, actor_encoding):
+                           rollout_info: BafcInfo, action): 
+        actor_params = (self._actor_networks.weight_params, 
+                        self._actor_networks.bias_params) 
+        eval_action = self._actor_networks(
+            self._actor_eval_samples, full_neurons=True)[0]
+        actor_encoding = self._encode_actor(actor_params, eval_action)
         critics, critic_state = self._critic_network(
             (actor_encoding, (observation, rollout_info.action)), state.critic)
         # [T*B, n_actor]
@@ -407,10 +383,16 @@ class BafcAlgorithm(OffPolicyAlgorithm):
             if self._actor_update_counter % self._actor_utd == 0:
                 self._train_mode = TrainMode.critic
                 self._critic_network.set_obs_action_batch_dominate(False)
+                for p in self._actor_networks.parameters():
+                    p.requires_grad_(False)
+                self._actor_eval_samples.requires_grad_(True)
         elif self._train_mode == TrainMode.critic:
             if self._critic_update_counter % self._critic_utd == 0:
                 self._train_mode = TrainMode.actor
                 self._critic_network.set_obs_action_batch_dominate(True)
+                for p in self._actor_networks.parameters():
+                    p.requires_grad_(True)
+                self._actor_eval_samples.requires_grad_(False)
 
     def train_step(self, inputs: TimeStep, state: BafcState,
                    rollout_info: BafcInfo):
@@ -425,32 +407,26 @@ class BafcAlgorithm(OffPolicyAlgorithm):
                               actor=state.actor,
                               critic=state.critic)
 
-        # actor_params = (self._actor_networks.weight_params, 
-        #                 self._actor_networks.bias_params) 
-        actor_encoding = self._encode_actor()
-
         if self._train_mode == TrainMode.standard or (
                 self._critic_update_counter == 0
                 and self._actor_update_counter == 0):
             actor_state, actor_info = self._actor_train_step(
-                inputs.observation, action, actor_encoding, state.actor)
+                inputs.observation, action, state.actor)
             critic_state, critic_info = self._critic_train_step(
-                inputs.observation, state.critic, rollout_info, 
-                action, actor_encoding)
+                inputs.observation, state.critic, rollout_info, action)
             new_state = new_state._replace(actor=actor_state,
                                            critic=critic_state)
             self._critic_update_counter += 1
         else:
             if self._train_mode == TrainMode.actor:
                 actor_state, actor_info = self._actor_train_step(
-                    inputs.observation, action, actor_encoding, state.actor)
+                    inputs.observation, action, state.actor)
                 critic_info = BafcCriticInfo()
                 new_state = new_state._replace(actor=actor_state)
                 self._actor_update_counter += 1
             else:
                 critic_state, critic_info = self._critic_train_step(
-                    inputs.observation, state.critic, rollout_info, 
-                    action, actor_encoding)
+                    inputs.observation, state.critic, rollout_info, action)
                 actor_info = LossInfo(extra=BafcActorInfo())
                 # actor_info = LossInfo()
                 new_state = new_state._replace(critic=critic_state)
@@ -472,15 +448,19 @@ class BafcAlgorithm(OffPolicyAlgorithm):
     def calc_loss(self, info: BafcInfo):
         assert not self._is_eval
         actor_loss = info.actor
-        scalar_loss = actor_loss.extra.param_loss
-        if isinstance(scalar_loss, torch.Tensor):
-            scalar_loss = scalar_loss.mean()
+        eval_action_loss = actor_loss.extra.action_loss
+        param_loss = actor_loss.extra.param_loss
+        if isinstance(eval_action_loss, torch.Tensor):
+            eval_action_loss = eval_action_loss.mean()
+        if isinstance(param_loss, torch.Tensor):
+            param_loss = param_loss.mean()
         if self._train_mode == TrainMode.actor:
             critic_loss = LossInfo()
         else:
             critic_loss = self._calc_critic_loss(info)
 
         loss = math_ops.add_ignore_empty(actor_loss.loss, critic_loss.loss)
+        scalar_loss = math_ops.add_ignore_empty(eval_action_loss, param_loss)
 
         return LossInfo(
             loss=loss,

alf/networks/neural_graphs/actor_graph.py
@@ -5,6 +5,48 @@ from rff.layers import GaussianEncoding
 import alf
 
 
+def batch_to_graphs(
+    # weights,  # list of shape [bs, out, in] tensors
+    # biases,   # list of shape [bs, out] tensors
+    params,
+    weights_mean=None,
+    weights_std=None,
+    biases_mean=None,
+    biases_std=None,
+):
+    weights, biases = params
+    device = weights[0].device
+    bsz = weights[0].shape[0]
+    num_nodes = weights[0].shape[2] + sum(w.shape[1] for w in weights)
+
+    node_features = torch.zeros(bsz, num_nodes, device=device)
+    edge_features = torch.zeros(
+        bsz, num_nodes, num_nodes, device=device
+    )
+
+    row_offset = 0
+    col_offset = weights[0].shape[2]  # no edge to input nodes
+    for i, w in enumerate(weights):
+        _, num_out, num_in = w.shape
+        w_mean = weights_mean[i] if weights_mean is not None else 0
+        w_std = weights_std[i] if weights_std is not None else 1
+        edge_features[
+            :, col_offset:col_offset + num_out, row_offset:row_offset + num_in
+        ] = (w - w_mean) / w_std
+        row_offset += num_in
+        col_offset += num_out
+
+    row_offset = weights[0].shape[2]  # no bias in input nodes
+    for i, b in enumerate(biases):
+        _, num_out = b.shape
+        b_mean = biases_mean[i] if biases_mean is not None else 0
+        b_std = biases_std[i] if biases_std is not None else 1
+        node_features[:, row_offset:row_offset + num_out] = (b - b_mean) / b_std
+        row_offset += num_out
+
+    return node_features, edge_features
+
+
 @alf.configurable
 class ActorGraph(nn.Module):
     def __init__(
@@ -96,7 +138,8 @@ class ActorGraph(nn.Module):
         )
 
     def forward(self, inputs):
-        node_features, edge_features, eval_out = inputs
+        params, eval_out = inputs
+        node_features, edge_features = batch_to_graphs(params)
         eval_out = [
             proj(eval_out[i].permute(1, 2, 0)) for i, proj in enumerate(
                 self.proj_eval_out)]