3e84c08470c41fd306ba71872fc9ec1ff66383ff
Siqi Chai <32528321+sqchai@users.noreply.github.com>
2025-08-15 23:48:53 +0000

    handle mask_out_loss_for_last_step option in RLAlgorithm

--- alf/algorithms/rl_algorithm.py
+++ alf/algorithms/rl_algorithm.py
@@ -764,8 +764,11 @@ class RLAlgorithm(Algorithm):
             self._config.unroll_length)
 
         with record_time("time/train"):
-            valid_masks = (experience.step_type
-                           != StepType.LAST).to(torch.float32)
+            if self._config.mask_out_loss_for_last_step:
+                valid_masks = (experience.step_type
+                            != StepType.LAST).to(torch.float32)
+            else:
+                valid_masks = None
             loss_info, params = self.update_with_gradient(
                 loss_info, valid_masks)
             self.after_update(experience.time_step, train_info)
