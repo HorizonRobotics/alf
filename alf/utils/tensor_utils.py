cef267e2c0ecff72b9d1a16167d05e4390c7faf0
Andrew Choi <neffneff4@gmail.com>
2026-03-27 13:50:58 -0700

    Have global_norm properly handle when input consists of only None (#1832)

--- alf/utils/tensor_utils.py
+++ alf/utils/tensor_utils.py
@@ -205,13 +205,13 @@ def global_norm(tensors):
         norm (Tensor): a scalar tensor
     """
     assert alf.nest.is_nested(tensors), "tensors must be a nest! %s" % tensors
-    tensors = alf.nest.flatten(tensors)
+    tensors = [t for t in alf.nest.flatten(tensors) if t is not None]
     if not tensors:
         return torch.zeros((), dtype=torch.float32)
     return torch.sqrt(
         sum([
             math_ops.square(torch.norm(torch.reshape(t, [-1])))
-            for t in tensors if t is not None
+            for t in tensors
         ]))
 
 
